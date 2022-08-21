dd027f10bca2aec95671a38d9160262af8bf8f05
Peter Seibel <peter@gigamonkeys.com>
2022-08-20 21:23:30 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -37,7 +37,7 @@
 
 const size = 5;
 
-for (let x = (width % size) / 2; x < width - size; x += 2 * size) {
+for (let x = size + (width % size) / 2; x < width - size; x += 2 * size) {
     for (let y = size; y < height - size; y += 2 * size) {
         drawCircle(x, y, size, 'blue');
     }
