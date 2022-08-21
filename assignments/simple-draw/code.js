222d91e8653b185a46b7907330c539c01f25bd9a
Peter Seibel <peter@gigamonkeys.com>
2022-08-20 22:02:46 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -48,7 +48,7 @@ while (true) {
     let extra = gap * i;
     let x = (width * extra) / (height + extra);
     drawLine(x, 0, width, height, 'black');
-    if (width - x < width * 0.001) break;
+    if (width - x < width/2) break;
 
     if (i++ > 20000) break;
 }
