3e0bb8d4197fae30bc65c6f676a1e4d1d2d59638
Peter Seibel <peter@gigamonkeys.com>
2022-08-27 14:05:13 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -13,14 +13,14 @@ const drawFrame = (time) => {
     const timeGap = 2000 / 8;
 
 
-    ballAndLine(2 * Math.PI * 0/16, time, 0 * timeGap);
-    ballAndLine(2 * Math.PI * 1/16, time, 1 * timeGap);
-    ballAndLine(2 * Math.PI * 2/16, time, 2 * timeGap);
-    ballAndLine(2 * Math.PI * 3/16, time, 3 * timeGap);
-    ballAndLine(2 * Math.PI * 4/16, time, 4 * timeGap);
-    ballAndLine(2 * Math.PI * 5/16, time, 5 * timeGap);
-    ballAndLine(2 * Math.PI * 6/16, time, 6 * timeGap);
-    ballAndLine(2 * Math.PI * 7/16, time, 7 * timeGap);
+    ballAndLine(2 * Math.PI * 0/16, time, 7 * timeGap);
+    ballAndLine(2 * Math.PI * 1/16, time, 6 * timeGap);
+    ballAndLine(2 * Math.PI * 2/16, time, 5 * timeGap);
+    ballAndLine(2 * Math.PI * 3/16, time, 4 * timeGap);
+    ballAndLine(2 * Math.PI * 4/16, time, 3 * timeGap);
+    ballAndLine(2 * Math.PI * 5/16, time, 2 * timeGap);
+    ballAndLine(2 * Math.PI * 6/16, time, 1 * timeGap);
+    ballAndLine(2 * Math.PI * 7/16, time, 0 * timeGap);
     //drawBallAndLine(x1, midY, x2, midY, time, 0);
     //drawBallAndLine(midX, midY - r, midX, midY + r, time, 200);
 }
