1f707b9c4e03347a63bfab46aa64c6e989c3d5e4
Peter Seibel <peter@gigamonkeys.com>
2022-08-26 20:21:28 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -1,15 +1,16 @@
 const midX = width / 2;
 const midY = height / 2;
+const r = Math.min(midX, midY);
 
 const drawFrame = (time) => {
     clear();
-    drawCircle(midX, midY, Math.min(midX, midY), '#bbb');
+    drawCircle(midX, midY, r, '#bbb');
     drawLine(0, midY, width, midY, '#bbb');
     drawFilledCircle(ballX(time), midY, 10, 'blue');
 }
 
 const ballX = (time) => {
-    let i = time / 3;
+    let i = time / 4;
     let c = Math.floor(i / width) % 2;
     return c === 0 ? i % width : width - (i % width);
 };
