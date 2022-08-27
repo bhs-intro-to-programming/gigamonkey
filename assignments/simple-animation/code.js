701e41aea6db7ed307eb0fb422bc01cc27cb5bf4
Peter Seibel <peter@gigamonkeys.com>
2022-08-27 14:10:25 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -18,7 +18,9 @@ const drawFrame = (time) => {
     const timeGap = MILLIS_PER_PASS / 16;
 
     for (let i = 0; i < 8; i++) {
-        ballAndLine(2 * Math.PI * i/16, time, i * timeGap);
+        if (now() - start > i * INTRO_RATE) {
+            ballAndLine(2 * Math.PI * i/16, time, i * timeGap);
+        }
     }
 }
 
