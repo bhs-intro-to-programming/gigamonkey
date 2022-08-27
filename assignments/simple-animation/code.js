a385f72838542b72095cc866a4da9373b4f047c9
Peter Seibel <peter@gigamonkeys.com>
2022-08-27 13:45:43 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -27,7 +27,7 @@ const drawBall = (x1, y1, x2, y2, time, color) => {
 };
 
 const fromStartLinear = (d, time, ms) => {
-    const step = d/ms;
+    const step = 2 * d/ms;
     const i = time * step;
     return Math.floor(i / d) % 2 ? i % d : d - (i % d);
 };
