07267b5b027a6d9899dbf277c608d7354479007c
Peter Seibel <peter@gigamonkeys.com>
2022-08-27 13:35:06 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -30,7 +30,7 @@ const fromStartLinear = (d, time) => {
 };
 
 const fromStartSinusoidal = (d, time) => {
-    const i = time / 250;
+    const i = time / 500;
     return (1 - Math.cos(i)) / 2 * d;
 }
 
