079502c0f42515eef502d06140cd5ec0b9212687
Peter Seibel <peter@gigamonkeys.com>
2022-08-26 17:29:34 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -2,7 +2,7 @@ const gap = 4;
 
 drawFilledRect(0, 0, width, height, 'white');
 
-for (let y = height, x = 0; width - x > 2; y -= gap) {
+for (let y = height, x = 0; width - x > 1; y -= gap) {
     x = y > 0 ? 0 : width * -y / (height - y);
     drawLine(x, Math.max(y, 0), width, height, 'black');
 }
