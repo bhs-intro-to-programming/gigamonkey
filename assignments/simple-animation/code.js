98feb5f06517fe3fc4db3a7a9e08c5368baf3d7e
Peter Seibel <peter@gigamonkeys.com>
2022-08-26 20:46:07 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -27,7 +27,7 @@ const fromStart = (d, time) => {
 };
 
 const distance = (x1, y1, x2, y2) => {
-    Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 };
 
 animate(drawFrame);
