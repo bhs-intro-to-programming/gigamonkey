6776bc00c0a50054b47cce0fe9593b693fec82fa
Peter Seibel <peter@gigamonkeys.com>
2022-08-27 13:36:23 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -7,13 +7,13 @@ const drawFrame = (time) => {
     drawCircle(midX, midY, r, '#bbb');
     const x1 = midX - r;
     const x2 = midX + r;
-    drawBallAndLine(x1, midY, x2, midY, time);
-    drawBallAndLine(midX, midY - r, midX, midY + r, time);
+    drawBallAndLine(x1, midY, x2, midY, time, 0);
+    drawBallAndLine(midX, midY - r, midX, midY + r, time, 200);
 }
 
-const drawBallAndLine = (x1, y1, x2, y2, time) => {
+const drawBallAndLine = (x1, y1, x2, y2, time, timeOffset) => {
     drawLine(x1, y1, x2, y2, '#bbb');
-    drawBall(x1, y1, x2, y2, time, 'blue');
+    drawBall(x1, y1, x2, y2, time + timeOffset, 'blue');
 }
 
 const drawBall = (x1, y1, x2, y2, time, color) => {
