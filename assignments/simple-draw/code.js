d68c2c3ab1975ac6bd3b79f129c1f5c31dd9c72a
Peter Seibel <peter@gigamonkeys.com>
2022-08-29 21:01:53 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,10 +1,34 @@
-const gap = 4;
+const lerp = (a, b, amount) => {//0xrrggbb hex linear interpolate
+    const ar = a >> 16,
+          ag = a >> 8 & 0xff,
+          ab = a & 0xff,
 
-drawFilledRect(0, 0, width, height, 'white');
+          br = b >> 16,
+          bg = b >> 8 & 0xff,
+          bb = b & 0xff,
 
-for (let y = height, x = 0; width - x > 1; y -= gap) {
-    x = y > 0 ? 0 : width * -y / (height - y);
-    drawLine(x, Math.max(y, 0), width, height, 'black');
+          rr = ar + amount * (br - ar),
+          rg = ag + amount * (bg - ag),
+          rb = ab + amount * (bb - ab);
+
+    return (rr << 16) + (rg << 8) + (rb | 0);
+};
+const convert = (a,base)=> { //int -> str
+    let e=""
+    let b=""
+    while (a>0) {
+        let c=a%base
+        c=c==10?"A":c==11?"B":c==12?"C":c==13?"D":c==14?"E":c==15?"F":c
+        b=(c+b)
+        a=Math.floor(a/base)
+    }
+    while (b.length<6)b="0"+b
+    return "#"+b
 }
-
-drawFilledCircle(width / 2, height / 2, 150, 'rgba(255,0,255,0.25)');
+const gradient=(clr0,clr1)=> { // takes 2 24bit ints
+    drawFilledRect (0, 0, width, height, convert(clr0,16))
+    for (var i=0;i<width;i++) {
+        drawFilledRect (i, 0, 1, height, convert(lerp(clr1,clr0,i/width),16))
+    }
+}
+gradient(0xffffff,0x000000)//call
