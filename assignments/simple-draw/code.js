695cc3d7855a6fad3e35bf90ec4371d87f14ea93
Peter Seibel <peter@gigamonkeys.com>
2022-08-30 13:46:28 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -3,13 +3,13 @@
  * green, and blue components individually.
  */
 const lerp = (a, b, amount) => {
-    let r = 0;
-    for (let i = 0; i < 3; i++) {
-        const ac = a >> (i * 8) & 0xff;
-        const bc = b >> (i * 8) & 0xff;
-        r |= interpolate(ac, bc, amount) << (i * 8);
-    };
-    return r;
+  let r = 0;
+  for (let i = 0; i < 3; i++) {
+    const ac = a >> (i * 8) & 0xff;
+    const bc = b >> (i * 8) & 0xff;
+    r |= interpolate(ac, bc, amount) << (i * 8);
+  };
+  return r;
 };
 
 /*
@@ -26,9 +26,9 @@ const rgb = (a) => `#${a.toString(16).padStart(6, '0')}`;
  * Fill the drawing area with a gradient.
  */
 const gradient = (clr0, clr1) => {
-    for (let x = 0; x < width; x++) {
-        drawFilledRect(x, 0, 1, height, rgb(lerp(clr0, clr1, x / width)));
-    }
+  for (let x = 0; x < width; x++) {
+    drawFilledRect(x, 0, 1, height, rgb(lerp(clr0, clr1, x / width)));
+  }
 };
 
 gradient(0xff0000, 0x0000ff);
