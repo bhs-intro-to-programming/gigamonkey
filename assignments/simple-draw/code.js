848267f0a7d6ba6bf04874a8d2410826d3f5e0a7
Peter Seibel <peter@gigamonkeys.com>
2022-08-29 21:28:41 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,19 +1,30 @@
+/*
+ * Interpolate between two colors by interpolating the the red, 
+ * green, and blue components individually.
+ */
 const lerp = (a, b, amount) => {
-    // 0xrrggbb hex linear interpolate
-    return interpolate(a >> 16, b >> 16, amount) << 16 
-    | interpolate(a >> 8 & 0xff, b >> 8 & 0xff, amount) << 8
-    | interpolate(a & 0xff, b & 0xff, amount);
+    return interpolate(a >> 16, b >> 16, amount) << 16
+        | interpolate(a >> 8 & 0xff, b >> 8 & 0xff, amount) << 8
+        | interpolate(a & 0xff, b & 0xff, amount);
 };
 
-const interpolate = (a, b, amount) => a + amount * (b  - a);
+/*
+ * Linear interpolation between two values.
+ */
+const interpolate = (a, b, amount) => b + amount * (a - b);
 
+/*
+ * Convert a numeric value to an hex color string.
+ */
 const rgb = (a) => `#${a.toString(16).padStart(6, '0')}`;
 
+/*
+ * Fill the drawing area with a gradient.
+ */
 const gradient = (clr0, clr1) => {
-    // takes 2 24bit ints
     for (let i = 0; i < width; i++) {
         drawFilledRect(i, 0, 1, height, rgb(lerp(clr1, clr0, i / width)));
     }
 };
 
-gradient(0xff0000, 0x0000ff)//call
+gradient(0xff0000, 0x0000ff);
