36d56428c45727ae1f0abc5c31dfb02cda7e7977
Peter Seibel <peter@gigamonkeys.com>
2022-08-29 21:14:44 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -15,6 +15,23 @@ const lerp = (a, b, amount) => {
     return (rr << 16) + (rg << 8) + (rb | 0);
 };
 
+const lerp2 = (a, b, amount) => {
+    // 0xrrggbb hex linear interpolate
+    const ar = a >> 16;
+    const ag = a >> 8 & 0xff;
+    const ab = a & 0xff;
+
+    const br = b >> 16;
+    const bg = b >> 8 & 0xff;
+    const bb = b & 0xff;
+
+    const rr = ar + amount * (br - ar);
+    const rg = ag + amount * (bg - ag);
+    const rb = ab + amount * (bb - ab);
+
+    return (rr << 16) + (rg << 8) + (rb | 0);
+};
+
 const rgb = (a) => `#${a.toString(16).padStart(6, '0')}`;
 
 const gradient = (clr0, clr1) => { // takes 2 24bit ints
@@ -24,4 +41,4 @@ const gradient = (clr0, clr1) => { // takes 2 24bit ints
     }
 };
 
-gradient(0xff0000, 0x0000ff)//call
+gradient(0xff0000, 0xff00ff)//call
