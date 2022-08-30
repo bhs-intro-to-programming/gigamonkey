6a119888ec97d7e1dad419a82a7c0c8e27b3c82a
Peter Seibel <peter@gigamonkeys.com>
2022-08-29 21:21:37 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,3 +1,20 @@
+const OLDlerp = (a, b, amount) => {
+    // 0xrrggbb hex linear interpolate
+    const ar = a >> 16;
+    const ag = a >> 8 & 0xff;
+    const ab = a & 0xff;
+
+    const br = b >> 16;
+    const bg = b >> 8 & 0xff;
+    const bb = b & 0xff;
+
+    const rr = ar + amount * (br - ar);
+    const rg = ag + amount * (bg - ag);
+    const rb = ab + amount * (bb - ab);
+
+    return (rr << 16) + (rg << 8) + (rb | 0);
+};
+
 const lerp = (a, b, amount) => {
     // 0xrrggbb hex linear interpolate
     const ar = a >> 16;
@@ -8,37 +25,21 @@ const lerp = (a, b, amount) => {
     const bg = b >> 8 & 0xff;
     const bb = b & 0xff;
 
-    const rr = ar + amount * (br - ar);
-    const rg = ag + amount * (bg - ag);
-    const rb = ab + amount * (bb - ab);
+    const rr = interpolate(ar, br);
+    const rg = interpolate(ag, bg);
+    const rb = interpolate(ab, bb);
 
     return (rr << 16) + (rg << 8) + (rb | 0);
 };
 
-const lerp2 = (a, b, amount) => {
-    // 0xrrggbb hex linear interpolate
-    const ar = a >> 16;
-    const ag = a >> 8 & 0xff;
-    const ab = a & 0xff;
-
-    const br = b >> 16;
-    const bg = b >> 8 & 0xff;
-    const bb = b & 0xff;
-
-    const rr = ar + amount * (br - ar);
-    const rg = ag + amount * (bg - ag);
-    const rb = ab + amount * (bb - ab);
-
-    return (rr << 16) + (rg << 8) + (rb | 0);
-};
+const interpolate = (a, b, amount) => a + amount * (b  - a);
 
 const rgb = (a) => `#${a.toString(16).padStart(6, '0')}`;
 
-const gradient = (clr0, clr1) => { // takes 2 24bit ints
-    //drawFilledRect(0, 0, width, height, rgb(clr0));
+const gradient = (clr0, clr1) => {
+    // takes 2 24bit ints
     for (let i = 0; i < width; i++) {
         drawFilledRect(i, 0, 1, height, rgb(lerp(clr1, clr0, i / width)));
-        console.log(i);
     }
 };
 
