100d0bbcb39c6e8170c795347e9f8a7232ff2821
Peter Seibel <peter@gigamonkeys.com>
2022-08-29 21:17:42 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -35,8 +35,8 @@ const lerp2 = (a, b, amount) => {
 const rgb = (a) => `#${a.toString(16).padStart(6, '0')}`;
 
 const gradient = (clr0, clr1) => { // takes 2 24bit ints
-    drawFilledRect(0, 0, width, height, rgb(clr0));
-    for (let i = 0; i < width; i += 10) {
+    //drawFilledRect(0, 0, width, height, rgb(clr0));
+    for (let i = 0; i < width; i++) {
         drawFilledRect(i, 0, i + 1, height, rgb(lerp(clr1, clr0, i / width)));
     }
 };
