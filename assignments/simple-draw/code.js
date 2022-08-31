50f42bd0bfcdd71f25389df98891a90549840d12
Peter Seibel <peter@gigamonkeys.com>
2022-08-31 15:38:58 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -34,3 +34,4 @@ const gradient = (clr0, clr1) => {
 //gradient(0xff0000, 0x0000ff);
 
 drawFilledRect(20, 20, width - 40, height - 40, 'rgba(255, 0, 45, 0.5)');
+drawFilledRect(50, 50, width - 45, height - 40, 'rgba(255, 0, 255, 0.25)');
