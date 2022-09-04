1849ae7e15e0fc4aa761dbaf312babfe19ef4436
Peter Seibel <peter@gigamonkeys.com>
2022-09-04 16:33:42 -0700

    Updating assignments/test-cases-01/code.js

--- assignments/test-cases-01/code.js
+++ assignments/test-cases-01/code.js
@@ -7,3 +7,14 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const mod = (a, b) => a % b;
+
+const averageOfTwo = (a, b) => (a + b) / 2;
+
+const averageOfThree = (a, b, c) => (a + b + c) / 3;
+
+const distance = (a, b) => Math.abs(a - b);
+
+const manhattanDistance = (x1, y1, x2, y2) => distance(x1, x2) + distance(y1, y2);
+
+const euclidianDistance = (x1, y1, x2, y2) => 
+  Math.sqrt(distance(x1, x2) ** 2 + distance(y1, y2) ** 2);
