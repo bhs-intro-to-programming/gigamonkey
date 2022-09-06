9a497930762020e7327cff42e7f9a415438324b8
Peter Seibel <peter@gigamonkeys.com>
2022-09-05 18:43:11 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,7 +1,7 @@
 const padding = 10;
 const side = Math.min(width - padding * 2, height - padding * 2);
 
-const left = width/2 - side/2;
-const top = height/2 - side/2;
+const x = width/2 - side/2;
+const y = height/2 - side/2;
 
 drawFilledSquare(left, top, side, side, 'black');
