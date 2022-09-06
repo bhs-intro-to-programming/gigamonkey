4f27686e743c2fe6859b8619d07e1eb9e0ce2a0c
Peter Seibel <peter@gigamonkeys.com>
2022-09-05 18:43:26 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -4,4 +4,4 @@ const side = Math.min(width - padding * 2, height - padding * 2);
 const x = width/2 - side/2;
 const y = height/2 - side/2;
 
-drawFilledRect(left, top, side, side, 'black');
+drawFilledRect(x, y, side, side, 'black');
