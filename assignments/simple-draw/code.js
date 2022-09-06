f84cb7461eca7b3ecfbc5397d252a00c3013e7b4
Peter Seibel <peter@gigamonkeys.com>
2022-09-05 18:42:50 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,35 +1,7 @@
-/*
- * Interpolate between two colors by interpolating the red, 
- * green, and blue components individually.
- */
-const lerp = (a, b, amount) => {
-  let r = 0;
-  for (let i = 0; i < 3; i++) {
-    const ac = a >> (i * 8) & 0xff;
-    const bc = b >> (i * 8) & 0xff;
-    r |= interpolate(ac, bc, amount) << (i * 8);
-  };
-  return r;
-};
+const padding = 10;
+const side = Math.min(width - padding * 2, height - padding * 2);
 
-/*
- * Linear interpolation between two values.
- */
-const interpolate = (a, b, amount) => a + amount * (b - a);
+const left = width/2 - side/2;
+const top = height/2 - side/2;
 
-/*
- * Convert a numeric value to an hex color string.
- */
-const rgb = (a) => `#${a.toString(16).padStart(6, '0')}`;
-
-/*
- * Fill the drawing area with a gradient.
- */
-const gradient = (clr0, clr1) => {
-  for (let x = 0; x < width; x++) {
-    drawFilledRect(x, 0, 1, height, rgb(lerp(clr0, clr1, x / width)));
-  }
-};
-
-gradient(0xff0000, 0x0000ff);
-//test
+drawFilledSquare(left, top, side, side, 'black');
