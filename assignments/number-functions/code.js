48b64530769bc359a304f69740526d8d2cc81f52
Peter Seibel <peter@gigamonkeys.com>
2022-09-08 13:20:33 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,1 +1,11 @@
 const add = (a, b) => a + b;
+
+const subtract = (a , b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const mod = (a, b) => a % b;
+
+const averageOfTwo = (a, b) => (a + b) / 2;
