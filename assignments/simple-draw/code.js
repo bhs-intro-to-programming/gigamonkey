a8b6d6e4f6dad5211b705f0b18539c2f667d2daa
Peter Seibel <peter@gigamonkeys.com>
2022-09-11 07:40:19 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -15,7 +15,7 @@ const downTriangle = (x, y, side, color) => {
 
 const cutHoles = (x, y, side) => {
   cutBigHole(x, y, side);
-  if (side > 2) {
+  if (side > 4) {
     cutSmallHoles(x, y, side);
   }
 };
