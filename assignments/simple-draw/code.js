fd3c4600e4872d421abeef79bfc6afa083abb103
Peter Seibel <peter@gigamonkeys.com>
2022-09-11 13:25:56 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -79,7 +79,7 @@ const fillWithCircles = (radius) => {
   let yOffset = (width % d) / 2 + radius;
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
-      if (Math.random() < 0.12) {
+      if (Math.random() < 0.19) {
         drawFilledCircle(xOffset + c * d, yOffset + r * d, radius, 'blue');
       } else {
         drawCircle(xOffset + c * d, yOffset + r * d, radius, 'blue');
