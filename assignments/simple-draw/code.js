7363d2ff514a73009edf953644b77ace75c93138
Peter Seibel <peter@gigamonkeys.com>
2022-09-11 07:31:50 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -11,24 +11,20 @@ const downTriangle = (x, y, side, color) => {
   drawFilledTriangle(x, y, x + side / 2, y + side * RISE, x + side, y, color);
 }
 
-const cutHole = (x, y, side) => {
+const cutBigHole = (x, y, side) => {
   downTriangle(x + side * 0.25, y - (side/2 * RISE), side/2, 'white');
 };
 
 const cutSmallHoles = (x, y, side) => {
-  cutHole(x, y, side/2);
-  cutHole(x + side/2, y, side/2);
-  cutHole(x + side/4, y - side/2 * RISE, side/2);
+  cutBigHole(x, y, side/2);
+  cutBigHole(x + side/2, y, side/2);
+  cutBigHole(x + side/4, y - side/2 * RISE, side/2);
 }
 
 let side = MAX;
 
 // Draw the big blue triangle first
 upTriangle(0, bottom, side, 'blue');
-cutHole(0, bottom, side);
+cutBigHole(0, bottom, side);
 
 cutSmallHoles(0, bottom, side);
-// The next three holes
-//downTriangle(MAX * 0.25 * 0.5, bottom - (MAX * RISE * 0.5 * 0.5), MAX / 2 * 0.5, 'white');
-//downTriangle(MAX / 2 + MAX * 0.25 * 0.5, bottom - (MAX * RISE * 0.5 * 0.5), MAX / 2 * 0.5, 'white');
-//downTriangle(MAX / 2 - MAX * 0.125, bottom - (MAX * RISE * 0.75), MAX / 2 * 0.5, 'white');
