9fa3cb346522823ed89bc0fa2daf741bfa12c673
Peter Seibel <peter@gigamonkeys.com>
2022-09-11 07:32:46 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -11,6 +11,10 @@ const downTriangle = (x, y, side, color) => {
   drawFilledTriangle(x, y, x + side / 2, y + side * RISE, x + side, y, color);
 }
 
+const cutHoles = (x, y, side) => {
+  cutBigHole(x, y, size);
+};
+
 const cutBigHole = (x, y, side) => {
   downTriangle(x + side * 0.25, y - (side/2 * RISE), side/2, 'white');
 };
@@ -19,12 +23,12 @@ const cutSmallHoles = (x, y, side) => {
   cutBigHole(x, y, side/2);
   cutBigHole(x + side/2, y, side/2);
   cutBigHole(x + side/4, y - side/2 * RISE, side/2);
-}
+};
 
 let side = MAX;
 
 // Draw the big blue triangle first
 upTriangle(0, bottom, side, 'blue');
-cutBigHole(0, bottom, side);
+//cutBigHole(0, bottom, side);
 
-cutSmallHoles(0, bottom, side);
+//cutSmallHoles(0, bottom, side);
