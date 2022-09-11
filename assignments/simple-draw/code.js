689e93eb8fbb1b6ef96d483a32111404d820079c
Peter Seibel <peter@gigamonkeys.com>
2022-09-11 07:46:14 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -5,18 +5,30 @@ const bottom = height - (height - MAX * RISE) * 0.75;
 
 let triangles = 1;
 
+/*
+ * Draw a filled equilateral triange pointing up with bottom-left corner at 
+ * x,y and a given side length and color.
+ */
 const upTriangle = (x, y, side, color) => {
   drawFilledTriangle(x, y, side / 2, y - side * RISE, x + side, y, color);
 }
 
+/*
+ * Draw a filled equilateral triange pointing down with top-left corner at 
+ * x,y and a given side length and color.
+ */
 const downTriangle = (x, y, side, color) => {
   drawFilledTriangle(x, y, x + side / 2, y + side * RISE, x + side, y, color);
 }
 
-const cutHoles = (x, y, side) => {
+/*
+ * Cut all the holes in the triangle with its bottom-left corner at x,y and
+ * sides of length side.
+ */
+const cutHoles = (x, y, side, smallest) => {
   cutBigHole(x, y, side);
-  if (side > 4) {
-    cutSmallHoles(x, y, side);
+  if (side > smallest) {
+    cutSmallerHoles(x, y, side, smallest);
   }
 };
 
@@ -25,16 +37,14 @@ const cutBigHole = (x, y, side) => {
   downTriangle(x + side * 0.25, y - (side/2 * RISE), side/2, '#eeeeef');
 };
 
-const cutSmallHoles = (x, y, side) => {
-  cutHoles(x, y, side/2);
-  cutHoles(x + side/2, y, side/2);
-  cutHoles(x + side/4, y - side/2 * RISE, side/2);
+const cutSmallerHoles = (x, y, side, smallest) => {
+  cutHoles(x, y, side/2, smallest);
+  cutHoles(x + side/2, y, side/2, smallest);
+  cutHoles(x + side/4, y - side/2 * RISE, side/2, smallest);
 };
 
-let side = MAX;
-
 // Draw the big blue triangle first
-upTriangle(0, bottom, side, 'blue');
-cutHoles(0, bottom, side);
+upTriangle(0, bottom, MAX, 'blue');
+cutHoles(0, bottom, MAX, 32);
 
 console.log(`${triangles} triangles.`);
