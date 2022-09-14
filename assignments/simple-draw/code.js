8a4c6b71636481debace2dba62c6384cca5d5cd7
Peter Seibel <peter@gigamonkeys.com>
2022-09-13 17:50:02 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -6,10 +6,10 @@ drawFilledRect(0, (2 * height / 3) + (height / 6), width, (2 * height / 3), 'rgb
 let rect = 1000
 while (rect >= 2 * height / 3) {
   drawLine(0, rect, width, rect, 'rgba(0,128,0, 1', 0)
-  let rect = rect - 2
+  rect = rect - 2
   while (rect >= 3 * height / 4) {
     drawLine(0, rect, width, rect, 'rgba(0,128,0, 1', 0)
-    let rect = rect - 1
+    rect = rect - 1
   }
 }
 
