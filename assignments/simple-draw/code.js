ada77ae0f9d7998eca34ad30575c9eedc0b2d67d
Peter Seibel <peter@gigamonkeys.com>
2022-09-13 17:57:01 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -3,32 +3,33 @@
 drawFilledRect(0, 0, 500, 600, 'rgba(115,215,255,1)');
 drawFilledRect(0, (2 * height / 3) + (height / 6), width, (2 * height / 3), 'rgba(0, 64, 0, 1');
 
-let rect = 1000
+let rect = 1000;
 while (rect >= 2 * height / 3) {
-  drawLine(0, rect, width, rect, 'rgba(0,128,0, 1', 0)
-  rect = rect - 2
+  drawLine(0, rect, width, rect, 'rgba(0,128,0, 1', 0);
+  rect -= 2;
   while (rect >= 3 * height / 4) {
-    drawLine(0, rect, width, rect, 'rgba(0,128,0, 1', 0)
-    rect = rect - 1
+    drawLine(0, rect, width, rect, 'rgba(0,128,0, 1', 0);
+    rect -= 1;
   }
 }
 
-let rays = 500
+let rays = 500;
 while (rays >= width - 2 * width / 7) {
-  drawLine(0, 0, rays, height, 'rgba(242,242,37,.1', 1)
-  rays = rays - 1
+  drawLine(0, 0, rays, height, 'rgba(242,242,37,.1', 1);
+  rays -= 1;
 }
-let rays2 = 125
+let rays2 = 125;
 while (rays2 >= width * 0) {
-  drawLine(0, 0, rays2, height, 'rgba(242,242,37,.1', 1)
-  rays2 = rays2 - 1
+  drawLine(0, 0, rays2, height, 'rgba(242,242,37,.1', 1);
+  rays2 -= 1;
 }
 
-let rays3 = 3000
+let rays3 = 3000;
 while (rays3 >= 3 * width) {
-  drawLine(0, 0, rays3, height, 'rgba(242,242,37,.1', 1)
-  rays3 = rays3 - 6
+  drawLine(0, 0, rays3, height, 'rgba(242,242,37,.1', 1);
+  rays3 -= 6;
 }
+
 drawFilledCircle(0, 0, 100, 'rgba(250,220,15,1)')
 
 drawFilledCircle(width / 2, height / 3, 100, "red")
