07557db63dd5d0f1d5fd9faf6193ed9f650c908e
Peter Seibel <peter@gigamonkeys.com>
2022-09-14 17:01:53 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,19 +1,30 @@
-const add=(a, b)=>a+b;
+// Jack's code
 
-const subtract=(a,b)=>a-b;
+const add = (a, b) => {
+  return a + b
+}
+const subtract = (a, b) => {
+  return a - b
+}
+const multiply = (a, b) => {
+  return a * b
+}
+const divide = (a, b) => {
+  return a / b
+}
+const mod = (a, b) => {
+  return a % b
+}
+const averageOfTwo = (a, b) => {
+  return (a + b) / 2
+}
+const averageOfThree = (a, b, c) => {
+  return (a + b + c) / 3
+}
+const distance = (a, b) => {
+  return Math.abs(a - b)
+}
 
-const multiply=(a,b)=>a*b;
+const manhattanDistance = (x1, y1, x2, y2) => distance(x1, x2) + distance(y1, y2)
 
-const divide=(a,b)=>a/b;
-
-const mod=(a,b)=>a%b;
-
-const averageOfTwo=(a,b)=>(a+b)/2;
-
-const averageOfThree=(a,b,c)=>(a+b+c)/3;
-
-const distance=(a,b)=>Math.abs(a-b);
-
-const manhattanDistance=(x1,y1,x2,y2)=>distance(x1,x2)+distance(y1,y2);
-
-const euclideanDistance=(x1,y1,x2,y2)=>Math.sqrt((x1-x2)**2+(y1-y2)**2);
+const euclideanDistance = (x1, y1, x2, y2) => Math.sqrt (Math.abs(x1, x2) + (y1, y2) 
