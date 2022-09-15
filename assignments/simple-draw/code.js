b6c21b3803defaf7e0b32a62421d8a9053d5a4d5
Peter Seibel <peter@gigamonkeys.com>
2022-09-15 08:47:46 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -195,3 +195,5 @@ drawFilledCircle(width - 50, height - 50, 50, 'red');
 drawFilledRect(5, height - 20, 45, 15, 'purple');
 
 drawTriangle(width * 0.25, 10, width * 0.75, 20, width * 0.5, 80, 'blue');
+
+drawFilledTriangle(width * 0.25, hight - 10, width * 0.75, height - 20, width * 0.5, height - 80, 'blue');
