7dc4ad52e8a1a1cb2e6a19d15804289e308e9c4b
Peter Seibel <peter@gigamonkeys.com>
2022-09-15 09:09:03 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -189,4 +189,4 @@ drawCircle(55, height - 50, 30, 'blue');
 drawCircle(width - 70, height - 150, 30, 'blue');
 drawLine(55, height - 50, width - 70, height - 150, 'black');
 
-drawTriangle(width * 0.25, 150, width/2, 50, width * 0.75, 150, 'blue');
+drawTriangle(width * 0.25 + 5, 145, width/2, 55, width * 0.75 - 5, 145, 'blue');
