91b27c7eb7f8641b2f5be1d1c2e04b177b6db896
Peter Seibel <peter@gigamonkeys.com>
2022-09-16 19:32:32 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,32 +1,35 @@
-// Jack's code
+// Jaxon's code
 
 const add = (a, b) => {
   return a + b
 }
+
 const subtract = (a, b) => {
   return a - b
 }
+
 const multiply = (a, b) => {
   return a * b
 }
+
 const divide = (a, b) => {
   return a / b
 }
+
 const mod = (a, b) => {
   return a % b
 }
+
 const averageOfTwo = (a, b) => {
   return (a + b) / 2
 }
-const averageOfThree = (a, b, c) => {
-  return (a + b + c) / 3
+
+const averageOfThree = (a, b,c) => {
+  return (a + b+c) / 3
 }
-const distance = (a, b) => {
-  return Math.abs(a - b)
+const distance= (a,b) => {
+  return Math.abs(a-b)
 }
 
-const manhattanDistance = (x1, y1, x2, y2) => distance(x1, x2) + distance(y1, y2)
-
-const euclideanDistance = (x1, y1, x2, y2) => Math.sqrt (Math.abs(x1, x2) + (y1, y2){
-  return
-}
+const manhattanDistance= (x1,y1,x2,y2) => distance(x1,x2) + distance (y1,y2) 
+const euclideanDistance= (x1,y1,x2,y2) => distance(x1,x2) + distance (y1,y2) 
