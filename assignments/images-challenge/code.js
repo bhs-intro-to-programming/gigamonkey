695adee127ecdd4791c2e6465e72c80d87a8befe
Peter Seibel <peter@gigamonkeys.com>
2022-09-21 20:23:00 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -25,105 +25,108 @@
  */
 
 const drawLineCircle = (r) => {
-  for (let i = 0; r * 2 * i < width; i++) {
-    drawFilledCircle(r * 2 * i, 200, r, 'red');
+  for (let i = 0; r * 2 * i + r < width - r; i++) {
+    drawFilledCircle(r * 2 * i + r, height / 2, r, 'red');
   }
 }
 
-const drawLineCircleAlternating = (r) => {
-  for (let i = 0; r * 2 * i < width; i++) {
-    let color;
-    if (i % 2 === 1) {
-      color = 'red';
-    } else {
-      color = 'blue';
-    }
-    drawFilledCircle(r * 2 * i, 200, r, color);
+//drawLineCircle(11);
+
+
+const alterLineCircle = (r) => {
+  for (let i = 0; r * 2 * i + r < width; i++) {
+    let color = i % 2 === 0 ? 'blue' : 'red';
+    drawFilledCircle(r * 2 * i + r, 200, r, color);
   }
 }
+
+//alterLineCircle(10);
 
 
 const drawTarget = (numRing) => {
   for (let i = 0; i < numRing; i++) {
-    let color;
-    if (i % 2 === 1) {
-      color = 'red';
-    } else {
-      color = 'blue';
-    }
+    let color = i % 2 === 0 ? 'blue' : 'red';
     drawFilledCircle(width / 2, height / 2, ((numRing - i) / numRing) * (width / 2), color);
   }
 }
 
+//drawTarget(12);
+
 
 const drawCheckers = (edgeNum) => {
-  var checkWidth = Math.min(width, height);
-  var sqrWidth = checkWidth / edgeNum;
-  var color;
+  const checkWidth = Math.min(width, height);
+  const sqrWidth = checkWidth / edgeNum;
   for (let i = 0; i < edgeNum; i++) {
-    for (let j = 0; j < edgeNum + 1; j++) {
-      if (i % 2 === 1 && j % 2 === 1) {
-        color = 'red';
-      } else if (i % 2 === 1 && j % 2 === 0) {
-        color = 'blue';
-      } else if (i % 2 === 0 && j % 2 === 1) {
-        color = 'blue';
-      } else {
-        color = 'red';
-      }
+    for (let j = 0; j < edgeNum; j++) {
+      let color = (i % 2 === 1 && j % 2 === 0) || (i % 2 === 0 && j % 2 === 1) ? 'red' : 'blue';
       drawFilledRect(j * sqrWidth, i * sqrWidth, sqrWidth, sqrWidth, color);
     }
   }
 }
 
+//drawCheckers(10);
+
 
 const drawLineyCurve = (numLines) => {
-  let heightDist;
-  let widthDist;
+  const heightDist = height / numLines;
+  const widthDist = width / numLines;
   for (let i = 0; i < numLines; i++) {
-    heightDist = height / numLines;
-    widthDist = width / numLines;
     drawLine(0, heightDist * i, widthDist * i, height, 'blue', 1);
   }
 }
 
+//drawLineyCurve(150);
+
 
 const drawRectCircle = (r) => {
-  for (let i = 0; r * 2 * i < width; i++) {
-    for (let j = 0; r * 2 * j < height; j++)
-      drawCircle(r * 2 * i, r * 2 * j, r, 'red', 1);
+  const d = 2 * r
+  const numCircWid = Math.floor(width / d);
+  const numCircHei = Math.floor(height / d);
+  const startWid = (width - numCircWid * d) / 2;
+  const startHei = (height - numCircHei * d) / 2;
+  for (let i = 0; d * i < numCircWid * d; i++) {
+    for (let j = 0; d * j < numCircHei * d; j++)
+      drawCircle(d * i + startWid + r, d * j + startHei + r, r, 'red', 1);
   }
 }
 
+//drawRectCircle(20);
+
+
 const drawRanCircle = (r, prob) => {
-  for (let i = 0; r * 2 * i < width; i++) {
-    for (let j = 0; r * 2 * j < height; j++) {
-      var colorNum = Math.random();
-      if (colorNum > prob) {
-        drawCircle(r * 2 * i, r * 2 * j, r, 'red', 1);
+  const d = r * 2
+  for (let i = 0; d * i < width; i++) {
+    for (let j = 0; d * j < height; j++) {
+      if (Math.random() > prob) {
+        drawCircle(d * i, d * j, r, 'red', 1);
       } else {
-        drawFilledCircle(r * 2 * i, r * 2 * j, r, 'red');
+        drawFilledCircle(d * i, d * j, r, 'red');
       }
     }
   }
 }
 
+//drawRanCircle(10, 0.1);
 
+/*
 const drawSqrCrc = (r) => {
-  var checkLength = Math.min(width, height) - 1 * (r * 2);
-  for (let i = 0; r * 2 * i < checkLength; i++) {
-    drawCircle(r * 2 * i + r * 2, height / 2 - checkLength / 2, r, 'red', 1);
-    drawCircle(r * 2 * i + r * 2, height / 2 + checkLength / 2 - 20, r, 'red', 1);
-    drawCircle(width / 2 + checkLength / 2, r * 2 * i + 70, r, 'red', 1);
-    drawCircle(width / 2 - checkLength / 2 + 20, r * 2 * i + 70, r, 'red', 1);
+  var checkLength = Math.min(width % (r * 2), height * (r * 2)) * (r * 2);
+  for(let i = 0; r * 2 * i < checkLength; i++) {
+    drawCircle(r * 2 * i + r, height / 2 - checkLength / 2, r, 'red', 1);
+    drawCircle(r * 2 * i + r, height / 2 + checkLength / 2 - r * 2, r, 'red', 1);
+    drawCircle(r, r * 2 * i + height / 2 - checkLength / 2, r, 'red', 1);
+    drawCircle(checkLength + r, r * 2 * i + height / 2 - checkLength / 2, r, 'red', 1);
   }
 }
 
-// drawLineCircle(10);
-// drawLineCircleAlternating(10);
-// drawTarget(12);
-// drawCheckers(10);
-// drawLineyCurve(150);
-// drawRectCircle(20);
-// drawRanCircle(10, 0.0);
 drawSqrCrc(25);
+*/
+
+const drawSqrCrc = (r) => {
+  const numCirc = Math.floor(width / (r * 2));
+  for (let i = 0; numCirc > i; i++) {
+    drawCircle(r * 2 * i + r, (height - (height - numCirc * r * 2)) / 2, r, 'blue');
+  }
+}
+
+drawSqrCrc(18);
