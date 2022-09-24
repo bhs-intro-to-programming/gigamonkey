e0302f0189c660be2fc6827a23ed08970ec855cb
Peter Seibel <peter@gigamonkeys.com>
2022-09-24 16:53:10 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,12 +1,14 @@
 const z_sqr = ([x, y]) => {
   return [x ** 2 - y ** 2, 2 * x * y];
-}
+};
+
 const f = (z, c) => {
   // z_n+1 = z_n^2 + c
   const [i1, j1] = z_sqr(z);
   const [i2, j2] = c;
   return [i1 + i2, j1 + j2];
-}
+};
+
 const isPixelInSet = (c, iterations) => {
   let z = [0, 0];
   for (let i = 0; i < iterations; i++) {
@@ -16,15 +18,20 @@ const isPixelInSet = (c, iterations) => {
     }
   }
   return true;
-}
+};
+
+const cx = width / 2;
+const cy = height / 2;
+
+const coord = (gx, gy) => {
+  return [gx - cx, cy - gy];
+};
 
 const drawMandel = (iterations) => {
-  let xmath = 0
-  let ymath = 0
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       //console.log(isPixelInSet([xmath, ymath], iterations))
-      if (isPixelInSet([xmath, ymath], iterations)) {
+      if (isPixelInSet(coord(x, y), iterations)) {
         drawFilledRect(x, y, 1, 1, 'black')
       }
       xmath += 2 / width
