6610950bde6fc044f520b740cc84ea2926a0f54c
Peter Seibel <peter@gigamonkeys.com>
2022-09-26 11:27:02 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -60,4 +60,4 @@ const drawMandelbrot = (iterations, cx, cy, zoom) => {
   console.log(`Rendered in ${t / 1000} seconds.`);
 };
 
-drawMandelbrot(100, -Math.E / 7, -Math.E / 20, 1500);
+drawMandelbrot(1000, -Math.E / 7, -Math.E / 20, 1500);
