b3d438acdc336cab1bd45f207b73b96c4e0507b5
Peter Seibel <peter@gigamonkeys.com>
2022-09-27 20:55:19 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,174 +1,59 @@
+const RISE = Math.sin(60 * Math.PI / 180);
+const MAX = Math.min(width, height);
+const BOTTOM = height - (height - MAX * RISE) * 0.75;
+
 /*
- * This code is running in an environment the same as simple-draw. Thus you have
- * two variables that will be helpful.
- *
- *  width - the width of the drawing area.
- *  height - the height of the drawing area.
- *
- * And these methods which do the same thing as in simple-draw.
- *
- *  drawLine(x1, y1, x2, y2, color, lineWidth)
- *
- *  drawCircle(x, y, radius, color, lineWidth=1)
- *
- *  drawRect(x, y, w, h, color, lineWidth=1)
- *
- *  drawTriangle(x1, y1, x2, y2, x3, y3, color, lineWidth=1)
- *
- *  drawFilledCircle(x, y, r, color)
- *
- *  drawFilledRect(x, y, width, height, color)
- *
- *  drawFilledTriangle(x1, y1, x2, y2, x3, y3, color)
- *
- *  clear()
+ * Draw a filled equilateral triange pointing up with bottom-left corner at 
+ * x,y and a given side length and color.
  */
+const upTriangle = (x, y, side, color) => {
+  drawFilledTriangle(x, y, x + side / 2, y - side * RISE, x + side, y, color);
+};
 
-const lineOfCircles = (radius) => {
-  const diameter = 2 * radius
-  const dist = Math.floor(width / diameter) * diameter
-  const extra = width - dist
-  let x = 0
-  while (x < width - extra) {
-    drawFilledCircle(x + radius + extra / 2, height / 2, radius, 'red')
-    x += diameter
-  }
-}
-//lineOfCircles (23);
+/*
+ * Draw a filled equilateral triange pointing down with top-left corner at 
+ * x,y and a given side length and color.
+ */
+const downTriangle = (x, y, side, color) => {
+  drawFilledTriangle(x, y, x + side / 2, y + side * RISE, x + side, y, color);
+};
 
-const lineOfAltCircles = (radius) => {
-  const diameter = 2 * radius
-  const dist = Math.floor(width / diameter) * diameter
-  const extra = width - dist
-  let color = 'blue'
-  let x = 0
-  while (x < width - extra) {
-    if (color === 'red') {
-      color = 'blue'
-    } else {
-      color = 'red'
-    }
-    drawFilledCircle(x + radius + extra / 2, height / 2, radius, color)
-    x += diameter
+/*
+ * Cut all the holes in the triangle with its bottom-left corner at x,y and
+ * given side length. Also returns total number of triangles cut.
+ */
+const cutHoles = (x, y, side, smallest) => {
+  let t = 1;
+  cutBigHole(x, y, side);
+  if (side > smallest) {
+    t += cutSmallerHoles(x, y, side, smallest);
   }
-}
-//lineOfAltCircles (23);
+  return t;
+};
 
-const fillWithCircles = (radius) => {
-  const diameter = 2 * radius
-  const xdist = Math.floor(width / diameter) * diameter
-  const xextra = width - xdist
-  const ydist = Math.floor(height / diameter) * diameter
-  const yextra = height - ydist
-  let y = 0
-  while (y < height - yextra) {
-    let x = 0
-    while (x < width - xextra) {
-      drawCircle(x + radius + xextra / 2, y + radius + yextra / 2, radius, 'blue')
-      x += diameter
-    }
-    y += diameter
-  }
-}
-//fillWithCircles (23);
+/*
+ * Cut the big hole out of the middle of the given triangle.
+ */
+const cutBigHole = (x, y, side) => {
+  downTriangle(x + side * 0.25, y - (side / 2 * RISE), side / 2, '#ffffff');
+};
 
-const fillWithCirclesRand = (radius, probability) => {
-  const diameter = 2 * radius
-  const xdist = Math.floor(width / diameter) * diameter
-  const xextra = width - xdist
-  const ydist = Math.floor(height / diameter) * diameter
-  const yextra = height - ydist
-  let y = 0
-  while (y < height - yextra) {
-    let x = 0
-    while (x < width - xextra) {
-      let rand = Math.random()
-      if (rand > probability) {
-        drawCircle(x + radius + xextra / 2, y + radius + yextra / 2, radius, 'blue')
-      } else {
-        drawFilledCircle(x + radius + xextra / 2, y + radius + yextra / 2, radius, 'blue')
-      }
-      x += diameter
-    }
-    y += diameter
-  }
-}
-//fillWithCirclesRand (23, 0.2);
+/*
+ * Cut the holes out of the three smaller triangles left after cutting the
+ * big hole.
+ */
+const cutSmallerHoles = (x, y, side, smallest) => {
+  let t = 0;
+  t += cutHoles(x, y, side / 2, smallest);
+  t += cutHoles(x + side / 2, y, side / 2, smallest);
+  t += cutHoles(x + side / 4, y - side / 2 * RISE, side / 2, smallest);
+  return t;
+};
 
-const concentricCircles = (amountCircles) => {
-  if (height <= width) {
-    drawFilledCircle(width / 2, height / 2, height / 2, 'blue')
-    const radiuses = (height / 2) / (amountCircles)
-    let p = 0
-    let color = 'red'
-    let x = 0
-    while (x < height / 2) {
-      if (color === 'red') {
-        color = 'blue'
-      } else {
-        color = 'red'
-      }
-      drawFilledCircle(width / 2, height / 2, height / 2 - p, color)
-      p += radiuses
-    }
-  } else {
-    drawFilledCircle(width / 2, height / 2, width / 2, 'blue')
-    const radiuses = (width / 2) / (amountCircles)
-    let p = 0
-    let color = 'red'
-    let x = 0
-    while (x < width / 2) {
-      if (color === 'red') {
-        color = 'blue'
-      } else {
-        color = 'red'
-      }
-      drawFilledCircle(width / 2, height / 2, width / 2 - p, color)
-      p += radiuses
-    }
-  }
-}
-//concentricCircles(13);
+const sierpinski = (x, y, size, smallest) => {
+  upTriangle(x, y, size, 'blue');
+  const t = cutHoles(x, y, size, smallest);
+  console.log(`${t} triangles.`);
+};
 
-const checkerboard = (n) => {
-  if (height <= width) {
-    const squareSide = height / n
-    const widthextra = (width - height) / 2
-    drawFilledRect(widthextra, 0, height, height, 'red')
-    let startpos = 0
-    for (let x = 0; x < height - squareSide / (squareSide + 1); x += squareSide) {
-      for (let y = startpos; y < height; y += 2 * squareSide) {
-        drawFilledRect(x + widthextra, y, squareSide, squareSide, 'blue')
-      }
-      if (startpos == 0) {
-        startpos += squareSide
-      } else {
-        startpos -= squareSide
-      }
-    }
-  } else {
-    const squareSide = width / n;
-    const heightextra = (height - width) / 2
-    drawFilledRect(0, heightextra + 0, width, width, 'red')
-    let startpos = 0
-    for (let y = 0; y < width - squareSide / (squareSide + 1); y += squareSide) {
-      for (let x = startpos; x < width; x += 2 * squareSide) {
-        drawFilledRect(x, y + heightextra, squareSide, squareSide, 'blue')
-      }
-      if (startpos == 0) {
-        startpos += squareSide
-      } else {
-        startpos -= squareSide
-      }
-    }
-  }
-}
-//checkerboard(8);
-
-const notReallyCurved = (lines) => {
-  const draw = Math.min(width, height) / lines
-  for (let i = (height - width) / 2; i < width; i += draw) {
-    drawLine(width, height - (height - width) / 2, 0, i - draw)
-  }
-}
-//notReallyCurved (20);
+sierpinski((width % MAX) / 2, BOTTOM, MAX, 1);
