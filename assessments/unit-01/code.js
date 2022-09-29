d7a116763940c234e343a901a6330047760834d8
Peter Seibel <peter@gigamonkeys.com>
2022-09-29 13:42:38 -0700

    Creating assessments/unit-01/code.js

---
+++ assessments/unit-01/code.js
@@ -0,0 +1,8 @@
+// Write your code here. Please pay attention to the names of the functions I'm
+// asking you to write. If I ask you to write a function named foo and you call
+// it something else you will not get full credit for your answer even if
+// everything else is correct.
+
+// There are twenty functions to write so allocate your time accordingly. Don't
+// get stuck on one question for too long if there are other functions you can
+// write more easily.
