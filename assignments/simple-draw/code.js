512e2612a5e79cfcf479a6858682300e954b08e9
Peter Seibel <peter@gigamonkeys.com>
2022-09-30 09:46:34 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -61,7 +61,7 @@ const sierpinski = (x, y, size, smallest) => {
 const notReallyCurved = (lines) => {
   const gap = height / lines;
   for (let i = 0; i != lines + 1; i++) {
-    drawLine(0, height - (i * gap), height - (i * gap), height, 2)
+    drawLine(0, height - (i * gap), height - (i * gap), height, lineWidth=2)
   }
 }
 notReallyCurved(25);
