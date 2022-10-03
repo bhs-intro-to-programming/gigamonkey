eb69e26351edf69a3e48f549c38042a7566360fa
Peter Seibel <peter@gigamonkeys.com>
2022-10-03 13:52:54 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -78,7 +78,7 @@ const cutBigCarpetHole = (x, y, size) => {
 
 const embedGasket = (x, y, size) => {
   const h = RISE * size;
-  gasket(x, y - ((size - h) / 2), size, 1);
+  gasket(x, (y + size) - ((size - h) / 2), size, 1);
 };
 
 const cutSmallCarpetHoles = (x, y, size, smallest) => {
