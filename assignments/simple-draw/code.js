2953d16e6c388f4f3359a86b74ad5c08e85fd5f0
Peter Seibel <peter@gigamonkeys.com>
2022-10-03 11:59:06 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -64,7 +64,7 @@ const carpet = (x, y, size, smallest) => {
 };
 
 const cutCarpetHoles = (x, y, size, smallest) => {
-  drawFilledRect(x + size / 3, y + size / 3, size / 3, 'white');
+  drawFilledRect(x + size / 3, y + size / 3, size / 3, size / 3, 'white');
 };
 
 carpet((width - MAX_SIDE) / 2, (height - MAX_SIDE) / 2, MAX_SIDE, 1);
