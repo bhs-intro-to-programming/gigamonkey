2ace791ccf68cef4b6da9c6e9ae1781cbe09de30
Peter Seibel <peter@gigamonkeys.com>
2022-10-14 16:44:05 -0700

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -5,12 +5,27 @@ const rows = Math.floor(height / size);
 
 const fill = (r, c) => {
   drawFilledRect(c * size, r * size, size, size, 'green');
-}
+};
 
-for (let r = 0; r < rows; r++) {
-  for (let c = 0; c < columns; c++) {
-    if (Math.random() < 0.2) {
-      fill(r, c);
+const randomStart = (p) => {
+  const grid = [];
+  for (let r = 0; r < rows; r++) {
+    const row = [];
+    for (let c = 0; c < columns; c++) {
+      row.push(Math.random() < p);
+    }
+    grid.push(row);
+  }
+};
+
+const render = (grid) => {
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c]) {
+        fill(r, c);
+      }
     }
   }
 }
+
+render(randomStart(0.2));
