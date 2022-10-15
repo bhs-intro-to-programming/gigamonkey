fb7129f7f723b30731bb3a672da0493fa6cf298c
Peter Seibel <peter@gigamonkeys.com>
2022-10-15 16:58:07 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -1,9 +1,9 @@
-// You will need this function for one of the problems. It returns a random
-// number between 0, inclusive, and n, exclusive. Thus rand(3) could possibly
-// return 0, 1, or 2 but will not return 3.
-
+/*
+ * You will need this function for some of the problems. It returns a random
+ * number between 0, inclusive, and n, exclusive. Thus rand(3) could possibly
+ * return 0, 1, or 2 but will not return 3.
+ */
 const rand = (n) => Math.floor(Math.random() * n);
-
 
 // Write your code here. Remember that you get partial credit for a
 // syntactically correct function skeleton, more credit for a skeleton with a
