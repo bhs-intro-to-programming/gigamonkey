f2d9e11d6e687e548a84fe97f4933f48f527bc8e
Peter Seibel <peter@gigamonkeys.com>
2022-10-17 10:47:13 -0700

    Creating assignments/tic-tac-toe/code.js

---
+++ assignments/tic-tac-toe/code.js
@@ -0,0 +1,6 @@
+// Note the lack of a declaration (a let or const) here. This variable has been
+// defined in the framework but you can reassign it to a different function
+// value to handle mouse clicks. This is just a place holder.
+onclick = (x, y) => {
+  console.log(`Click at ${x},${y}`);
+};
