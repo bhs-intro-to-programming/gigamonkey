c2f0e95fc513a9c685f2200ed0957199c11c13b9
Peter Seibel <peter@gigamonkeys.com>
2022-10-17 11:03:56 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,6 +1,10 @@
-// Note the lack of a declaration (a let or const) here. This variable has been
-// defined in the framework but you can reassign it to a different function
-// value to handle mouse clicks. This is just a place holder.
+// This is a bit of a new thing. registerOnclick is a function provided by the
+// framework. But the argument we're passing to it is *another* function. Notice
+// how the argument here looks like what we normally put on the righthand side
+// of the equals sign in our normal `const foo = ...` function definition. This
+// is called an anonymous function. We'll discuss this in more detail in a few
+// weeks but for now you can just adapt this code.
+
 registerOnclick((x, y) => {
   console.log(`Click at ${x},${y}`);
 });
