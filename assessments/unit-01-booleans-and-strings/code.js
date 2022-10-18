64aa2fc75c165c7ec73bf3a2e6387be42417240e
Peter Seibel <peter@gigamonkeys.com>
2022-10-18 08:58:11 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -22,5 +22,5 @@ const evenGreaterThanZero = (n) => {
 };
 
 const isLeapYear = (year) => {
-  return year % 4 === 0 && (year % 100 !== 100 || year % 400 === 0);
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
