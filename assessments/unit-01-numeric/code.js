5f7f4800e18e48f5252d7c5784756dff9b094c91
Peter Seibel <peter@gigamonkeys.com>
2022-10-18 08:46:40 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -3,9 +3,44 @@ const JUPITER_GRAVITY = 24.79;
 const EARTH_GRAVITY = 9.8;
 const G = 6.6743e-11;
 
-// Write your code here. Remember that you get partial credit for a
-// syntactically correct function skeleton, more credit for a skeleton with a
-// reasonable argument list, yet more credit for a working function, and full
-// credit for a clear and simple working function.
+const itemsLeftOver = (people, items) => {
+  return items % people;
+};
 
-const foo = () => {};
+const areaOfCircle = (r) => {
+  return Math.PI * r ** 2;
+};
+
+const volumeOfCube = (s) => {
+  return s ** 3;
+};
+
+const populationGrowth = (population, growth) => {
+  return population * growth;
+};
+
+const earnedRunAverage = (earnedRuns, inningsPitched) => {
+  return (earnedRuns / inningsPitched) * 9;
+};
+
+const valueOfJewels = (diamonds, diamondsPrice, emeralds, emeraldsPrice) => {
+  return diamonds * diamondsPrice + emeralds * emeraldsPrice;
+};
+
+const payWithOvertime = (hours, hourlyRate, overtimeRate) => {
+  const regular = Math.min(hours, 8);
+  const overtime = hours - regular;
+  return regular * hourlyRate + overtime * overtimeRate;
+};
+
+const firstClassPostage = (oz) => {
+  return 60 + Math.ceil(oz - 1) * 24; 
+};
+
+const weightOnJupiter = (kgOnEarth) => {
+  kgOnEarth * (JUPITER_GRAVITY / EARTH_GRAVITY);
+};
+
+const gravity = (m1, m2, distance) => {
+  return G * (m1 * m2) / distance ** 2;
+};
