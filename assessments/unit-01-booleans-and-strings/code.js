7a17f0e265ae34c2a972e3fb4702e49166b599c1
Peter Seibel <peter@gigamonkeys.com>
2022-10-18 08:57:52 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -5,8 +5,22 @@
  */
 const rand = (n) => Math.floor(Math.random() * n);
 
-// Write your code here. Remember that you get partial credit for a
-// syntactically correct function skeleton, more credit for a skeleton with a
-// reasonable argument list, yet more credit for a working function, and full
-// credit for a clear and simple working function.
+const fireAlarm = (pulled, smoke, drill) => {
+  return pulled || smoke || drill;
+};
 
+const canBePresident = (age, naturalBorn, yearsInUS) => {
+  return age >= 35 && naturalBorn && yearsInUS >= 14;
+};
+
+const willSeeTweet = (followsTweeter, followsRetweeter, tweeterBlocked) => {
+  return (followsTweeter || followsRetweeter) && !tweeterBlocked;
+};
+
+const evenGreaterThanZero = (n) => {
+  return n > 0 && n % 2 === 0;
+};
+
+const isLeapYear = (year) => {
+  return year % 4 === 0 && (year % 100 !== 100 || year % 400 === 0);
+};
