0aa891e4d2f17df17770a2856a1efc2ba2eeebc3
Peter Seibel <peter@gigamonkeys.com>
2022-10-23 17:23:29 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -4,6 +4,6 @@ const sum = (ns) => ns.reduce((acc, n) => acc + n, 0);
 
 const evens = (ns) => ns.filter((n) => n % 2 === 0);
 
-const anyOverOneHundred = ns.some(n => n > 100);
+const anyOverOneHundred = (ns) => ns.some(n => n > 100);
 
 const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i+1).fill(i+1));
