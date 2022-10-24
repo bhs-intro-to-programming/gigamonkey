0faa19a004168031e89a81d087c378c7928bda09
Peter Seibel <peter@gigamonkeys.com>
2022-10-23 17:22:37 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -2,5 +2,6 @@ const countTens = (ns) => ns.reduce((acc, n) => acc + (n === 10 ? 1 : 0), 0);
 
 const sum = (ns) => ns.reduce((acc, n) => acc + n, 0);
 
+const evens = (ns) => ns.filter((n) => n % 2 === 0);
 
 const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i+1).fill(i+1));
