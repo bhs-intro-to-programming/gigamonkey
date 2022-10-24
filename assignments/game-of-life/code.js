612564d64abee3f98709ec1f28febd823624cb30
Peter Seibel <peter@gigamonkeys.com>
2022-10-24 14:13:40 -0700

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -150,8 +150,8 @@ const blinker = (r, c) => {
 const multiBlinker = (r, c) => {
   addPattern(grid, r, c, BLINKER);
   addPattern(grid, r, c + 6, BLINKER);
-  addPattern(grid, r - 5, c + 4, BLINKER2);
-  addPattern(grid, r + 3, c + 4, BLINKER2);
+  addPattern(grid, r - 4, c + 4, BLINKER2);
+  addPattern(grid, r + 2, c + 4, BLINKER2);
 };
 
 const block = (r, c) => {
