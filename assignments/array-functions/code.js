44d08d1f4a4670477c9cee7722c57d8c90ff7455
Peter Seibel <peter@gigamonkeys.com>
2022-10-23 17:22:11 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,4 +1,6 @@
 const countTens = (ns) => ns.reduce((acc, n) => acc + (n === 10 ? 1 : 0), 0);
 
+const sum = (ns) => ns.reduce((acc, n) => acc + n, 0);
+
 
 const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i+1).fill(i+1));
