efc57bd994aa3f5b4720b3754078f29ba2fd322f
Peter Seibel <peter@gigamonkeys.com>
2022-10-23 17:30:54 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,12 +1,3 @@
-const numbers = (start, end, step = 1) => {
-  if (end === undefined) {
-    end = start;
-    start = 0;
-  }
-  const len = Math.ceil((end - start) / step);
-  return Array(len).fill().map((_, i) => start + (i * step));
-};
-
 const countTens = (ns) => ns.reduce((acc, n) => acc + (n === 10 ? 1 : 0), 0);
 
 const sum = (ns) => ns.reduce((acc, n) => acc + n, 0);
@@ -15,4 +6,4 @@ const evens = (ns) => ns.filter(n => n % 2 === 0);
 
 const anyOverOneHundred = (ns) => ns.some(n => n > 100);
 
-const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i+1).fill(i+1));
+const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i + 1).fill(i + 1));
