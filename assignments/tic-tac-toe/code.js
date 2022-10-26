f0b6d7f1594e0c9cd9240ac23bb0ecfeeb36120f
Peter Seibel <peter@gigamonkeys.com>
2022-10-25 19:49:52 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -20,7 +20,7 @@ const diags = [[0, 4, 8], [6, 4, 2]];
 
 const lines = [...rows, ...cols, ...diags];
 
-const boardSize = Math.min(width, height) - 20;
+const boardSize = Math.min(width, height) - 50;
 const boxSize = boardSize / 3;
 const boardX = (width - boardSize) / 2
 const boardY = (height - boardSize) / 2;
