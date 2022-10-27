cc6e087ce6d439e45985982333f3ad8c0b420c36
Peter Seibel <peter@gigamonkeys.com>
2022-10-27 08:43:31 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,3 +1,32 @@
 const findFnord = (s) => s.indexOf('fnord');
 
 const stringContains = (s, ss) => s.indexOf(ss) > -1;
+
+const firstAndLast = (s) => {
+  return s[0] + s[s.length - 1];
+};
+
+const swapFrontAndBack = (s) => {
+  return s.substring(s.length / 2) + s.substring(0, s.length / 2);
+};
+
+const simplePigLatin = (s, firstVowel) => {
+  return s.substring(firstVowel) + s.substring(0, firstVowel) + 'ay';
+};
+
+const randomCharacter = (s) => {
+  return s[rand(s.length)];
+};
+
+const randomCharacterUpDown = (s) => {
+  const i = rand(s.length);
+  return s[i].toUpperCase() + s[i].toLowerCase();
+};
+
+const isAllUpperCase = (s) => {
+  return s === s.toUpperCase();
+};
+
+const sameIgnoringCase = (s1, s2) => {
+  return s1.toLowerCase() === s2.toLowerCase();
+};
