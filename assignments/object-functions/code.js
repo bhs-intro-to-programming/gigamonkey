478cc4f463bd3598c46e4e6768b160faef0df746
Peter Seibel <peter@gigamonkeys.com>
2022-11-06 12:17:58 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -3,3 +3,7 @@ const getX = (o) => o.x;
 const point = (x, y) => { return { x: x, y: y }; };
 
 const emptyObject = () => ({});
+
+const distance = (p1, p2) => {
+  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
+}
