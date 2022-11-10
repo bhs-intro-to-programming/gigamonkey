388213f603a62874a761d9e12009f3cb6d1c053c
Peter Seibel <peter@gigamonkeys.com>
2022-11-10 15:48:36 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -13,3 +13,10 @@ const emptyObject = () => {
 const distance = (p1, p2) => {
   return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 }
+
+const midpoint = (p1, p2) => {
+  return { 
+    x: 0,
+    y: 0
+  }
+}
