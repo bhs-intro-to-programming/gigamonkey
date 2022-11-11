748d06466a3ea2792c0b8b81c4361ddb303886d6
Peter Seibel <peter@gigamonkeys.com>
2022-11-11 10:24:27 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -1,16 +1,55 @@
-const WHITE_KING   = '♔';
-const WHITE_QUEEN  = '♕';
-const WHITE_ROOK   = '♖';
+const WHITE_KING = '♔';
+const WHITE_QUEEN = '♕';
+const WHITE_ROOK = '♖';
 const WHITE_BISHOP = '♗';
 const WHITE_KNIGHT = '♘';
-const WHITE_PAWN   = '♙';
-const BLACK_KING   = '♚';
-const BLACK_QUEEN  = '♛';
-const BLACK_ROOK   = '♜';
+const WHITE_PAWN = '♙';
+const BLACK_KING = '♚';
+const BLACK_QUEEN = '♛';
+const BLACK_ROOK = '♜';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
-const BLACK_PAWN   = '♟';
+const BLACK_PAWN = '♟';
 
-registerOnclick((x, y) => { 
-  drawText(WHITE_KING, x, y, 'black', Math.min(width, height) / 8);
-});
+// Example of drawing one of the pieces
+//drawText(WHITE_KING, width/2, height/2, 'black', 64);
+
+const drawBoard = () => {
+  for (let i = 0; i < 8; i++) {
+    drawLine(width / 8 * i, 0, width / 8 * i, height, 'black', 2)
+  }
+  for (let j = 0; j < 8; j++) {
+    drawLine(0, height / 8 * j, width, height / 8 * j, 'black', 2)
+  }
+}
+drawBoard()
+
+let board =
+  [[WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_KING, WHITE_QUEEN, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK],
+  [WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  [BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN],
+  [BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK]]
+//let player = 'w'
+
+const row = (y) => Math.floor(y / (height / 8))
+const column = (x) => Math.floor(x / (width / 8))
+
+const select = (x, y) => {
+  drawText(board[row(y)][column(x)], width / 8 * column(x), height / 8 * (row(y) + 1), 'red', 45)
+}
+
+registerOnclick(select);
+
+const drawArray = () => {
+  for (let j = 0; j < board.length; j++)
+    for (let i = 0; i < 8; i++) {
+      if (board[j][i] !== '') {
+        drawText(board[j][i], width / 8 * (i), height / 8 * (j + 1), 'black', 40);
+      }
+    }
+}
+drawArray()
