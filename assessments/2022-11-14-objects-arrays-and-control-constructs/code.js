dfa4e295a5d7c094bd90fc2223af2a2d9ba1f93f
Peter Seibel <peter@gigamonkeys.com>
2022-11-15 20:36:08 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,15 +2,28 @@
 // should save you some time.
 
 const area = (rect) => {
+  return rect.width * rect.height;
 };
 
 const higherPaid = (e1, e2) => {
+  if (e1.salary > e2.salary) {
+    return e1;
+  } else {
+    return e2;
+  }
 };
 
 const isSamePoint = (p1, p2) => {
+  return p1.x === p2.x && p1.y === p2.y;
 };
 
 const totalWithTip = (bill, tipPercentage) => {
+  const tip = bill.subtotal * tipPercentage;
+  return {
+    subtotal: bill.subtotal,
+    tip: tip,
+    total: bill.subtotal + tip,
+  };
 };
 
 const isWinner = (player) => {
