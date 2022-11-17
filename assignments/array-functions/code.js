50679170819db6cb0483fb156d58b47bc9ff37e9
Peter Seibel <peter@gigamonkeys.com>
2022-11-16 20:43:28 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,9 +1,9 @@
-const countTens = (ns) => ns.reduce((acc, n) => acc + (n === 10 ? 1 : 0), 0);
-
-const sum = (ns) => ns.reduce((acc, n) => acc + n, 0);
-
-const evens = (ns) => ns.filter(n => n % 2 === 0);
-
-const anyOverOneHundred = (ns) => ns.some(n => n > 100);
-
-const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i + 1).fill(i + 1));
+const countTens = (ns) => {
+  let count = 0;
+  for (let i = 0; i < ns.length; i++) {
+    if (ns[i] === 10) {
+      count++;
+    }
+  }
+  return count;
+};
