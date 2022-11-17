9944309f1dae86047451d9c4145178720335916b
Peter Seibel <peter@gigamonkeys.com>
2022-11-16 20:51:57 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -1,33 +1,18 @@
-const getX = (obj) => {
-  return obj.x;
-}
+const getX = (obj) => obj.x;
 
-const point = (x, y) => {
-  return { x: x, y: y }
-}
+const point = (x, y) => ({x, y});
 
-const emptyObject = () => {
-  return {}
-}
+const emptyObject = () => ({});
 
-const distance = (p1, p2) => {
-  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
-}
+const distance = (p1, p2) => Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 
-const midpoint = (p1, p2) => {
-  return {
+const midpoint = (p1, p2) => ({
     x: (p1.x + p2.x) / 2,
     y: (p1.y + p2.y) / 2
   }
-}
+});
 
-const sumSalaries = (objs) => {
-  let sum = 0
-  for (let i = 0; i < objs.length; i++) {
-    sum = sum + objs[i].salary;
-  }
-  return sum;
-}
+const sumSalaries = (objs) => objs.reduce((acc, o) => acc + o.salary, 0);
 
 const newHighScore = (currentHighScore, players) => {
   let high = currentHighScore
