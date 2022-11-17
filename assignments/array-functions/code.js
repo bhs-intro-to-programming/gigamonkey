15bd7c51d2eba394e77d5408597cf9b476dbed48
Peter Seibel <peter@gigamonkeys.com>
2022-11-16 20:41:55 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -6,14 +6,4 @@ const evens = (ns) => ns.filter(n => n % 2 === 0);
 
 const anyOverOneHundred = (ns) => ns.some(n => n > 100);
 
-const xpyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i + 1).fill(i + 1));
-
-
-const pyramid = (int) => {
- const array = [];
- for (let pos = 1; pos <= int; pos++){
-   for (let ipos = 1; ipos <= pos; ipos++){
-   array.push(pos)
- }}
- return array
-}
+const pyramid = (n) => Array(n).fill().flatMap((_, i) => Array(i + 1).fill(i + 1));
