ea1335872003ff840c1e61a10d76e85602e1329c
Peter Seibel <peter@gigamonkeys.com>
2022-11-16 20:34:05 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -14,6 +14,6 @@ const averageOfThree = (a, b, c) => (a + b + c) / 3;
 
 const distance = (a, b) => Math.abs(a - b);
 
-const manhattanDistance = (x1, y1, x2, y2 => Math.abs(x1 - x2) + Math.abs(y1 - y2);
+const manhattanDistance = (x1, y1, x2, y2) => Math.abs(x1 - x2) + Math.abs(y1 - y2);
 
 const euclideanDistance = (x1, y1, x2, y2) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
