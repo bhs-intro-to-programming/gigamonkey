9f73db9d01409acf51cd6cdc8a5b1bd01d8b3390
Peter Seibel <peter@gigamonkeys.com>
2022-11-20 09:51:48 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -1,18 +1,17 @@
 const edgeSize = (width - height) / 2
 const b = Array(9).fill().map(() => Array(9).fill().map(() => Array(1).fill('')))
 let selected = 1;
+let indilen = 0
+let index = 1
 
 const drawBoard1st = () => {
   for (let i = 0; i < 10; i++) {
     const thick = i % 3 === 0 ? 3 : 1
     drawLine(edgeSize, height / 9 * i, width - edgeSize, height / 9 * i, 'black', thick)
     drawLine(edgeSize + height / 9 * i, 0, edgeSize + height / 9 * i, height, 'black', thick)
-  }
-}
-
-const drawNumberMenu = () => {
-  for (let i = 0; i < 9; i++) {
-    drawText(i + 1, edgeSize / 10 * i, 20, 'black', 20)
+    if (i !== 9) {
+      drawText(i + 1, edgeSize / 10 * i, 20, 'black', 20)
+    }
   }
 }
 
@@ -23,40 +22,44 @@ registerOnclick((x, y) => {
     const col = Math.floor((x - edgeSize) / (height / 9))
     const row = Math.floor(y / (height / 9))
     b[row][col][0] = selected
+    b[row][col][selected] = selected
     drawText(selected, edgeSize + col * (height / 9) + height / 64, row * (height / 9) + height * 6 / 64, 'black', height / 9)
     for (let i = 0; i < 9; i++) {
-      b[row][i].push(selected)
-      b[i][col].push(selected)
+      b[row][i][selected] = selected
+      b[i][col][selected] = selected
+    }
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        b[Math.floor(row / 3) * 3 + j][Math.floor(col / 3) * 3 + i][selected] = selected
+      }
     }
   }
 })
 
 drawBoard1st()
-drawNumberMenu()
 
 const lineNine = () => {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      if (b[j][i].length === 9 && b[j][i][0] === '') {
-        for (let p = 1; p < 10; p++) {
-          if (b[j][i].indexOf(p) === -1) {
-            drawText(p, edgeSize + i * (height / 9) + height / 64, j * (height / 9) + height * 6 / 64, 'gray', height / 9)
-            b[j][i][0] = p
-            for (let i = 0; i < 9; i++) {
-              b[row][i].push(selected)
-              b[i][col].push(selected)
-            }
+      for (let c = 1; c < 10; c++) {
+        if (b[j][i][c] === c) {
+          indilen++
+        } else index = c
+      }
+      if (indilen === 8 && b[j][i][0] === '') {
+        drawText(index, edgeSize + i * (height / 9) + height / 64, j * (height / 9) + height * 6 / 64, 'gray', height / 9)
+        b[j][i][0] = index
+        for (let l = 0; l < 9; l++) {
+          b[j][l][index] = index
+          b[l][i][index] = index
+        }
+        for (let a = 0; a < 3; a++) {
+          for (let o = 0; o < 3; o++) {
+            b[Math.floor(j / 3) * 3 + o][Math.floor(i / 3) * 3 + a][index] = index
           }
         }
       }
-    }
-  }
-}
-
-squareNine = () => {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      //you get the point future me 0_0
+      indilen = 0
     }
   }
 }
