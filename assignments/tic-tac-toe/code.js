c61f405d05052fd5b0d4019af005c8be1c7de1ab
Peter Seibel <peter@gigamonkeys.com>
2022-11-23 10:10:57 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,50 +1,45 @@
 let move = 0;
 
 registerOnclick((x, y) => {
+  let marker;
+  let color;
+
   if (move % 2 === 0) {
-      if (x < width/3 && y < height/3){
-        drawText('X', 75,  40, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 &&  y < height * 1/3){
-         drawText('X', 280,  40, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 &&  y < height * 1/3){
-         drawText('X', 490,  40, 'red', Math.min(width, height) * 0.3);
-      }else if(x < width/3 && y<height*2/3 && y>height*1/3){
-          drawText('X', 75,  90, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width/3 && x<width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('X', 280,  90, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('X', 490,  90, 'red', Math.min(width, height) * 0.3);
-      }else if(x < width * 1/3 && y>height*2/3){
-          drawText('X', 75,  140, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 && y>height*2/3){
-          drawText('X', 280,  140, 'red', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y>height*2/3){
-          drawText('X', 490,  140, 'red', Math.min(width, height) * 0.3);
-      }
-  }else{
-      if (x < width/3 && y < height/3){
-        drawText('O', 75,  40, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 &&  y < height * 1/3){
-         drawText('O', 280,  40, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 &&  y < height * 1/3){
-         drawText('O', 490,  40, 'blue', Math.min(width, height) * 0.3);
-      }else if(x < width/3 && y<height*2/3 && y>height*1/3){
-          drawText('O', 75,  90, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width/3 && x<width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('O', 280,  90, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y<height*2/3 && y>height*1/3){
-          drawText('O', 490,  90, 'blue', Math.min(width, height) * 0.3);
-      }else if(x < width * 1/3 && y>height*2/3){
-          drawText('O', 75,  140, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 1/3 && x < width * 2/3 && y>height*2/3){
-          drawText('O', 280,  140, 'blue', Math.min(width, height) * 0.3);
-      }else if(x > width * 2/3 && y>height*2/3){
-          drawText('O', 490,  140, 'blue', Math.min(width, height) * 0.3);
-      }
-      
+    marker = 'X';
+    color = 'red';
+  } else {
+    marker = 'O';
+    color = 'blue';
   }
-  
-  move++;
+
+  if (x < width / 3) {
+    if (y < height / 3) {
+      drawText(marker, 75, 40, color, Math.min(width, height) * 0.3);
+    } else if (y < height * 2 / 3 && y > height * 1 / 3) {
+      drawText(marker, 75, 90, color, Math.min(width, height) * 0.3);
+    } else if (y > height * 2 / 3) {
+      drawText(marker, 75, 140, color, Math.min(width, height) * 0.3);
+    }
+  } else if (x > width / 3 && x < width * 2 / 3) {
+    if (y < height * 1 / 3) {
+      drawText(marker, 280, 40, color, Math.min(width, height) * 0.3);
+    } else if (y < height * 2 / 3 && y > height * 1 / 3) {
+      drawText(marker, 280, 90, color, Math.min(width, height) * 0.3);
+    } else if (y > height * 2 / 3) {
+      drawText(marker, 280, 140, color, Math.min(width, height) * 0.3);
+    }
+  } else if (x > width * 2 / 3) {
+    if (y < height * 1 / 3) {
+      drawText(marker, 490, 40, color, Math.min(width, height) * 0.3);
+    } else if (y < height * 2 / 3 && y > height * 1 / 3) {
+      drawText(marker, 490, 90, color, Math.min(width, height) * 0.3);
+    } else if (y > height * 2 / 3) {
+      drawText(marker, 490, 140, color, Math.min(width, height) * 0.3);
+    }
+  }
+}
+
+move++;
 });
 const board = () => {
   for (let i = 0; i < 2; i++) {
