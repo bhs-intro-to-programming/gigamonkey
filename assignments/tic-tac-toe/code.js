5bf1cde47d881376d5d0d5e2d963578e7225d07a
Peter Seibel <peter@gigamonkeys.com>
2022-11-23 10:28:43 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -16,7 +16,7 @@ registerOnclick((x, y) => {
   const xPos = (col * width / 3) + width / 9;
 
   const row = 1 + Math.floor(y / (height / 3));
-  const yPos = (row * height / 3) - (height / 9);
+  const yPos = (row * height / 3) - (height / 9) + (height / 18);
 
   drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
 
