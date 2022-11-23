114be17837d1b50cdfb908a8b9466951ea4decaa
Peter Seibel <peter@gigamonkeys.com>
2022-11-23 13:39:47 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,24 +1,17 @@
 let move = 0;
+let fontSize = Math.min(width, height) * 0.3);
 
 registerOnclick((x, y) => {
-  let marker;
-  let color;
 
-  if (move % 2 === 0) {
-    marker = 'X';
-    color = 'red';
-  } else {
-    marker = 'O';
-    color = 'blue';
-  }
-
+  const [ marker, color ] = move % 2 === 0 ? [ 'X', 'red' ] : [ 'O', 'blue' ]
+  
+  const row = Math.floor(y / (height / 3));
   const col = Math.floor(x / (width / 3));
-  const xPos = (col * width / 3) + width / 9;
 
-  const row = 1 + Math.floor(y / (height / 3));
-  const yPos = (row * height / 3) - (height / 9) + (height / 18);
+  const xPos = col * width / 3 + width / 9;
+  const yPos = row * height / 3 - height / 9 + height / 18;
 
-  drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
+  drawText(marker, xPos, yPos, color, fontSize);
 
   move++;
 });
@@ -29,8 +22,8 @@ const board = () => {
     drawLine(x, 0, x, height, 'black', 5);
   }
   for (let i = 0; i < 2; i++) {
-    const x = (i + 1) * height / 3
-    drawLine(0, x, width, x, 'black', 5);
+    const y = (i + 1) * height / 3
+    drawLine(0, y, width, y, 'black', 5);
   }
 
 }
