52b44413ab9917ae865b49ff97d3c33e620d05e0
Peter Seibel <peter@gigamonkeys.com>
2022-11-28 17:22:58 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,7 +4,7 @@ const boardX = (width - boardSize) / 2;
 
 const drawBoard = () => {
   const x1 = boardX + boardSize / 3;
-  const x2 = boardX + boardSize * 2 / 3;
+  const x2 = boardX + boardSize * (2 / 3);
   drawLine(x1, 0, x1, boardSize, 'grey');
   drawLine(x2, 0, x2, boardSize, 'grey');
 };
