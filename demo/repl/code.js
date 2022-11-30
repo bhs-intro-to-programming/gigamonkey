0c648dcca053c19a4e118531b4d059cbed4e2c53
Peter Seibel <peter@gigamonkeys.com>
2022-11-30 14:53:22 -0800

    Updating demo/repl/code.js

--- demo/repl/code.js
+++ demo/repl/code.js
@@ -6,9 +6,10 @@
 // below 1000.
 
 const euler1 = (n) => {
+  let sum = 0;
   for (let i = 1; i < n; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-      console.log(i);
+      sum += i;
     }
   }
 }
