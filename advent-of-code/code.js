a9f034fc53da64541c38cb6635e3c612623ff12a
Peter Seibel <peter@gigamonkeys.com>
2022-12-01 12:16:33 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -1,13 +1,16 @@
-// Write your code here. To run a function on a given test file you need to
-// create a file in your github repo in the advent-of-code branch and in the
-// advent-of-code directory. Then you can use the
-
+// Set this variable to your Github handle.
 const USER = 'gigamonkey';
+
+// Then write your code here. To run a function on a given test file you need to
+// create a file in your github repo in the advent-of-code branch and in the
+// advent-of-code directory. Then you can use the the run function 
+
+const lines = (s) => s.split('\n');
 
 const day01Part1 = (s) => {
   let elf = 0;
   let max = 0;
-  for (const line of s.split('\n')) {
+  for (const line of lines(s)) {
     if (line === '') {
       max = Math.max(max, elf);
       elf = 0;
@@ -18,4 +21,21 @@ const day01Part1 = (s) => {
   return Math.max(max, elf);
 };
 
-run('day_01.problem', day01Part1); // 74394
+const day01Part2 = (s) => {
+  const elves = [];
+  let elf = 0;
+  for (const line of lines(s)) {
+    if (line === '') {
+      elves.push(elf);
+    } else {
+      elf += Number.parseInt(line, 10);
+    }
+  }
+  elves.push(elf);
+  elves.sort();
+  return elves.slice(0, 3).reduce((acc, n) => acc + n, 0);
+}
+
+
+//run('day_01.problem', day01Part1); // 74394
+run('day_01.problem', day01Part2); // 74394
