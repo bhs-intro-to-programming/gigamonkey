9eb145603e3ca88d6d4954e818e6d3f1a5ba1b36
Peter Seibel <peter@gigamonkeys.com>
2022-12-01 11:57:31 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -1,9 +1,8 @@
-
 // Write your code here. To run a function on a given test file you need to
 // create a file in your github repo in the advent-of-code branch and in the
 // advent-of-code directory. Then you can use the
 
-const USER = 'gigamonkey';
+const USER = 'xgigamonkey';
 
 const day01Part1 = (s) => {
   let elf = 0;
