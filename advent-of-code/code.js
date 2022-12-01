a8c1d0d55694e9f7c974869e04a69f5b2c437ecf
Peter Seibel <peter@gigamonkeys.com>
2022-12-01 10:35:11 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -1,5 +1,8 @@
 const url = "https://github.com/bhs-intro-to-programming/gigamonkey/blob/main/advent-of-code/code.js";
 
+const foo = async () => {
 const d = await fetch(url);
-
 console.log(d);
+}
+
+foo();
