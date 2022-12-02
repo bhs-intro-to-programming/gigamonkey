5bd4f02bee82ff05bf0fba5a65dde26e395282bd
Peter Seibel <peter@gigamonkeys.com>
2022-12-02 07:33:33 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -61,7 +61,7 @@ const day2 = () => {
 
   const part1 = (s) => {
     console.log(s);
-    const total = 0;
+    let total = 0;
     for (const round of lines(s)) {
       total += score('ABC'.indexOf(round[0]), 'XYZ'.indexOf(round[2]));
     }
