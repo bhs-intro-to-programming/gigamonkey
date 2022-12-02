e7d90f4c61dc8760059afccc2844a2e384929eb4
Peter Seibel <peter@gigamonkeys.com>
2022-12-02 09:03:14 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -74,5 +74,5 @@ const day2 = () => {
 
 run('day_01.problem', day1().part1); // 74394
 run('day_01.problem', day1().part2); // 212836
-// run('day_02.problem', day2().part1); // 9241
-// run('day_02.problem', day2().part2); // 14610
+run('day_02.problem', day2().part1); // 9241
+run('day_02.problem', day2().part2); // 14610
