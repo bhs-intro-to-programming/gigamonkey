35f1623de397e120f9a3b23740b198cf17998d01
Peter Seibel <peter@gigamonkeys.com>
2022-12-04 11:23:49 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,30 +1,19 @@
-// for me:  *  drawLine(x1, y1, x2, y2, color, lineWidth)
 const max = Math.max(width, height)
 const min = Math.min(width, height)
+const leftX = max / 2 - min / 2;
+const rightX = max / 2 + min / 2;
 
 const drawBoard = (borders) => {
   const start = borders ? 0 : 1;
   const end = borders ? 3 : 2;
-  const leftX = max / 2 - min / 2;
 
   for (let i = start; i <= end; i++) {
-    const verticalLineX = leftX + min * (i / 3);
+    const verticalX = leftX + min * (i / 3);
     const horizontalY = height * (i / 3);
-    drawLine(verticalLineX, height, verticalLineX, 0, 'grey', 2)
-    drawLine(leftX, horizontalY, leftX + min, horizontalY, 'gray', 2)
+    drawLine(verticalX, height, verticalX, 0, 'grey', 2)
+    drawLine(leftX, horizontalY, rightX, horizontalY, 'gray', 2)
   }
-}
-
-drawBoard(false) //set 1 for borders and 0 for no borders
-
-const coordArray = [
-  [[''], [''], ['']],
-  [[''], [''], ['']],
-  [[''], [''], ['']],
-]
-
-const player1 = 'X' //will go first
-const player2 = 'O'
+};
 
 const gameWin = (winner) => {
   if (winner.winType == 'h') {
@@ -34,11 +23,10 @@ const gameWin = (winner) => {
   } else if (winner.winType == 'd') {
     return drawLine(width / 2 - height / (winner.winLoc == 1 ? -2 : 2), 0, width / 2 + height / (winner.winLoc == 0 ? 2 : -2), height, 'gray', 8)
   }
-}
-
-let winner = {}
+};
 
 const winSearch = (player) => {
+  let winner = {};
   for (let c = 0; c <= 2; c++) {
     if ((coordArray[c][0] == player) && (coordArray[c][1] == player) && (coordArray[c][2] == player)) winner = { winner: player, winType: 'h', winLoc: c };
     if ((coordArray[0][c] == player) && (coordArray[1][c] == player) && (coordArray[2][c] == player)) winner = { winner: player, winType: 'v', winLoc: c };
@@ -47,29 +35,23 @@ const winSearch = (player) => {
   if ((coordArray[2][0] == player) && (coordArray[1][1] == player) && (coordArray[0][2] == player)) winner = { winner: player, winType: 'd', winLoc: 1 };
   gameWin(winner)
   return winner.winner
-}
-
-let turns = 0;
-let player = player1;
+};
 
 registerOnclick((x, y) => {
   let xPos;
   let yPos;
-  if (y < 1 / 3 * height) {
-    yPos = 0
-  } else if (y < 2 / 3 * height) {
-    yPos = 1
-  } else if (y < height) {
-    yPos = 2
-  }
-  if (x < leftX || x > max / 2 + min / 2) {
+
+  const yPos = Math.floor(y / (height / 3));
+  const xPos = Math.floor((x - leftX) / (min / 3));
+
+  if (x < leftX || x > rightX) {
     drawText(x > leftX ? '⇦' : '⇨', x - height / 12, y + height / 12, 'black', height / 3)
   } else {
     if (x < max / 2 - min / 6) {
       xPos = 0
     } else if (x < max / 2 + min / 6) {
       xPos = 1
-    } else if (x < max / 2 + min / 2) {
+    } else if (x < rightX) {
       xPos = 2
     }
     if (coordArray[yPos][xPos] == '' && winSearch(player) == undefined) {
@@ -83,3 +65,17 @@ registerOnclick((x, y) => {
   }
 });
 
+
+const player1 = 'X' //will go first
+const player2 = 'O'
+
+const coordArray = [
+  [[''], [''], ['']],
+  [[''], [''], ['']],
+  [[''], [''], ['']],
+];
+
+let turns = 0;
+let player = player1;
+
+drawBoard(false);
