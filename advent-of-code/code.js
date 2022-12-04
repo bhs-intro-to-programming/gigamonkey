aea8deab7759d8d01441494dcc06f913d7c42dd4
Peter Seibel <peter@gigamonkeys.com>
2022-12-03 22:00:31 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -128,7 +128,7 @@ const day4 = () => {
 };
 
 // N.B. These won't necessarily output in order due to async fetch.
-if (true) {
+if (false) {
   run('day_01.problem', day1().part1, 74394);
   run('day_01.problem', day1().part2, 212836);
   run('day_02.problem', day2().part1, 9241);
