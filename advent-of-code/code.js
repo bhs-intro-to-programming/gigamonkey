781222bb709f4544cf2be0c4252500170a692365
Peter Seibel <peter@gigamonkeys.com>
2022-12-03 21:59:30 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -110,19 +110,18 @@ const day4 = () => {
 
   const parsePair = (line) => line.split(',').map((x) => x.split('-').map(Number));
 
+  const pairs = (s) => lines.map(parsePair);
+
   const subset = (a, b) => a[0] >= b[0] && a[1] <= b[1];
 
   const disjoint = (a, b) => a[1] < b[0] || b[1] < a[0];
 
   const part1 = (s) => 
-    lines(s)
-    .map(parsePair)
+    pairs(s)
     .reduce((acc, [a,b]) => acc + (subset(a, b) || subset(b, a) ? 1 : 0), 0);
 
   const part2 = (s) => 
-    lines(s)
-    .map(parsePair)
-    .reduce((acc, [a, b]) => acc + (!disjoint(a, b) ? 1 : 0), 0);
+    pairs(s).reduce((acc, [a, b]) => acc + (!disjoint(a, b) ? 1 : 0), 0);
 
   return { part1, part2 };
 
