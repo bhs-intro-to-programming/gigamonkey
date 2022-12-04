519056ee70a2923a8e0f517dc5a9d8564c7f3d57
Peter Seibel <peter@gigamonkeys.com>
2022-12-04 10:51:29 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,139 +1,67 @@
-const boardSize = Math.min(width, height) * 0.75;
-const boardLeft = (width - boardSize) / 2;
-const boardTop = (height - boardSize) / 2;
-const cellSize = boardSize / 3;
-const fontSize = boardSize / 3;
-const lineEndAdjustment = cellSize * 0.7;
-
-let move = 0;
-
-const board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-];
-
-const lines = [
-  // Rows
-  [[0, 0], [0, 1], [0, 2]],
-  [[1, 0], [1, 1], [1, 2]],
-  [[2, 0], [2, 1], [2, 2]],
-
-  // Cols
-  [[0, 0], [1, 0], [2, 0]],
-  [[0, 1], [1, 1], [2, 1]],
-  [[0, 2], [1, 2], [2, 2]],
-
-  // Diagonals
-  [[0, 0], [1, 1], [2, 2]],
-  [[2, 0], [1, 1], [0, 2]],
-];
-
-
-const drawBoard = () => {
-  const x1 = boardLeft + cellSize;
-  const x2 = boardLeft + 2 * cellSize;
-  const y1 = boardTop + cellSize;
-  const y2 = boardTop + 2 * cellSize;;
-  drawLine(x1, boardTop, x1, boardTop + boardSize, 'grey', 2);
-  drawLine(x2, boardTop, x2, boardTop + boardSize, 'grey', 2);
-  drawLine(boardLeft, y1, boardLeft + boardSize, y1, 'grey', 2);
-  drawLine(boardLeft, y2, boardLeft + boardSize, y2, 'grey', 2);
-};
-
-const drawMarker = (marker, r, c) => {
-  const [x, y] = cellCenter(r, c);
-  const nudge = marker === 'O' ? cellSize / 9 : cellSize / 19;
-  drawText(marker, x - (fontSize * 0.3 + nudge), y + fontSize * 0.3, 'black', fontSize);
-};
-
-const cellCenter = (r, c) => {
-  return [
-    boardLeft + c * cellSize + cellSize / 2,
-    boardTop + r * cellSize + cellSize / 2
-  ];
+// for me:  *  drawLine(x1, y1, x2, y2, color, lineWidth)
+const max = Math.max(width,height)
+const min = Math.min(width,height)
+const drawBoard = (borders) =>{
+for (let editVar = 1-borders; editVar <= 2+borders; editVar += 1){
+  drawLine(max/2-min/2 + min*(editVar/3), height, max/2-min/2 + min*(editVar/3), 0, 'grey', 2)
+  drawLine(max/2-min/2, height*(editVar/3), max/2+min/2, height*(editVar/3), 'gray', 2)
+}}
+drawBoard(0) //set 1 for borders and 0 for no borders
+const coordArray = [
+  [[''], [''], ['']],
+  [[''], [''], ['']],
+  [[''], [''], ['']],
+]
+const player1 = 'X' //will go first
+const player2 = 'O'
+const gameWin = (winner) =>{
+if (winner.winType == 'h'){
+  return drawLine(width/2 - height/2,height/6+height*(winner.winLoc/3), width/2 + height/2, height/6+height*(winner.winLoc/3), 'grey', 8)
+} else if (winner.winType == 'v'){
+  return drawLine(width/2-height/3+height*(winner.winLoc/3),height, width/2-height/3+height*(winner.winLoc/3), 0, 'gray', 8)
+} else if (winner.winType == 'd'){
+  return drawLine(width/2-height/(winner.winLoc == 1 ? -2 : 2), 0, width/2+height/(winner.winLoc == 0 ? 2 : -2),height, 'gray', 8)
+} 
 }
-const coordinates = (x, y) => {
-  return [
-    Math.floor((y - boardTop) / cellSize),
-    Math.floor((x - boardLeft) / cellSize)
-  ];
+let winner = {}
+const winSearch = (player) =>{
+  for(let c = 0; c<=2; c++){
+    if ((coordArray[c][0] == player) && (coordArray[c][1] == player) && (coordArray[c][2] == player)) winner = {winner: player,winType: 'h' ,winLoc: c};
+    if ((coordArray[0][c] == player) && (coordArray[1][c] == player) && (coordArray[2][c] == player)) winner = {winner: player,winType: 'v' ,winLoc: c};
+     }
+  if ((coordArray[0][0] == player) && (coordArray[1][1] == player) && (coordArray[2][2] == player)) winner = {winner: player,winType: 'd' ,winLoc: 0};
+  if ((coordArray[2][0] == player) && (coordArray[1][1] == player) && (coordArray[0][2] == player)) winner = {winner: player,winType: 'd' ,winLoc: 1}; 
+  gameWin(winner) 
+  return winner.winner
 }
-
-const validCoordinate = (c) => 0 <= c && c < 3;
-
-const validMove = (r, c) => {
-  return validCoordinate(r) && validCoordinate(c) && board[r][c] === '';
-}
-
-const makeMove = (r, c) => {
-  const marker = move % 2 === 0 ? 'X' : 'O';
-  drawMarker(marker, r, c);
-  board[r][c] = marker;
-  move++;
-}
-
-const isWinner = () => findWinner() !== null;
-
-const findWinner = () => {
-  for (let i = 0; i < lines.length; i++) {
-    if (isWinningLine(lines[i])) {
-      return lines[i];
-    }
-  }
-  return null;
-};
-
-const isWinningLine = (line) => {
-  const marker = markerAt(line[0]);
-  if (marker !== '') {
-    return marker === markerAt(line[1]) && marker === markerAt(line[2]);
-  } else {
-    return false;
-  }
-};
-
-const markerAt = (coord) => {
-  const [r, c] = coord; // e.g. [0, 0]
-  return board[r][c];
-};
-
-const drawThreeInARow = (winner) => {
-  const [r1, c1] = winner[0];
-  const [r2, c2] = winner[winner.length - 1];
-
-  const [x1, y1] = cellCenter(r1, c1);
-  const [x2, y2] = cellCenter(r2, c2);
-
-  let adjX1 = x1;
-  let adjX2 = x2;
-  let adjY1 = y1;
-  let adjY2 = y2;
-
-  if (y1 === y2 || x1 !== x2) {
-    adjX1 -= lineEndAdjustment;
-    adjX2 += lineEndAdjustment;
-  }
-
-  if (x1 === x2 || y1 !== y2) {
-    const slope = y1 < y2 ? 1 : -1;
-    adjY1 -= (slope * lineEndAdjustment);
-    adjY2 += (slope * lineEndAdjustment);
-  }
-
-  drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
-};
-
+let turns = 0; 
+let player = player1;
 registerOnclick((x, y) => {
-  const [r, c] = coordinates(x, y);
-  if (!isWinner() && validMove(r, c)) {
-    makeMove(r, c);
-    const winner = findWinner();
-    if (winner !== null) {
-      drawThreeInARow(winner);
-    }
-  }
-});
+let xPos;
+let yPos;
+ if (y < 1/3 * height){
+    yPos = 0
+  } else if (y < 2/3 * height){
+    yPos = 1
+  } else if (y < height) {
+    yPos = 2
+  } 
+  if (x < max/2 - min/2 || x > max/2 + min/2){
+   drawText(x>max/2-min/2?'⇦':'⇨', x-height/12, y+height/12, 'black', height/3)
+  }else{
+  if (x < max/2 - min/6){
+    xPos = 0
+  } else if (x < max/2 + min/6){
+    xPos = 1
+  } else if (x < max/2 + min/2){
+    xPos = 2
+  } 
+  if (coordArray[yPos][xPos] == '' && winSearch(player) == undefined){
+    coordArray[yPos][xPos] = player
+    drawText(player, max/2-min/2 - min*0.1 +min/6 + (min * xPos/3), min*0.1 + min/6 + min*yPos/3, 'black', min * 0.3)
+    console.log (coordArray, 'recent: ', xPos, yPos, 'winner: ', winSearch(player))
+    player = player === player1 ? player2 : player1
+    turns++
+    if (turns == 9 && winSearch(player) == undefined) drawText('◯', max/2-min/2, 8/9*height, 'grey', height);
+}}});
 
-
-drawBoard();
