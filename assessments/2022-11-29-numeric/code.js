1a489966c335eba612f5dab3b40b18c5c9a5fbb4
Peter Seibel <peter@gigamonkeys.com>
2022-12-05 11:53:38 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -9,3 +9,7 @@ const chocolatesPerPerson = (chocolates, people) => {
 const extraChocolates = (chocolates, people) => {
   return chocolates % people;
 };
+
+const leftOut = (chocolates, people) => {
+  return Math.max(people - chocolates, 0);
+};
