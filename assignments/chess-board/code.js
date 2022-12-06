b42b74ff322047214b4ffce4e75a311645e5cd4e
Peter Seibel <peter@gigamonkeys.com>
2022-12-06 15:17:32 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -17,36 +17,38 @@ const PIECES = {
   }
 };
 
+const size = Math.min(width, height) * 0.80;
 
-const boardSize = Math.min(width, height) * 0.80;
-const boardX = (width - boardSize) / 2;
-const boardY = (height - boardSize) / 2;
+const gui = {
+  size: size,
+  x: (width - size) / 2,
+  y: (height - size) / 2,
+};
 
-const xy = (row, col) => [
-  boardX + col * boardSize / 8,
-  boardY + row * boardSize / 8
+const xy = (gui, row, col) => [
+  gui.x + col * gui.size / 8,
+  gui.y + row * gui.size / 8
 ];
 
 // GUI
 
-const emptyBoard = () => {
-  const sq = boardSize / 8;
+const emptyBoard = (gui) => {
+  const sq = gui.size / 8;
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      const [x, y] = xy(i, j);
+      const [x, y] = xy(gui, i, j);
       const color = (i + j) % 2 === 0 ? 'white' : 'grey';
       drawFilledRect(x, y, sq, sq, color);
     }
   }
-  drawRect(boardX, boardY, boardSize, boardSize, 'grey', 1);
+  drawRect(gui.x, gui.y, gui.size, gui.size, 'grey', 1);
 };
 
 const drawPiece = (piece, rank, file) => {
-  console.log(JSON.stringify(piece));
   const text = PIECES[piece.color][piece.type];
-  const sq = boardSize / 8;
+  const sq = gui.size / 8;
   const [x, y] = xy(rank, file);
-  drawText(text, x + sq * 0.2, y + sq * 0.75, 'black', boardSize / 8);
+  drawText(text, x + sq * 0.2, y + sq * 0.75, 'black', gui.size / 8);
 };
 
 const drawBoard = (board) => {
@@ -69,15 +71,15 @@ const placePiece = (board, piece, row, col) => {
 
 const piece = (color, type) => {
   return { color, type };
-}
+};
 
-
+const possibleSquares = (piece) => {
+};
 
 
 // Actual runtime
 
 const board = Array(8).fill().map(() => Array(8).fill(null));
-
 
 placePiece(board, piece('white', 'king'), 7, 4);
 placePiece(board, piece('white', 'queen'), 7, 3);
