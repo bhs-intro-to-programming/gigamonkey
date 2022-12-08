06717a9dab1954b8a060e422c21442250a5eb3dd
Peter Seibel <peter@gigamonkeys.com>
2022-12-08 12:22:08 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -368,10 +368,11 @@ const day8 = () => {
 
   const part1 = (s) => visibility(forest(s)).reduce((a1, row) => a1 + row.reduce((a2, v) => a2 + (v ? 1 : 0), 0), 0);
 
-  const part2 = (s) => 
+  const part2 = (s) => scenic(forest(s), 3, 2);
+  /*
     forest(s).reduce((max, row, i, trees) => 
       Math.max(max, row.reduce((max, _, j) => Math.max(max, scenic(trees, i, j)), 0)), 0);
-  
+  */
   return { part1, part2 };
 };
 
