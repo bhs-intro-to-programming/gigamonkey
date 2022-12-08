0f060655f932605139beb6e838cfc56f20603ed4
Peter Seibel <peter@gigamonkeys.com>
2022-12-07 20:29:34 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -138,9 +138,9 @@ const pairs = (n) => {
 
 const r = () => {
   const ns = [];
-  for (let i = 0; i < 10; i++) {
-    for (let j = i; j < 10; j++) {
-      for (let k = j; k < 10; k++) {
+  for (let i = 0; i < 100; i++) {
+    for (let j = i; j < 100; j++) {
+      for (let k = j; k < 100; k++) {
         const n = i ** 3 + j ** 3 + k ** 3;
         if (!(n in ns)) ns[n] = [];
         ns[n].push([i, j, k]);
