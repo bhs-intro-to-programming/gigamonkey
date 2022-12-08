040c436b943f8c0692013ed9e07461b7c5f41611
Peter Seibel <peter@gigamonkeys.com>
2022-12-08 12:30:10 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -21,6 +21,9 @@ const intersection = (...args) => {
   }, null);
 };
 
+const indices = (rows, cols) =>
+  Array(rows).fill().map((_, i) => Array(cols).fill().map((_, j) => [i, j]));
+
 const show = (s) => s;
 
 ////////////////////////////////////////////////////////////////
@@ -368,7 +371,7 @@ const day8 = () => {
 
   const part1 = (s) => visibility(forest(s)).reduce((a1, row) => a1 + row.reduce((a2, v) => a2 + (v ? 1 : 0), 0), 0);
 
-  const part2 = (s) => //scenic(forest(s), 3, 2);
+  const part2 = (s) => 
     forest(s).reduce((max, row, i, trees) => 
       Math.max(max, row.reduce((max, _, j) => Math.max(max, scenic(trees, i, j)), 0)), 0);
   
