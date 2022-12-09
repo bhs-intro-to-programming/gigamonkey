635dae130491a65cbe848811ccd36e2eeba72db4
Peter Seibel <peter@gigamonkeys.com>
2022-12-09 10:50:32 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -454,4 +454,5 @@ if (false) {
 run('day_09.test', day9().part1, 13);
 run('day_09.problem', day9().part1, 6563);
 run('day_09.test', day9().part2);
+run('day_09.test2', day9().part2);
 run('day_09.problem', day9().part2);
