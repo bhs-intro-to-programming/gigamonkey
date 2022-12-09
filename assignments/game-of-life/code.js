798fcf50c44e706b2a52743524d16faf69648a4b
Peter Seibel <peter@gigamonkeys.com>
2022-12-08 19:25:22 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,217 +1,74 @@
-let SIZE = 4;
-let P = 0.2;
-let RATE = 0;
+drawFilledRect(0, 0, width, height, 'black')
+const cellsize = 5
 
-const H_BLINKER = `###`;
+const makeArrayRow = (columns) => {
+  let row = []
+  for (let i = 0; i < columns; i++) {
+    if (Math.random() < .2) {
+      row.push(1)
+    } else {
+      row.push(0)
+    }
+  }
+  return row
+}
+const makeArray = (rows, columns) => {
+  let b = []
+  for (let i = 0; i < rows; i++) {
+    b.push(makeArrayRow(columns))
+  }
+  return b;
+}
 
-const V_BLINKER = '#\n#\n#';
-
-const BLOCK = '##\n##';
-
-const GLIDER = `
-#..
-.##
-##.
-`;
-
-const PULSAR = `
-..OOO...OOO..
-.............
-O....O.O....O
-O....O.O....O
-O....O.O....O
-..OOO...OOO..
-.............
-..OOO...OOO..
-O....O.O....O
-O....O.O....O
-O....O.O....O
-.............
-..OOO...OOO..
-`;
-
-const GOSPER_GG = `
-........................#...........
-......................#.#...........
-............##......##............##
-...........#...#....##............##
-##........#.....#...##..............
-##........#...#.##....#.#...........
-..........#.....#.......#...........
-...........#...#....................
-............##......................
-`;
-
-const BUTTERFLY = `
-....##.....
-...#..#....
-....#.#....
-.#...#.##..
-#.#...#..#.
-#..#...#.#.
-.##.#...#..
-...#.#.....
-...#..#....
-....##.....
-`;
-
-const columns = Math.floor(width / SIZE);
-const rows = Math.floor(height / SIZE);
-const xoffset = (width - columns * SIZE) / 2;
-const yoffset = (height - rows * SIZE) / 2;
-
-const render = (grid) => {
-  drawFilledRect(0, 0, width, height, 'black');
-  for (let r = 0; r < grid.length; r++) {
-    for (let c = 0; c < grid[r].length; c++) {
-      if (grid[r][c]) {
-        fillCell(r, c);
+const drawTheThings = (b, cellsize) => {
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < b[i].length; j++) {
+      if (b[i][j] === 1) {
+        drawFilledRect(j * cellsize, i * cellsize, cellsize, cellsize, 'palevioletred')
       }
     }
   }
-};
+}
 
-const fillCell = (r, c) => {
-  drawFilledRect(xoffset + c * SIZE, yoffset + r * SIZE, SIZE, SIZE, 'rgb(0, 255, 0)');
-};
-
-const nextGeneration = (grid) => {
-  const next = newGrid();
-  for (let r = 0; r < grid.length; r++) {
-    for (let c = 0; c < grid[r].length; c++) {
-      next[r][c] = isAlive(grid, r, c);
-    }
-  }
-  return next;
-};
-
-const isAlive = (grid, r, c) => {
-  const n = livingNeighbors(grid, r, c);
-  return n === 3 || grid[r][c] && n == 2;
-};
-
-const livingNeighbors = (grid, r, c) => {
-  let count = 0;
-  for (let dx = -1; dx <= 1; dx++) {
-    for (let dy = -1; dy <= 1; dy++) {
-      if (!(dx === 0 && dy === 0)) {
-        const nr = r + dx;
-        const nc = c + dy;
-        if (inBounds(nr, grid) && inBounds(nc, grid[nr]) && grid[nr][nc]) {
-          count++
+const neighbors = (b, y, x) => {
+  let n = 0
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      if (i !== 0 || j !== 0) {
+        const r = y + i;
+        const c = x + j;
+        if (0 <= r && r < b.length && 0 <= c && c < b[r].length) {
+          if (b[r][c] === 1) n++;
         }
       }
     }
   }
-  return count;
-};
+  return n
+}
 
-const inBounds = (index, array) => 0 <= index && index < array.length;
-
-const newGrid = () => {
-  const grid = [];
-  for (let r = 0; r < rows; r++) {
-    const row = [];
-    for (let c = 0; c < columns; c++) {
-      row.push(false);
-    }
-    grid.push(row);
-  }
-  return grid;
-};
-
-const addPattern = (grid, top, left, pattern) => {
-  const p = pattern.trim().split('\n');
-  for (let r = 0; r < p.length; r++) {
-    for (let c = 0; c < p[r].length; c++) {
-      grid[top + r][left + c] = p[r][c] !== '.';
+const whatLives = (b, cellsize) => {
+  for (let i = 0; i < height / cellsize; i++) {
+    for (let j = 0; j < width / cellsize; j++) {
+      if (neighbors(b, i, j) > 2) {
+        b[i][j] = 1
+      } else {
+        if (b[i] === undefined) {
+          console.log(i + ' ' + j)
+        }
+        b[i][j] = 0
+      }
     }
   }
-};
+}
 
-let next = now();
-let grid = newGrid();
 
-const clearGrid = () => {
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < columns; c++) {
-      grid[r][c] = false;
-    }
-  }
-};
+const board = makeArray(Math.floor(height / cellsize), Math.floor(width / cellsize));
+drawTheThings(board, cellsize);
 
-const randomize = (p) => {
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < columns; c++) {
-      grid[r][c] = Math.random() < p;
-    }
-  }
-};
+const redraw = (t) => {
+  clear()
+  whatLives(board, cellsize)
+  drawTheThings(board, cellsize)
+}
 
-const glider = (r, c) => {
-  addPattern(grid, r, c, GLIDER);
-};
-
-const gun = (r, c) => {
-  addPattern(grid, r, c, GOSPER_GG);
-};
-
-const blinker = (r, c) => {
-  addPattern(grid, r, c, H_BLINKER);
-};
-
-const multiBlinker = (r, c) => {
-  addPattern(grid, r, c, H_BLINKER);
-  addPattern(grid, r, c + 6, H_BLINKER);
-  addPattern(grid, r - 4, c + 4, V_BLINKER);
-  addPattern(grid, r + 2, c + 4, V_BLINKER);
-};
-
-const block = (r, c) => {
-  addPattern(grid, r, c, BLOCK);
-};
-
-const pulsar = (r, c) => {
-  addPattern(grid, r, c, PULSAR);
-};
-
-// Try foo(12, 42) with SIZE = 4. Or foo(12, 64) on big screen
-const foo = (r, c) => {
-  pulsar(5, columns - 22);
-  pulsar(Math.floor(rows / 2), Math.floor(columns / 2));
-  gun(r, c);
-};
-
-const bar = () => {
-  const mr = Math.floor(rows / 2);
-  const mc = Math.floor(columns / 2);
-  pulsar(mr - 42, mc + 72)
-  pulsar(mr, mc);
-  gun(mr - 45, mc - 62);
-};
-
-const makePulsar = () => {
-  glider(10, 30);
-  glider(0, 25);
-};
-
-const makeExplosion = () => {
-  const midRow = Math.floor(rows / 2);
-  const midCol = Math.floor(columns / 2);
-  addPattern(grid, midRow, midCol, '##\n##');
-  glider(0, midCol - (midRow - 4));
-};
-
-const drawFrame = (t) => {
-  if (t > next) {
-    render(grid);
-    const iters = RATE < 0 ? -RATE : 1;
-    for (let i = 0; i < iters; i++) {
-      grid = nextGeneration(grid)
-    }
-    next += Math.max(0, RATE);
-    next = Math.max(next, now());
-  }
-};
-
-animate(drawFrame);
+animate(redraw)
