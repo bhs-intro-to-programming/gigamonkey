bf853b10b5b51b7c3ede9ef702349f0ed3ca0d70
Peter Seibel <peter@gigamonkeys.com>
2022-12-09 10:37:19 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -409,7 +409,7 @@ const day9 = () => {
     const knots = Array(n).fill().map(() => ({ x: 0, y: 0 }));
     const tail = knots[n - 1];
 
-    const visited = new Set();
+    const visited = new Set(['0,0']);
     lines(s).map(parseLine).forEach(([mover, steps]) => {
       for (let i = 0; i < steps; i++) {
         knots.forEach((k, i) => {
