d62616fad63c167c935efb693e5878f975273405
Peter Seibel <peter@gigamonkeys.com>
2022-12-09 19:24:57 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

---
+++ assessments/2022-12-08-control-constructs/code.js
@@ -0,0 +1,17 @@
+const yesIfEven = (n) => {
+  if (n % 2 === 0) {
+    return 'yes';
+  } else {
+    return 'no';
+  }
+};
+
+const countXs = (s) => {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'x') {
+      count++;
+    }
+  }
+  return count;
+}
