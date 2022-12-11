96cec594e7ef0dd2f1c177366d17d814ff66553d
Peter Seibel <peter@gigamonkeys.com>
2022-12-10 22:29:23 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -518,7 +518,7 @@ const day11 = () => {
 
   const part2 = (s) => {
     const ms = monkeys(s, BigInt);
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 40; i++) {
       ms.forEach((m) => monkeySeeMonkeyDeux(m, ms));
     }
     const busy = ms.map(m => m.inspected).sort((a, b) => b - a);
