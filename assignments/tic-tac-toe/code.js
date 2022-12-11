0564ae1d60e332ff70f1ce12cc03037ac7a23d86
Peter Seibel <peter@gigamonkeys.com>
2022-12-10 19:37:56 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,81 +1,4 @@
-const boardSize = Math.min(width, height);
-
-const leftX = width / 2 - boardSize / 2;
-const rightX = width / 2 + boardSize / 2;
-const topY = height / 2 - boardSize / 2;
-const bottomY = height / 2 + boardSize / 2;
-
-const drawBoard = (borders) => {
-  const start = borders ? 0 : 1;
-  const end = borders ? 3 : 2;
-
-  for (let i = start; i <= end; i++) {
-    const verticalX = leftX + boardSize * (i / 3);
-    const horizontalY = topY + boardSize * (i / 3);
-    drawLine(verticalX, topY, verticalX, bottomY, 'grey', 2);
-    drawLine(leftX, horizontalY, rightX, horizontalY, 'gray', 2);
-  }
-};
-
-const gameWin = (winner) => {
-  if (winner.winType === 'h') {
-    const y = topY + boardSize / 6 + boardSize * (winner.winLoc / 3);
-    return drawLine(leftX, y, rightX, y, 'grey', 8);
-
-  } else if (winner.winType === 'v') {
-    const x = leftX + boardSize / 6 + boardSize * (winner.winLoc / 3);
-    return drawLine(x, topY, x, bottomY, 'gray', 8);
-
-  } else if (winner.winType === 'd') {
-    const x1 = winner.winLoc === 0 ? leftX : rightX;
-    const x2 = winner.winLoc === 0 ? rightX : leftX;
-    return drawLine(x1, topY, x2, bottomY, 'gray', 8);
-  }
-};
-
-const winSearch = (p) => {
-  console.log('here');
-  let winner = {};
-  for (let c = 0; c <= 2; c++) {
-    if (board[c][0] === p && board[c][1] === p && board[c][2] === p) {
-      winner = { winner: p, winType: 'h', winLoc: c };
-    }
-    if (board[0][c] === p && board[1][c] === p && board[2][c] === p) {
-      winner = { winner: p, winType: 'v', winLoc: c };
-    }
-  }
-
-  if (board[0][0] === p && board[1][1] === p && board[2][2] === p) {
-    winner = { winner: p, winType: 'd', winLoc: 0 };
-  }
-  if (board[2][0] === p && board[1][1] === p && board[0][2] === p) {
-    winner = { winner: p, winType: 'd', winLoc: 1 };
-  }
-
-  gameWin(winner)
-  return winner.winner
-};
-
-registerOnclick((x, y) => {
-  const yPos = Math.floor((y - topY) / (boardSize / 3));
-  const xPos = Math.floor((x - leftX) / (boardSize / 3));
-
-  if (xPos < 0 || xPos > 2) {
-    drawText(xPos > 2 ? '⇦' : '⇨', x - boardSize / 12, y + boardSize / 12, 'black', boardSize / 3)
-  } else {
-    if (board[yPos][xPos] === '' && winSearch(player) === undefined) {
-      board[yPos][xPos] = player
-      drawText(player, leftX - boardSize * 0.1 + boardSize / 6 + (boardSize * xPos / 3), boardSize * 0.1 + boardSize / 6 + boardSize * yPos / 3, 'black', boardSize * 0.3)
-      console.log(board, 'recent: ', xPos, yPos, 'winner: ', winSearch(player))
-      player = player === player1 ? player2 : player1
-      turns++
-      if (turns === 9 && winSearch(player) === undefined) drawText('◯', leftX, 8 / 9 * height, 'grey', height);
-    }
-  }
-});
-
-const player1 = 'X' //will go first
-const player2 = 'O'
+let move = 0;
 
 const board = [
   ['', '', ''],
@@ -83,7 +6,57 @@ const board = [
   ['', '', ''],
 ];
 
-let turns = 0;
-let player = player1;
+registerOnclick((x, y) => {
+  const marker = move % 2 === 0 ? 'X' : 'O';
+  const color = move % 2 === 0 ? 'red' : 'blue';
 
-drawBoard(false);
+  const row = 1 + Math.floor(y / (height / 3));
+  const col = Math.floor(x / (width / 3));
+
+  const xPos = (col * width / 3) + width / 9;
+  const yPos = (row * height / 3) - (height / 9) + (height / 18);
+
+  if (board[row - 1][col] === '') {
+    drawText(marker, xPos, yPos, color, Math.min(width, height) * 0.3);
+
+    board[row - 1][col] = marker;
+    move++;
+    checkWinner()
+  };
+});
+
+function checkWinner() {
+  //checking horizontally
+  for (let r = 0; r < 3; r++) {
+    if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != '') {
+      drawLine(0, (height / 6) + (r * height / 3), width, (height / 6) + (r * height / 3), "yellow", 20);
+    }
+  }
+  //checking vertically
+  for (let c = 0; c < 3; c++) {
+    if (board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[0][c] != '') {
+      drawLine((width / 6) + (c * width / 3) - (width/100), 0, (width / 6) + (c * width / 3)- (width/100), height, "yellow", 20);
+    }
+  }
+  //checking diagonally from left to right
+  if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '') {
+    drawLine(0, 0, width, height, 'yellow', 20);
+  }
+
+  //checking diagonally from right to left
+  if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '') {
+    drawLine(0, height, width, 0, 'yellow', 20);
+  }
+}
+const drawBoard = () => {
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 5);
+  }
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 5);
+  }
+}
+
+drawBoard()
