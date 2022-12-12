be8ee7a5074d2d828c9e3f5c0b9b79cbe1c1d376
Peter Seibel <peter@gigamonkeys.com>
2022-12-11 20:34:00 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -487,7 +487,7 @@ const day11 = () => {
     '+': (old, a, b, monkeys) => {
       return old.map((r, i) => {
         const mod = monkeys[i].divisibleBy;
-        return (evaluate(a, r) % mod + evaluate(b, r) %  mod) % mod;
+        return (evaluate(a, r) + evaluate(b, r)) % mod;
       });
     },
     '*': (old, a, b, monkeys) => {
