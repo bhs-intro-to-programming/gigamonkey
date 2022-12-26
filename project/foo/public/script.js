5f6fb372783bebecae16fa2e65a981e67f8a2fc4
Peter Seibel <peter@gigamonkeys.com>
2022-12-26 15:15:12 -0800

    Update script.js

--- project/foo/public/script.js
+++ project/foo/public/script.js
@@ -1,5 +1,7 @@
 /*
- * Get or create a single element.
+ * Get or create a single element. If the query is in the form of a tag, e.g.
+ * '<p>' it creates an element of that type. Otherwise it queries the document
+ * using the argument as a selector.
  */
 const $ = (q) => {
   let m;
@@ -8,10 +10,10 @@ const $ = (q) => {
   } else {
     return document.querySelector(q);
   }
-}
+};
 
 /*
- * Get all elements matching the selector.
+ * Get all elements matching selector.
  */
 const $$ = (q) => document.querySelectorAll(q);
 
@@ -21,10 +23,12 @@ const $$ = (q) => document.querySelectorAll(q);
 const text = (t) => document.createTextNode(t);
 
 /*
- * Create a node from literal HTML.
+ * Create an element or elements from literal HTML. If the HTML specifies just
+ * one element it is returned. Otherwise returns an array of elements.
  */
 const html = (html) => {
   const t = document.createElement('template');
-  t.innerHTML = html;
-  return t.content;
+  t.innerHTML = html.trim();
+  const children = [...t.content.children];
+  return children.length == 1 ? children[0] : children;
 };
