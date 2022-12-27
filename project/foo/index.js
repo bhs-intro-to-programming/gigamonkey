86c4b6b796bf80fcefed6451c7a8a5c36e353d9b
Peter Seibel <peter@gigamonkeys.com>
2022-12-26 16:43:58 -0800

    Get port from listener.

--- project/foo/index.js
+++ project/foo/index.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+app.listen(port, function () {
+  console.log(`Listening on port ${this.address().port}`)
+});
