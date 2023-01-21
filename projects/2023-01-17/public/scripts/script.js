c627425b0680fa81f71a47e26af3da08ac3f65c7
Peter Seibel <peter@gigamonkeys.com>
2023-01-21 22:43:36 +0000

    Take advantage of varargs append

--- projects/2023-01-17/public/scripts/script.js
+++ projects/2023-01-17/public/scripts/script.js
@@ -2,11 +2,9 @@
 
 const body = document.querySelector('body');
 
-const add = (e) => body.append(e);
-
 const text = (s) => document.createTextNode(s);
 
-const tagged = (tag, s) => {
+const taggedText = (tag, s) => {
   const e = document.createElement(tag);
   // A note on the next line: I fibbed a bit when I told you that the condition
   // in an if had to be a boolean value. In fact you can use any value in a
@@ -26,7 +24,7 @@ const tagged = (tag, s) => {
   return e;
 };
 
-const withChildren = (tag, children) => {
+const taggedChildren = (tag, children) => {
   const element = document.createElement(tag);
   children.forEach(c => element.append(c));
   return element;
@@ -37,12 +35,14 @@ const addAttribute = (element, attribute, value) => {
   return element;
 }
 
-const h1 = (s) => tagged('h1', s);
-const p = (s) => tagged('p', s);
-const code = (s) => tagged('code', s);
-const li = (children) => withChildren('li', children);
-const a = (href, text) => addAttribute(tagged('a', text), 'href', href);
-const img = (src, alt) => addAttribute(addAttribute(tagged('img'), 'src', src), 'alt', alt);
+const h1 = (s) => taggedText('h1', s);
+const p = (s) => taggedText('p', s);
+const code = (s) => taggedText('code', s);
+const ol = (children) => taggedChildren('ol', children);
+const li = (children) => taggedChildren('li', children);
+const a = (href, text) => addAttribute(taggedText('a', text), 'href', href);
+const img = (src, alt) => addAttribute(addAttribute(taggedText('img'), 'src', src), 'alt', alt);
+const div = (children) => taggedChildren('div', children);
 
 // This is a bit fancy. Most of you haven't learned all the pieces needed to
 // understand this function. Basically the first two arguments are functions
@@ -50,102 +50,102 @@ const img = (src, alt) => addAttribute(addAttribute(tagged('img'), 'src', src), 
 // argument is an array of strings and this maps over that array return a new
 // array consisting of the elements by calling the two function arguments in
 // alternating order.
-const alternating = (a, b, items) => items.map((item, i) => (i % 2 === 0 ? a  : b)(item));
+const alternating = (a, b, items) => items.map((item, i) => (i % 2 === 0 ? a : b)(item));
 
 // Now build the page
 
-add(h1('Can you tell me why HTML was developed?'));
+body.append(
+  h1('Can you tell me why HTML was developed?'),
 
-add(p(`
-  HTML (Hypertext Markup Language) was developed to create a standard way
-  to create and structure documents on the World Wide Web. It allows
-  developers to create structured documents, including headings,
-  paragraphs, images and links, that can be displayed in a web browser.
-  It was created to simplify the process of creating and sharing
-  documents online, so that anyone can create and view web pages with a
-  consistent look and feel.
-`));
+  p(`
+    HTML (Hypertext Markup Language) was developed to create a standard way
+    to create and structure documents on the World Wide Web. It allows
+    developers to create structured documents, including headings,
+    paragraphs, images and links, that can be displayed in a web browser.
+    It was created to simplify the process of creating and sharing
+    documents online, so that anyone can create and view web pages with a
+    consistent look and feel.
+`),
 
-add(h1('What about using HTML for GUIs?'));
+  h1('What about using HTML for GUIs?'),
 
-add(p(`
-  HTML can be used to create user interfaces for web applications, but it
-  is primarily designed as a markup language for creating structured
-  documents to be displayed in web browsers. While it can be used to
-  create basic user interfaces, it is not typically considered a good
-  choice for creating complex, interactive user interfaces. Other
-  technologies such as JavaScript and CSS are typically used in
-  conjunction with HTML to create more advanced user interfaces for web
-  applications. Additionally, there are other technologies such as
-  Electron, React Native and NativeScript that allow to create
-  cross-platform desktop and mobile apps using web technologies such as
-  HTML, CSS, and JavaScript.
-`));
+  p(`
+    HTML can be used to create user interfaces for web applications, but it
+    is primarily designed as a markup language for creating structured
+    documents to be displayed in web browsers. While it can be used to
+    create basic user interfaces, it is not typically considered a good
+    choice for creating complex, interactive user interfaces. Other
+    technologies such as JavaScript and CSS are typically used in
+    conjunction with HTML to create more advanced user interfaces for web
+    applications. Additionally, there are other technologies such as
+    Electron, React Native and NativeScript that allow to create
+    cross-platform desktop and mobile apps using web technologies such as
+    HTML, CSS, and JavaScript.
+`),
 
-add(h1('What are some of the main elements of HTML?'));
+  h1('What are some of the main elements of HTML?'),
 
-add(p('Some of the main elements of HTML include:'));
+  p('Some of the main elements of HTML include:'),
 
-add(withChildren('ol', [
-
-  li(alternating(code, text, [
-    '<html>',
-    `: This is the root element of an HTML document, and it contains
+  ol([
+    li(alternating(code, text, [
+      '<html>',
+      `: This is the root element of an HTML document, and it contains
       all of the other elements.`
-  ])),
+    ])),
 
-  li(alternating(code, text, [
-    '<head>',
-    `: This element contains information about the document,
+    li(alternating(code, text, [
+      '<head>',
+      `: This element contains information about the document,
       such as the title of the page, which is displayed in the
       browser's title bar or tab.`
-  ])),
+    ])),
 
-  li(alternating(code, text, [
-    '<body>',
-    `: This element contains the content of the document
+    li(alternating(code, text, [
+      '<body>',
+      `: This element contains the content of the document
       that is displayed in the browser window.`
-  ])),
+    ])),
 
-  li(alternating(code, text, [
-    '<h1>', ' to ', '<h6>',
-    `: These elements are used for headings and subheadings.
+    li(alternating(code, text, [
+      '<h1>', ' to ', '<h6>',
+      `: These elements are used for headings and subheadings.
       <h1> is the highest level heading, while <h6> is the lowest.`
+    ])),
+
+    li(alternating(code, text, ['<p>', ': This element is used for paragraphs of text.'])),
+
+    li(alternating(code, text, [
+      '<a>',
+      `: This element is used for hyperlinks, which allow users to navigate between pages on the web.`
+    ])),
+
+    li(alternating(code, text, ['<img>', ': This element is used to embed images in a web page.'])),
+
+    li(alternating(code, text, [
+      '<ul>', ' and ', '<ol>',
+      `: These elements are used for unordered and ordered lists, respectively.`
+    ])),
+
+    li(alternating(code, text, ['<li>', ': This element is used for list items.'])),
+
+    li(alternating(code, text, [
+      '<div>', ' and ', '<span>',
+      `: These elements are used for grouping and applying styles to elements on a web page.`
+    ])),
+  ]),
+
+  taggedChildren('p', alternating(text, code, [
+    'These are some of the main elements, but there are many more available, like ',
+    '<header>', ', ', '<nav>', ', ', '<main>', ', ', '<footer>', ', ', '<form>',
+    ', ', '<input>', ', ', '<select>', ', etc.',
   ])),
 
-  li(alternating(code, text, ['<p>', ': This element is used for paragraphs of text.'])),
+  h1('Can you find a picture of the inventor of the web?'),
 
-  li(alternating(code, text, [
-    '<a>',
-    `: This element is used for hyperlinks, which allow users to navigate between pages on the web.`
-  ])),
+  img('/images/tim-berners-lee.jpg', 'A young Tim Berners-Lee'),
 
-  li(alternating(code, text, ['<img>', ': This element is used to embed images in a web page.'])),
-
-  li(alternating(code, text, [
-    '<ul>', ' and ', '<ol>',
-    `: These elements are used for unordered and ordered lists, respectively.`
-  ])),
-
-  li(alternating(code, text, ['<li>', ': This element is used for list items.'])),
-
-  li(alternating(code, text, [
-    '<div>', ' and ', '<span>',
-    `: These elements are used for grouping and applying styles to elements on a web page.`
-  ])),
-]));
-
-add(withChildren('p', alternating(text, code, [
-  'These are some of the main elements, but there are many more available, like ',
-  '<header>', ', ', '<nav>', ', ', '<main>', ', ', '<footer>', ', ', '<form>',
-  ', ', '<input>', ', ', '<select>', ', etc.',
-])));
-
-add(tagged('h1', 'Can you find a picture of the inventor of the web?'));
-
-add(img('/images/tim-berners-lee.jpg', 'A young Tim Berners-Lee'));
-
-add(p(`
+  p(`
   I'm sorry, I am a text based model and I am not able to find or show
   images. However, I can tell you that the inventor of the World Wide Web
   (WWW) is Sir Tim Berners-Lee. He is a British computer scientist and is
@@ -156,17 +156,17 @@ add(p(`
   web server, called httpd. He also wrote the first version of the HTML
   language, which provided the structure for web pages. He is currently
   working on a decentralized web project called Solid.
-`));
+`),
 
-add(addAttribute(withChildren('div', [
-  withChildren('p', [
-    text('Text from 2023-01-14 conversation with '),
-    a('https://chat.openai.com/chat', 'ChatGPT')
-  ]),
-  withChildren('p', [
-    text('Photo of a young Tim Berners-Lee from '),
-    a('https://www.flickr.com/photos/itupictures/16662336315', 'Flickr'),
-    text(' no thanks to ChatGPT. ('),
-    a('https://creativecommons.org/licenses/by/2.0/', 'CC BY 2.0')
-  ])
-]), 'class', 'credits'));
+  addAttribute(div([
+    taggedChildren('p', [
+      text('Text from 2023-01-14 conversation with '),
+      a('https://chat.openai.com/chat', 'ChatGPT')
+    ]),
+    taggedChildren('p', [
+      text('Photo of a young Tim Berners-Lee from '),
+      a('https://www.flickr.com/photos/itupictures/16662336315', 'Flickr'),
+      text(' no thanks to ChatGPT. ('),
+      a('https://creativecommons.org/licenses/by/2.0/', 'CC BY 2.0')
+    ])
+  ]), 'class', 'credits'));
