a7f696cb9af50e39b01c6db29a4a9196a0fc72ee
Peter Seibel <peter@gigamonkeys.com>
2023-02-02 20:59:26 +0000

    Simple caesar cipher page.

--- projects/caesar-cipher/public/scripts/script.js
+++ projects/caesar-cipher/public/scripts/script.js
@@ -1,63 +1,31 @@
-////////////////////////////////////////////////////////////////////////////////
-// Add a single paragraph dynamically
+const plaintext = document.getElementById('plaintext');
+const ciphertext = document.getElementById('ciphertext');
+const key = document.getElementById('key');
+const keyDisplay = document.getElementById('show-key');
 
-// Create a paragraph element
-const p = document.createElement('p');
 
-// Create the text of the paragraph.
-const text = document.createTextNode('This paragraph is created dynamically.');
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-// Add the text to the paragraph.
-p.append(text);
-
-// And add the new paragraph to the body of the document.
-document.querySelector('body').append(p);
-
-////////////////////////////////////////////////////////////////////////////////
-// Add ten paragraphs.
-
-// Do the same thing as before but in a loop
-for (let i = 0; i < 10; i++) {
-  const p = document.createElement('p');
-  p.append(document.createTextNode(`Counting ${i}.`));
-  document.querySelector('body').append(p);
+const encodeChar = (char, k) => {
+  const n = alphabet.indexOf(char.toLowerCase());
+  if (n !== -1) {
+    const encoded = alphabet[(n + k) % 26];
+    return char.toUpperCase() === char ? encoded.toUpperCase() : encoded;
+  } else {
+    return char;
+  }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// This function is an event handler. (Similar to the functions we used with
-// registerOnclick in some of our old environments.)
+const cc = (s, k) => [...s].map(c => encodeChar(c, k)).join('');
 
-const recordClick = (e) => {
-  // Create a paragraph element.
-  const p = document.createElement('p');
+const encrypt = () => {
+  ciphertext.innerText = cc(plaintext.value, Number(key.value));
+}
 
-  // Create the text of the paragrph, using the event object we were passed
-  const text = document.createTextNode(`Clicked on “${e.target.parentNode.innerText}”`);
+plaintext.oninput = encrypt;
+key.oninput = (e) => {
+  encrypt();
+  keyDisplay.innerText = e.target.value;
+}
 
-  // Add the text to the paragraph
-  p.append(text);
-
-  // Add the paragraph to the body of the document
-  document.getElementById('clicks').append(p);
-};
-
-////////////////////////////////////////////////////////////////////////////////
-// Add something to every existing paragraph.
-
-document.querySelectorAll('p').forEach((p) => {
-  // Create a bold element.
-  const b = document.createElement('b');
-
-  // Create some text and add it to the bold element.
-  b.append(document.createTextNode('Click me!'));
-
-  // Register the recordClick function to handle when the bold element is
-  // clicked.
-  b.onclick = recordClick;
-
-  // Append some text to the current paragraph.
-  p.append(document.createTextNode(' '));
-
-  // And finally append the bold element to the paragraph.
-  p.append(b);
-});
+keyDisplay.innerText = key.value;
