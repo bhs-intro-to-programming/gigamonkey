cd1495c7d0cc85a0abd36e35c5fba11864908f33
Peter Seibel <peter@gigamonkeys.com>
2023-02-03 12:26:29 -0800

    Add repulsion from walls.

--- projects/boids/public/script.js
+++ projects/boids/public/script.js
@@ -11,8 +11,11 @@ const randomBoid = () => boid(
   randomInt(width),
   randomInt(height),
   (1 + randomInt(5)) * randomSign(),
-  (1 + randomInt(5)) * randomSign())
+  (1 + randomInt(5)) * randomSign());
 
+const distance = (b1, b2) => Math.hypot(b1.x - b2.x, b1.y - b2.y);
+
+const neighbors = (boid, boids) => boids.filter(b => distance(b, boid) < 50);
 
 const drawBoid = (b) => {
   drawFilledCircle(b.x, b.y, 5, 'blue');
@@ -29,14 +32,25 @@ const updatePositions = (boids, t) => {
 const updatePosition = (b, elapsed) => {
   b.x = clamp(b.x + b.dx/10 * elapsed, 0, width);
   b.y = clamp(b.y + b.dy/10 * elapsed, 0, height);
-  b.dx += -1 + Math.random() * 2;
-  b.dy += -1 + Math.random() * 2;
+  updateVelocity(b);
+};
+
+const updateVelocity = (b) => {
+  b.dx -= (b.x - 0) / (10 * width / 2);
+  b.dx += (width - b.x) / (10 * width / 2);
+
+  b.dy -= b.y / (10 * height / 2);
+  b.dy += (height - b.y) / (10 * height / 2);
+
+  // b.dx += -1 + Math.random() * 2;
+  // b.dy += -1 + Math.random() * 2;
 };
 
 const clamp = (n, min, max) => (n < min ? min : n > max ? max : n);
 
 // This has to come early so width and height are set before we use them.
 setCanvas(document.getElementById('screen'));
+
 
 const boids = Array(100).fill().map(randomBoid);
 
