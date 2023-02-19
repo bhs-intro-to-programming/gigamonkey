b83614577b56d07e82f8ea16239478000ba82dcf
Peter Seibel <peter@gigamonkeys.com>
2023-02-19 23:56:07 +0000

    Adding ciphertext P (and fixing name of textarea)

--- projects/caesar-demo/public/script.js
+++ projects/caesar-demo/public/script.js
@@ -1,15 +1,14 @@
-const ciphertext = document.querySelector('#ciphertext');
+const plaintext = document.querySelector('#plaintext');
 const key = document.querySelector('#key');
+const ciphertext = document.querySelector('#ciphertext');
 
-
-ciphertext.onchange = (e) => {
-  record(e.target, e.target.value);
+plaintext.onchange = (e) => {
+  recordCiphertext();
 };
 
 key.onchange = (e) => {
   record(e.target, e.target.value);
 };
-
 
 
 // Add a div to every fieldset for logging input events.
@@ -19,6 +18,9 @@ const record = (element, text) => {
   const p = document.createElement('p');
   p.innerText = text;
   element.closest('fieldset').querySelector('div').append(p);
-}
+};
 
+const recordCiphertext = () => {
+  ciphertext.innerText = plaintext.value;
+};
 
