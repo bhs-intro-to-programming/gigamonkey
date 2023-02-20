4ca9949de3848dcf73398cb3dd266e4ec9142672
Peter Seibel <peter@gigamonkeys.com>
2023-02-20 17:36:27 +0000

    Switching to ids on elements.

--- projects/caeser-demo/public/script.js
+++ projects/caeser-demo/public/script.js
@@ -1,3 +1,6 @@
+const plaintext = document.getElementById('plaintext');
+const key = document.getElementById('key');
+
 // Add a div to every fieldset for logging input events.
 document.querySelectorAll('fieldset').forEach(fs => fs.append(document.createElement('div')));
 
@@ -7,14 +10,10 @@ const record = (element, text) => {
   element.closest('fieldset').querySelector('div').append(p);
 }
 
-const addOnChange = (element) => {
-  element.onchange = (e) => {
-    if (e.target.getAttribute('type') === 'checkbox') {
-      record(e.target, `${e.target.value} is checked: ${e.target.checked}`);
-    } else {
-      record(e.target, e.target.value);
-    }
-  }
+plaintext.onchange = (e) => {
+  record(e.target, e.target.value);
 };
 
-document.querySelectorAll('input, textarea').forEach(addOnChange);
+key.onchange = (e) => {
+  record(e.target, e.target.value);
+};
