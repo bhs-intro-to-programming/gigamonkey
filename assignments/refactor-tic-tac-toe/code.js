7c4abd9a4665e04a62d3ffb707bbf967527e79d4
Peter Seibel <peter@gigamonkeys.com>
2023-02-22 09:43:53 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -55,15 +55,16 @@ const lines = [
   [[2, 0], [1, 1], [0, 2]],
 ];
 
-// Draw the board
-const x1 = boardLeft + cellSize;
-const x2 = boardLeft + 2 * cellSize;
-const y1 = boardTop + cellSize;
-const y2 = boardTop + 2 * cellSize;;
-drawLine(x1, boardTop, x1, boardTop + boardSize, 'grey', 2);
-drawLine(x2, boardTop, x2, boardTop + boardSize, 'grey', 2);
-drawLine(boardLeft, y1, boardLeft + boardSize, y1, 'grey', 2);
-drawLine(boardLeft, y2, boardLeft + boardSize, y2, 'grey', 2);
+const drawBoard = () => {
+  const x1 = boardLeft + cellSize;
+  const x2 = boardLeft + 2 * cellSize;
+  const y1 = boardTop + cellSize;
+  const y2 = boardTop + 2 * cellSize;;
+  drawLine(x1, boardTop, x1, boardTop + boardSize, 'grey', 2);
+  drawLine(x2, boardTop, x2, boardTop + boardSize, 'grey', 2);
+  drawLine(boardLeft, y1, boardLeft + boardSize, y1, 'grey', 2);
+  drawLine(boardLeft, y2, boardLeft + boardSize, y2, 'grey', 2);
+};
 
 registerOnclick((x, y) => {
 
@@ -148,3 +149,5 @@ registerOnclick((x, y) => {
     }
   }
 });
+
+drawBoard();
