98401eb21070b19c5ac918772255b25844c401a6
Peter Seibel <peter@gigamonkeys.com>
2023-02-22 19:51:33 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -58,6 +58,35 @@ const findWinner = () => {
   return null;
 };
 
+const drawWinningLine = (winner) => {
+  const [r1, c1] = winner[0];
+  const [r2, c2] = winner[winner.length - 1];
+
+  const x1 = boardLeft + c1 * cellSize + cellSize / 2;
+  const y1 = boardTop + r1 * cellSize + cellSize / 2;
+  const x2 = boardLeft + c2 * cellSize + cellSize / 2;
+  const y2 = boardTop + r2 * cellSize + cellSize / 2;
+
+  let adjX1 = x1;
+  let adjX2 = x2;
+  let adjY1 = y1;
+  let adjY2 = y2;
+
+  if (y1 === y2 || x1 !== x2) {
+    adjX1 -= lineEndAdjustment;
+    adjX2 += lineEndAdjustment;
+  }
+
+  if (x1 === x2 || y1 !== y2) {
+    const slope = y1 < y2 ? 1 : -1;
+    adjY1 -= (slope * lineEndAdjustment);
+    adjY2 += (slope * lineEndAdjustment);
+  }
+
+  drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
+};
+
+
 registerOnclick((x, y) => {
 
   let winner = findWinner();
@@ -80,32 +109,7 @@ registerOnclick((x, y) => {
     // Check if there's a winner now
     winner = findWinner();
     if (winner !== null) {
-      // Draw the line through three in a row
-      const [r1, c1] = winner[0];
-      const [r2, c2] = winner[winner.length - 1];
-
-      const x1 = boardLeft + c1 * cellSize + cellSize / 2;
-      const y1 = boardTop + r1 * cellSize + cellSize / 2;
-      const x2 = boardLeft + c2 * cellSize + cellSize / 2;
-      const y2 = boardTop + r2 * cellSize + cellSize / 2;
-
-      let adjX1 = x1;
-      let adjX2 = x2;
-      let adjY1 = y1;
-      let adjY2 = y2;
-
-      if (y1 === y2 || x1 !== x2) {
-        adjX1 -= lineEndAdjustment;
-        adjX2 += lineEndAdjustment;
-      }
-
-      if (x1 === x2 || y1 !== y2) {
-        const slope = y1 < y2 ? 1 : -1;
-        adjY1 -= (slope * lineEndAdjustment);
-        adjY2 += (slope * lineEndAdjustment);
-      }
-
-      drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
+      drawWinningLine(winner);
     }
   }
 });
