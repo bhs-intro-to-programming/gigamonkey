98dac74aada7bcd183eeaffc73eb2ff3dfcd7d5f
Peter Seibel <peter@gigamonkeys.com>
2023-02-23 13:06:21 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -1,6 +1,6 @@
 const drawPicture = (horizon, base, size) => {
   drawBackground(horizon);
-  drawSnowman(width/2, base, size, [ 3, 5, 8 ]);
+  drawSnowman(width/2, base, size, [ 4, 5, 8 ]);
 };
 
 const drawBackground = (horizon) => {
