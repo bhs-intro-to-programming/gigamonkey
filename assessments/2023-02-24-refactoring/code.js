bf664e9cf0a185831e11f26142ec754c05ffbd57
Peter Seibel <peter@gigamonkeys.com>
2023-02-23 10:53:51 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -11,7 +11,7 @@ const drawBackground = () => {
 
 const drawSnowman = (x, size) => {
   // Snowman at base of height size
-  const headSize = size * 0.25;
+  const headSize = size * 0.15;
   const bodySize = size * 0.75;
   const headY = height * 0.2;
   drawHead(x, headY, headSize);
