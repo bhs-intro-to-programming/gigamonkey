e344c6e8c73dd1afafc366cc7870cf9a8b04459c
Peter Seibel <peter@gigamonkeys.com>
2023-02-23 09:13:31 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -7,6 +7,7 @@ const drawHead = (x, y, size) => {
   drawCircle(x, y, size, 'black');
   drawFilledCircle(x - size * 0.25, y - size * 0.25, 4, 'black');
   drawFilledCircle(x + size * 0.25, y - size * 0.25, 4, 'black');
+  drawFilledTriangle(x, y, x + 30, y + 5, x, y + 2, 'orange')
 }
 
 drawSnowman();
