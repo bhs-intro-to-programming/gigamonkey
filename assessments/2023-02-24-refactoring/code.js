600522dd2c5dc669b45d465e75a5131ba322e9d0
Peter Seibel <peter@gigamonkeys.com>
2023-02-23 09:34:06 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -39,7 +39,7 @@ const drawArms = (x, y, size) => {
 const drawArm = (x, y, size, direction) => {
   const x1 = x + size * 0.45 * direction;
   const y1 = y - size * 0.2;
-  const x2 = x1 + size * 1.5 * direction;
+  const x2 = x1 + size * 1.75 * direction;
   const y2 = y1 - 30;
   drawLine(x1, y1, x2, y2, 'black', 3);
 };
