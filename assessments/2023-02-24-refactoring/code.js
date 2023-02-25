2de1866f3ab245bc7a5fcd07d94be839c46db310
Peter Seibel <peter@gigamonkeys.com>
2023-02-25 13:23:01 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -1,10 +1,11 @@
-const drawBackground=(horizon)=>{
+const drawPicture = (horizon, base, size) => {
+
+  // Draw the background
   drawFilledRect(0, 0, width, horizon, '#ddeeff');
   drawFilledRect(0, horizon, width, height, 'white');
   drawLine(0, horizon, width, horizon, '#bbb');
-}
 
-const drawSnowman=()=>{
+  // Draw the snowman
   const x = width / 2;
   const proportions = [3, 4, 5];
   const [headP, torsoP, buttP] = proportions;
@@ -17,29 +18,29 @@ const drawSnowman=()=>{
   const headY = (base - size) + headSize / 2;
   const torsoY = headY + headSize / 2 + torsoSize / 2;
   const buttY = torsoY + torsoSize / 2 + buttSize / 2;
-}
-
-const drawHead=()=>{
   const headRadius = headSize / 2;
+  // Draw the head
+ const drawHead =(x) => {
   drawCircle(x, headY, headRadius + 2, 'black', 3);
   drawFilledCircle(x, headY, headRadius, 'white', 3);
-}
- const drawEyes=()=>{
+ };
+const drawEyes =(x) => {
   const eyeSpacing = headRadius * 0.25;
   drawFilledCircle(x - eyeSpacing, headY - eyeSpacing, 4, 'black');
   drawFilledCircle(x + eyeSpacing, headY - eyeSpacing, 4, 'black');
-
-const drawNose=()=>{
+}
+  // Draw the nose
   const noseLength = headRadius * 0.8;
   drawFilledTriangle(x, headY, x + noseLength, headY + noseLength * 0.2, x, headY + noseLength * 0.3, 'orange');
 
-  const drawMouth=()=>{
+ const drawMoulth = (dy) =>{
   for (let i = 0; i < 5; i++) {
     const dy = -2 * (2.1 ** Math.abs(i - 2));
     drawFilledCircle(x - (i - 2.3) * headRadius * 0.21, headY + headRadius * 0.65 + dy, 4, 'black');
   }
+ };
 
- const drawHat=()=>{
+  // Draw the hat
   const brimTop = headY - headRadius * 0.9;
   const brimWidth = headRadius * 2.25;
   const brimHeight = brimWidth * 0.08;
@@ -47,54 +48,29 @@ const drawNose=()=>{
   const hatHeight = headRadius * 1.25;
   drawFilledRect(x - brimWidth / 2, brimTop, brimWidth, brimHeight, 'black');
   drawFilledRect(x - hatWidth / 2, brimTop - hatHeight, hatWidth, hatHeight, 'black');
- }
-  const drawTorso=()=>{
+
+  // Draw the torso
   const torsoRadius = torsoSize / 2;
   drawCircle(x, torsoY, torsoRadius + 2, 'black', 3);
   drawFilledCircle(x, torsoY, torsoRadius, 'white', 3);
-  }
-  const drawArms=()=>{
-  let x1 = x + torsoRadius * 0.6;
-  let x2 = x + torsoRadius * 2.35;
-  drawLine(x1, torsoY - torsoRadius * 0.25, x2, torsoY - torsoRadius * 0.85, 'black', 3);
-  x1 = x + torsoRadius * 0.6 * -1;
-  x2 = x + torsoRadius * 2.35 * -1;
-  drawLine(x1, torsoY - torsoRadius * 0.25, x2, torsoY - torsoRadius * 0.85, 'black', 3);
-  }
- const drawButtons=()=>{
+
+  const drawArms= (x) => {
+     x1 = x + torsoRadius * 0.6 *-1;
+     x2 = x + torsoRadius * 2.35 *-1;
+     drawline(x1, torsoY - torsoRadius * 0.25, x2, torsoY - torsoRadius * 0.85, 'black', 3);{
+    
+  } return drawArms;
+}
+
+  // Draw the buttons
   for (let i = 0; i < 3; i++) {
     drawFilledCircle(x, torsoY - torsoRadius * 0.5 + i * torsoRadius * 0.5, 4, 'black');
   }
 
-  const drawButt=()=>{
+  // Draw the butt
   const buttRadius = buttSize / 2;
   drawCircle(x, buttY, buttRadius + 2, 'black', 3);
   drawFilledCircle(x, buttY, buttRadius, 'white', 3);
-
-const drawPicture = (horizon, base, size) => {
-
-drawBackground(horizon)
-
-drawSnowman()
-
-drawNose()
-
-drawHead()
-
-drawEyes()
-
-drawMout()
-
-drawHat()
-
-drawTorso()
-
-drawArms()
-
-drawButtons()
-
-drawButt()
-
 };
 
 drawPicture(height * 0.7, height * 0.9, height * 0.7);
