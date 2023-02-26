49deebd63c6009a73a68c7195dab8e419324603f
Peter Seibel <peter@gigamonkeys.com>
2023-02-26 14:38:01 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -1,3 +1,4 @@
+
 const boardSize = Math.min(width, height) * 0.75;
 const boardLeft = (width - boardSize) / 2;
 const boardTop = (height - boardSize) / 2;
@@ -38,56 +39,63 @@ const drawBoard = () => {
   drawLine(x2, boardTop, x2, boardTop + boardSize, 'grey', 2);
   drawLine(boardLeft, y1, boardLeft + boardSize, y1, 'grey', 2);
   drawLine(boardLeft, y2, boardLeft + boardSize, y2, 'grey', 2);
+}
+
+const winner = (i, p) => {
+  let r = lines[i][p][0]
+  let c = lines[i][p][1]
+  return board[r][c]
 };
 
+
 const findWinner = () => {
-  // Check if there's a winner already.
   for (let i = 0; i < lines.length; i++) {
-    let r = lines[i][0][0];
-    let c = lines[i][0][1];
-    const m0 = board[r][c];
-    r = lines[i][1][0];
-    c = lines[i][1][1];
-    const m1 = board[r][c];
-    r = lines[i][2][0];
-    c = lines[i][2][1];
-    const m2 = board[r][c];
+    const m0 = winner(i, 0)
+    const m1 = winner(i, 1)
+    const m2 = winner(i, 2)
     if (m0 !== '' && m0 === m1 && m0 === m2) {
       return lines[i];
     }
   }
   return null;
-  //empty value for arrays and objects 
+}
+
+
+
+
+const threeInARow = (winner) => {
+
+  //let winner = findWinner
+  if (winner !== null) {
+
+
+    const [r1, c1] = winner[0];
+    const [r2, c2] = winner[winner.length - 1];
+
+    const x1 = boardLeft + c1 * cellSize + cellSize / 2;
+    const y1 = boardTop + r1 * cellSize + cellSize / 2;
+    const x2 = boardLeft + c2 * cellSize + cellSize / 2;
+    const y2 = boardTop + r2 * cellSize + cellSize / 2;
+
+    let adjX1 = x1;
+    let adjX2 = x2;
+    let adjY1 = y1;
+    let adjY2 = y2;
+
+    if (y1 === y2 || x1 !== x2) {
+      adjX1 -= lineEndAdjustment;
+      adjX2 += lineEndAdjustment;
+    }
+
+    if (x1 === x2 || y1 !== y2) {
+      const slope = y1 < y2 ? 1 : -1;
+      adjY1 -= (slope * lineEndAdjustment);
+      adjY2 += (slope * lineEndAdjustment);
+    }
+    drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
+  }
 };
 
-
-const drawWinningLine = (winner) => {
-  const [r1, c1] = winner[0];
-  const [r2, c2] = winner[winner.length - 1];
-
-  const x1 = boardLeft + c1 * cellSize + cellSize / 2;
-  const y1 = boardTop + r1 * cellSize + cellSize / 2;
-  const x2 = boardLeft + c2 * cellSize + cellSize / 2;
-  const y2 = boardTop + r2 * cellSize + cellSize / 2;
-
-  let adjX1 = x1;
-  let adjX2 = x2;
-  let adjY1 = y1;
-  let adjY2 = y2;
-
-  if (y1 === y2 || x1 !== x2) {
-    adjX1 -= lineEndAdjustment;
-    adjX2 += lineEndAdjustment;
-  }
-
-  if (x1 === x2 || y1 !== y2) {
-    const slope = y1 < y2 ? 1 : -1;
-    adjY1 -= (slope * lineEndAdjustment);
-    adjY2 += (slope * lineEndAdjustment);
-  }
-
-  drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
-};
 
 const makeMove = (r, c) => {
   const marker = move % 2 === 0 ? 'X' : 'O';
@@ -97,31 +105,23 @@ const makeMove = (r, c) => {
   drawText(marker, x - (fontSize * 0.3 + nudge), y + fontSize * 0.3, 'black', fontSize);
   board[r][c] = marker;
   move++;
-};
-
-//game isn't over
-const gameOver = () => findWinner() !== null || move >= 9
-
-//legal move 
-const isLegalMove = (r, c) => {
-  return 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '';
-};
+}
 
 registerOnclick((x, y) => {
+
+  let winner = findWinner()
 
   const r = Math.floor((y - boardTop) / cellSize);
   const c = Math.floor((x - boardLeft) / cellSize);
 
-  if (!gameOver()) {
-    if (isLegalMove(r, c)) {
-      makeMove(r, c);
-      
-      //EXTRACTING ARGUMENTS is sorta just making it a top level function, then doing the const thing, putting the local variables in the arguments, and calling it again with the locals arguments at the bottom, very repetitive process.
-      // Check if there's a winner now
-      let winner = findWinner();
-      if (winner !== null) {
-        drawWinningLine(winner);
-      }
+  // Only do anything if it's a legal move and the game isn't over.
+  if (winner === null && 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '') {
+
+    makeMove(r, c)
+
+    winner = findWinner()
+    if (winner !== null) {
+      threeInARow(winner)
     }
   }
 });
