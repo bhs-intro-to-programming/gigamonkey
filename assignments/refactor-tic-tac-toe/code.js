11ccaf18584e1e7580674a348893b8234c2f7f86
Peter Seibel <peter@gigamonkeys.com>
2023-02-26 14:26:25 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -29,112 +29,87 @@ const lines = [
   [[2, 0], [1, 1], [0, 2]],
 ];
 
-const drawTheBoard = () => {
-  for (let i = 1; i <= 2; i++) {
-    drawVerticalLine(boardLeft + i * cellSize);
-    drawHorizontalLine(boardTop + i * cellSize);
-  }
+const makeBoard = () => {
+  const x1 = boardLeft + cellSize;
+  const x2 = boardLeft + 2 * cellSize;
+  const y1 = boardTop + cellSize;
+  const y2 = boardTop + 2 * cellSize;;
+  drawLine(x1, boardTop, x1, boardTop + boardSize, 'grey', 2);
+  drawLine(x2, boardTop, x2, boardTop + boardSize, 'grey', 2);
+  drawLine(boardLeft, y1, boardLeft + boardSize, y1, 'grey', 2);
+  drawLine(boardLeft, y2, boardLeft + boardSize, y2, 'grey', 2);
 };
 
-const drawVerticalLine = (x) => {
-  drawLine(x, boardTop, x, boardTop + boardSize, 'grey', 2);
-};
-
-const drawHorizontalLine = (y) => {
-  drawLine(boardLeft, y, boardLeft + boardSize, y, 'grey', 2);
-};
-
-const getMark = (line, p) => {
-  let r = line[p][0];
-  let c = line[p][1];
-  return board[r][c];
-};
-
-const getMarks = (line) => {
-  return [
-    getMark(line, 0),
-    getMark(line, 1),
-    getMark(line, 2),
-  ];
-};
-
-const allTheSame = (marks) => marks[0] === marks[1] && marks[0] === marks[2];
-
-const isWinner = (line) => {
-  const marks = getMarks(line);
-  return marks[0] !== '' && allTheSame(marks);
-}
-
-const findWinner = () => {
-  for (let i = 0; i < lines.length; i++) {
-    if (isWinner(lines[i])) {
-      return lines[i];
+const checkForWinner = () => {
+   for (let i = 0; i < lines.length; i++) {
+    let r = lines[i][0][0];
+    let c = lines[i][0][1];
+    const m0 = board[r][c];
+    r = lines[i][1][0];
+    c = lines[i][1][1];
+    const m1 = board[r][c];
+    r = lines[i][2][0];
+    c = lines[i][2][1];
+    const m2 = board[r][c];
+    if (m0 !== '' && m0 === m1 && m0 === m2) {
+     return lines[i];
     }
   }
-  return null;
-};
-
-const drawWinningLine = (winner) => {
-  const [r1, c1] = winner[0];
-  const [r2, c2] = winner[winner.length - 1];
-
-  const x1 = boardLeft + c1 * cellSize + cellSize / 2;
-  const y1 = boardTop + r1 * cellSize + cellSize / 2;
-  const x2 = boardLeft + c2 * cellSize + cellSize / 2;
-  const y2 = boardTop + r2 * cellSize + cellSize / 2;
-
-  let adjX1 = x1;
-  let adjX2 = x2;
-  let adjY1 = y1;
-  let adjY2 = y2;
-
-  if (y1 === y2 || x1 !== x2) {
-    adjX1 -= lineEndAdjustment;
-    adjX2 += lineEndAdjustment;
-  }
-
-  if (x1 === x2 || y1 !== y2) {
-    const slope = y1 < y2 ? 1 : -1;
-    adjY1 -= (slope * lineEndAdjustment);
-    adjY2 += (slope * lineEndAdjustment);
-  }
-
-  drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
-};
-
-const makeMove = (r, c) => {
-  const marker = move % 2 === 0 ? 'X' : 'O';
-  const x = boardLeft + c * cellSize + cellSize / 2;
-  const y = boardTop + r * cellSize + cellSize / 2;
-  const nudge = marker === 'O' ? cellSize / 9 : cellSize / 19;
-  drawText(marker, x - (fontSize * 0.3 + nudge), y + fontSize * 0.3, 'black', fontSize);
-  board[r][c] = marker;
-  move++;
-};
-
-const gameOver = () => findWinner() !== null || move === 9;
-
-const isLegalMove = (r, c) => {
-  return 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '';
-};
-
-const maybeDrawWinningLine = (winner) => {
-  if (winner !== null) {
-    drawWinningLine(winner);
-  }
-};
+}
 
 registerOnclick((x, y) => {
 
-  const r = Math.floor((y - boardTop) / cellSize);
-  const c = Math.floor((x - boardLeft) / cellSize);
+  let winner = null;
+  let r;
+  let c;
 
-  if (!gameOver()) {
-    if (isLegalMove(r, c)) {
-      makeMove(r, c);
-      maybeDrawWinningLine(findWinner());
+
+  r = Math.floor((y - boardTop) / cellSize);
+  c = Math.floor((x - boardLeft) / cellSize);
+
+  // Only do anything if it's a legal move and the game isn't over.
+  if (winner === null && 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '') {
+
+    // Draw the mark and record the move
+    const markRecord = () => {
+      const marker = move % 2 === 0 ? 'X' : 'O';
+      const x = boardLeft + c * cellSize + cellSize / 2;
+      const y = boardTop + r * cellSize + cellSize / 2;
+      const nudge = marker === 'O' ? cellSize / 9 : cellSize / 19;
+      drawText(marker, x - (fontSize * 0.3 + nudge), y + fontSize * 0.3, 'black', fontSize);
+      board[r][c] = marker;
+      move++;
+    }; markRecord()
+
+
+    checkForWinner()
+    if (winner !== null) {
+      // Draw the line through three in a row
+      const [r1, c1] = winner[0];
+      const [r2, c2] = winner[winner.length - 1];
+
+      const x1 = boardLeft + c1 * cellSize + cellSize / 2;
+      const y1 = boardTop + r1 * cellSize + cellSize / 2;
+      const x2 = boardLeft + c2 * cellSize + cellSize / 2;
+      const y2 = boardTop + r2 * cellSize + cellSize / 2;
+
+      let adjX1 = x1;
+      let adjX2 = x2;
+      let adjY1 = y1;
+      let adjY2 = y2;
+
+      if (y1 === y2 || x1 !== x2) {
+        adjX1 -= lineEndAdjustment;
+        adjX2 += lineEndAdjustment;
+      }
+
+      if (x1 === x2 || y1 !== y2) {
+        const slope = y1 < y2 ? 1 : -1;
+        adjY1 -= (slope * lineEndAdjustment);
+        adjY2 += (slope * lineEndAdjustment);
+      }
+
+      drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
     }
   }
-});
-
-drawTheBoard();
+}); makeBoard()
