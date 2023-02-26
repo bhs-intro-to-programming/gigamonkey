3eb91f1a12139992599b67f4c6de7d69341daee5
Peter Seibel <peter@gigamonkeys.com>
2023-02-25 17:01:40 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -28,7 +28,7 @@ const drawHead = (x, y, radius, eyeSpacing) => {
   drawEyes(x, y, radius, eyeSpacing);
   drawNose(x, y, radius);
   drawMouth(x, y,radius);
-  drawHat(x, y, y - radius * 0.9, radius * 2.25);
+  drawHat(x, y, y - radius * 0.9, radius * 2.25, radius * 1.25);
 };
 
 const drawSnowball = (x, y, r) => {
@@ -58,10 +58,9 @@ const drawMouth = (x, headY, headRadius) => {
   }
 };
 
-const drawHat = (x, y, brimTop, brimWidth) => {
+const drawHat = (x, y, brimTop, brimWidth, hatHeight) => {
   const brimHeight = brimWidth * 0.08;
   const hatWidth = brimWidth * 0.7;
-  const hatHeight = r * 1.25;
   drawFilledRect(x - brimWidth / 2, brimTop, brimWidth, brimHeight, 'black');
   drawFilledRect(x - hatWidth / 2, brimTop - hatHeight, hatWidth, hatHeight, 'black');
 };
