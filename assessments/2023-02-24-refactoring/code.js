5d5c97443750ec00f0b823d6dcc898229dc063af
Peter Seibel <peter@gigamonkeys.com>
2023-02-25 19:19:42 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -35,13 +35,7 @@ const drawHead = (x, y, size) => {
 
   drawEyes(x, y, r);
   drawNose(x, y, r);
-
- 
-  // Draw the mouth
-  for (let i = 0; i < 5; i++) {
-    const dy = -2 * (2.1 ** Math.abs(i - 2));
-    drawFilledCircle(x - (i - 2.3) * r * 0.21, y + r * 0.65 + dy, 4, 'black');
-  }
+  drawMouth(x, y, r);
 
   // Draw the hat
   const brimTop = y - r * 0.9;
@@ -62,8 +56,14 @@ const drawEyes = (x, y, r) => {
 const drawNose = (x, y, r)=> {
   const noseLength = r * 0.8;
   drawFilledTriangle(x, y, x + noseLength, y + noseLength * 0.2, x, y + noseLength * 0.3, 'orange');
+};
 
-}
+const drawMouth = (x, y, r) => {
+  for (let i = 0; i < 5; i++) {
+    const dy = -2 * (2.1 ** Math.abs(i - 2));
+    drawFilledCircle(x - (i - 2.3) * r * 0.21, y + r * 0.65 + dy, 4, 'black');
+  }
+};
 
 const drawTorso = (x, y, size) => {
   const torsoRadius = size / 2;
