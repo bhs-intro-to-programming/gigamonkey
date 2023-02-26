2df637c3e48a3c894249a3175b3c72b4bccf350a
Peter Seibel <peter@gigamonkeys.com>
2023-02-26 14:35:16 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -29,7 +29,7 @@ const lines = [
   [[2, 0], [1, 1], [0, 2]],
 ];
 
-const makeBoard = () => {
+const drawBoard = () => {
   const x1 = boardLeft + cellSize;
   const x2 = boardLeft + 2 * cellSize;
   const y1 = boardTop + cellSize;
@@ -40,8 +40,9 @@ const makeBoard = () => {
   drawLine(boardLeft, y2, boardLeft + boardSize, y2, 'grey', 2);
 };
 
-const checkForWinner = () => {
-   for (let i = 0; i < lines.length; i++) {
+const findWinner = () => {
+  // Check if there's a winner already.
+  for (let i = 0; i < lines.length; i++) {
     let r = lines[i][0][0];
     let c = lines[i][0][1];
     const m0 = board[r][c];
@@ -52,64 +53,77 @@ const checkForWinner = () => {
     c = lines[i][2][1];
     const m2 = board[r][c];
     if (m0 !== '' && m0 === m1 && m0 === m2) {
-     return lines[i];
+      return lines[i];
     }
   }
-}
+  return null;
+  //empty value for arrays and objects 
+};
+
+
+const drawWinningLine = (winner) => {
+  const [r1, c1] = winner[0];
+  const [r2, c2] = winner[winner.length - 1];
+
+  const x1 = boardLeft + c1 * cellSize + cellSize / 2;
+  const y1 = boardTop + r1 * cellSize + cellSize / 2;
+  const x2 = boardLeft + c2 * cellSize + cellSize / 2;
+  const y2 = boardTop + r2 * cellSize + cellSize / 2;
+
+  let adjX1 = x1;
+  let adjX2 = x2;
+  let adjY1 = y1;
+  let adjY2 = y2;
+
+  if (y1 === y2 || x1 !== x2) {
+    adjX1 -= lineEndAdjustment;
+    adjX2 += lineEndAdjustment;
+  }
+
+  if (x1 === x2 || y1 !== y2) {
+    const slope = y1 < y2 ? 1 : -1;
+    adjY1 -= (slope * lineEndAdjustment);
+    adjY2 += (slope * lineEndAdjustment);
+  }
+
+  drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
+};
+
+const makeMove = (r, c) => {
+  const marker = move % 2 === 0 ? 'X' : 'O';
+  const x = boardLeft + c * cellSize + cellSize / 2;
+  const y = boardTop + r * cellSize + cellSize / 2;
+  const nudge = marker === 'O' ? cellSize / 9 : cellSize / 19;
+  drawText(marker, x - (fontSize * 0.3 + nudge), y + fontSize * 0.3, 'black', fontSize);
+  board[r][c] = marker;
+  move++;
+};
+
+//game isn't over
+const gameOver = () => findWinner() !== null || move < 9
+
+//legal move 
+const isLegalMove = (r, c) => {
+  return 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '';
+};
 
 registerOnclick((x, y) => {
 
-  let winner = null;
-  let r;
-  let c;
+  const r = Math.floor((y - boardTop) / cellSize);
+  const c = Math.floor((x - boardLeft) / cellSize);
 
-
-  r = Math.floor((y - boardTop) / cellSize);
-  c = Math.floor((x - boardLeft) / cellSize);
-
-  // Only do anything if it's a legal move and the game isn't over.
-  if (winner === null && 0 <= r && r < 3 && 0 <= c && c < 3 && board[r][c] === '') {
-
-    // Draw the mark and record the move
-    const markRecord = () => {
-      const marker = move % 2 === 0 ? 'X' : 'O';
-      const x = boardLeft + c * cellSize + cellSize / 2;
-      const y = boardTop + r * cellSize + cellSize / 2;
-      const nudge = marker === 'O' ? cellSize / 9 : cellSize / 19;
-      drawText(marker, x - (fontSize * 0.3 + nudge), y + fontSize * 0.3, 'black', fontSize);
-      board[r][c] = marker;
-      move++;
-    }; markRecord()
-
-
-    winner = checkForWinner()
-    if (winner) {
-      // Draw the line through three in a row
-      const [r1, c1] = winner[0];
-      const [r2, c2] = winner[winner.length - 1];
-
-      const x1 = boardLeft + c1 * cellSize + cellSize / 2;
-      const y1 = boardTop + r1 * cellSize + cellSize / 2;
-      const x2 = boardLeft + c2 * cellSize + cellSize / 2;
-      const y2 = boardTop + r2 * cellSize + cellSize / 2;
-
-      let adjX1 = x1;
-      let adjX2 = x2;
-      let adjY1 = y1;
-      let adjY2 = y2;
-
-      if (y1 === y2 || x1 !== x2) {
-        adjX1 -= lineEndAdjustment;
-        adjX2 += lineEndAdjustment;
+  if (!gameOver()) {
+    if (isLegalMove(r, c)) {
+      makeMove(r, c);
+      
+      //EXTRACTING ARGUMENTS is sorta just making it a top level function, then doing the const thing, putting the local variables in the arguments, and calling it again with the locals arguments at the bottom, very repetitive process.
+      // Check if there's a winner now
+      let winner = findWinner();
+      if (winner !== null) {
+        drawWinningLine(winner);
       }
-
-      if (x1 === x2 || y1 !== y2) {
-        const slope = y1 < y2 ? 1 : -1;
-        adjY1 -= (slope * lineEndAdjustment);
-        adjY2 += (slope * lineEndAdjustment);
-      }
-
-      drawLine(adjX1, adjY1, adjX2, adjY2, 'red', 15);
     }
   }
-}); makeBoard()
+});
+
+drawBoard();
