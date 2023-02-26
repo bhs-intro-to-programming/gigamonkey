1ab2e3b428cad81b5c7db4f0f2d63abc70f53e54
Peter Seibel <peter@gigamonkeys.com>
2023-02-25 19:20:14 -0800

    Updating assessments/2023-02-24-refactoring/code.js

--- assessments/2023-02-24-refactoring/code.js
+++ assessments/2023-02-24-refactoring/code.js
@@ -36,15 +36,7 @@ const drawHead = (x, y, size) => {
   drawEyes(x, y, r);
   drawNose(x, y, r);
   drawMouth(x, y, r);
-
-  // Draw the hat
-  const brimTop = y - r * 0.9;
-  const brimWidth = r * 2.25;
-  const brimHeight = brimWidth * 0.08;
-  const hatWidth = brimWidth * 0.7;
-  const hatHeight = r * 1.25;
-  drawFilledRect(x - brimWidth / 2, brimTop, brimWidth, brimHeight, 'black');
-  drawFilledRect(x - hatWidth / 2, brimTop - hatHeight, hatWidth, hatHeight, 'black');
+  drawHat(x, y, r);
 };
 
 const drawEyes = (x, y, r) => {
@@ -63,6 +55,16 @@ const drawMouth = (x, y, r) => {
     const dy = -2 * (2.1 ** Math.abs(i - 2));
     drawFilledCircle(x - (i - 2.3) * r * 0.21, y + r * 0.65 + dy, 4, 'black');
   }
+};
+
+const drawHat = (x, y, r) => {
+  const brimTop = y - r * 0.9;
+  const brimWidth = r * 2.25;
+  const brimHeight = brimWidth * 0.08;
+  const hatWidth = brimWidth * 0.7;
+  const hatHeight = r * 1.25;
+  drawFilledRect(x - brimWidth / 2, brimTop, brimWidth, brimHeight, 'black');
+  drawFilledRect(x - hatWidth / 2, brimTop - hatHeight, hatWidth, hatHeight, 'black');
 };
 
 const drawTorso = (x, y, size) => {
