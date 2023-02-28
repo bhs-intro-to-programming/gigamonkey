b22193fe5aef02ef4871b139f1d7fedc9625e20e
Peter Seibel <peter@gigamonkeys.com>
2023-02-27 17:42:14 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,19 +1,25 @@
 // N.B. Do not use the array methods of the same name to implement these functions!
 
 const filter = (predicate, array) => {
+ return array.filter(predicate)
 };
 
 const map = (fn, array) => {
+  return array.map(fn)
 };
 
 const flatMap = (fn, array) => {
+  return array.flatMap(fn)
 };
 
 const reduce = (fn, initialValue, array) => {
+  return array.reduce(initialValue(fn))
 };
 
 const every = (predicate, array) => {
+  return array.every(predicate)
 };
 
 const some = (predicate, array) => {
+  return array.some(predicate)
 };
