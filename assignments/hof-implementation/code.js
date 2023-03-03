418de49fc4e320d955f4711cd928ceebf40c4f31
Peter Seibel <peter@gigamonkeys.com>
2023-03-02 17:03:36 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,31 +1,57 @@
 // N.B. Do not use the array methods of the same name to implement these functions!
 
 const filter = (predicate, array) => {
- return array.filter(predicate)
+  let list = []
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i])){
+      list.push(array[i])
+    }
+  }
+  return list
 };
 
 const map = (fn, array) => {
-  return array.map(fn)
-};
-
-const flatMap = (fn, array) => {
-  const r = [];
-  for (const a of array) {
-    for (const x of fn(a)) {
-      r.push(x);
-    }
+  let list = []
+  for (let i = 0; i < array.length; i++) {
+    list.push(fn(array[i]))
   }
-  return r;
+  return list
 };
 
 const reduce = (fn, initialValue, array) => {
-  return array.reduce(fn, initialValue)
+  let number = initialValue
+  for (let i = 0; i < array.length; i++) {
+    number = fn(number, array[i])
+  }
+  return number
+};
+
+const flatMap = (fn, array) => {
+  let list = []
+  
+  for ( let i = 0; i < array.length; i++) {
+    let abc = fn(array[i])
+    for (let l = 0; l < abc.length; l++) {
+      list.push(abc[l])
+    }
+  }
+  return list
 };
 
 const every = (predicate, array) => {
-  return array.every(predicate)
+  for (let i = 0; i < array.length; i++) {
+    if (!predicate(array[i])) {
+      return false
+    }
+  }
+  return true
 };
 
 const some = (predicate, array) => {
-  return array.some(predicate)
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      return true
+    }
+  }
+  return false
 };
