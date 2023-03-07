4b9585b7f1c9e8900b27c4ad663a9ca90fbee6c1
Peter Seibel <peter@gigamonkeys.com>
2023-03-06 20:23:43 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -17,13 +17,19 @@ for (let i = 1; i <= 3; i++) {
 let i = 0
 registerOnclick((x, y) => {
   if (i % 2 == 0) {
+    let xx;
     if (x < (width / 3)) {
-      drawText('X', width / 6, y, 'black', Math.min(width, height) * 0.3);
+      //drawText('X', width / 6, y, 'black', Math.min(width, height) * 0.3);
+      xx = width / 6;
     } else if ((width / 3) < x < (2 * width / 3)) {
-      drawText('X', width / 2, y, 'black', Math.min(width, height) * 0.3);
+      //drawText('X', width / 2, y, 'black', Math.min(width, height) * 0.3);
+      xx = width / 2;
     } else {
-      drawText('X', 5 * width / 6, y, 'black', Math.min(width, height) * 0.3);
+      // drawText('X', 5 * width / 6, y, 'black', Math.min(width, height) * 0.3);
+      xx = 5 * width / 6;
     }
+    drawText('X', xx, y, 'black', Math.min(width, height) * 0.3);
+
     i++
   } else {
     drawText('O', x, y, 'black', Math.min(width, height) * 0.3);
