41c576f23b0fe503f7f29737555c3518a19da6c0
Peter Seibel <peter@gigamonkeys.com>
2023-03-08 10:10:29 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,9 +1,17 @@
-const filter = (fn, array) => {
+const filter = (predicate, array) => {
   let r = [];
   for (let i = 0; i < array.length; i++) {
-    if (fn(array[i])) {
+    if (predicate(array[i])) {
       r.push(array[i]);
     }
+  }
+  return r;
+};
+
+const map = (fn, array) => {
+  let r = [];
+  for (let i = 0; i < array.length; i++) {
+    r.push(fn(array[i]));    
   }
   return r;
 };
