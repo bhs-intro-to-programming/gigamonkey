9cf1f85996d83cf5b30ed86faff8f74892a47bde
Peter Seibel <peter@gigamonkeys.com>
2023-03-09 12:33:36 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,68 +1,217 @@
-const CELLSIZE = 4;
-const ROWS = Math.floor(height / CELLSIZE)
-const COLS = Math.floor(width / CELLSIZE)
+let SIZE = 4;
+let P = 0.2;
+let RATE = 0;
 
-const ALL_OFFSETS = Array(8).fill().map((_, i) => i / 4 * Math.PI).map((a) => {
-  const offset = (n) => Math.sign(Math.round(n * 10));
-  return {
-    row: offset(Math.sin(a)),
-    col: offset(Math.cos(a))
-  };
-});
+const H_BLINKER = `###`;
 
-const emptyGrid = (rows, cols) => Array(rows).fill().map(() => Array(cols).fill(false));
+const V_BLINKER = '#\n#\n#';
 
-const originalCells = (rows, cols) => {
-  const cells = emptyGrid(rows, cols);
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      if (Math.random() < 0.23) {
-        cells[y][x] = true
+const BLOCK = '##\n##';
+
+const GLIDER = `
+#..
+.##
+##.
+`;
+
+const PULSAR = `
+..OOO...OOO..
+.............
+O....O.O....O
+O....O.O....O
+O....O.O....O
+..OOO...OOO..
+.............
+..OOO...OOO..
+O....O.O....O
+O....O.O....O
+O....O.O....O
+.............
+..OOO...OOO..
+`;
+
+const GOSPER_GG = `
+........................#...........
+......................#.#...........
+............##......##............##
+...........#...#....##............##
+##........#.....#...##..............
+##........#...#.##....#.#...........
+..........#.....#.......#...........
+...........#...#....................
+............##......................
+`;
+
+const BUTTERFLY = `
+....##.....
+...#..#....
+....#.#....
+.#...#.##..
+#.#...#..#.
+#..#...#.#.
+.##.#...#..
+...#.#.....
+...#..#....
+....##.....
+`;
+
+const columns = Math.floor(width / SIZE);
+const rows = Math.floor(height / SIZE);
+const xoffset = (width - columns * SIZE) / 2;
+const yoffset = (height - rows * SIZE) / 2;
+
+const render = (grid) => {
+  drawFilledRect(0, 0, width, height, 'black');
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c]) {
+        fillCell(r, c);
       }
     }
   }
-  return cells;
 };
 
-const drawCells = (cells) => {
-  for (let r = 0; r < cells.length; r++) {
-    for (let c = 0; c < cells[0].length; c++) {
-      const color = cells[r][c] ? 'green' : 'black';
-      drawFilledRect(c * CELLSIZE, r * CELLSIZE, CELLSIZE, CELLSIZE, color);
-    }
-  }
+const fillCell = (r, c) => {
+  drawFilledRect(xoffset + c * SIZE, yoffset + r * SIZE, SIZE, SIZE, 'rgb(0, 255, 0)');
 };
 
-const nextCells = (cells) => {
-  const next = emptyGrid(cells.length, cells[0].length)
-  for (let i = 0; i < next.length; i++) {
-    for (let j = 0; j < next[0].length; j++) {
-      const livingNeighbors = countLivingNeighbors(i, j, cells)
-      if (cells[i][j]) {
-        next[i][j] = 2 <= livingNeighbors && livingNeighbors <= 3;
-      } else {
-        next[i][j] = livingNeighbors === 3;
-      }
+const nextGeneration = (grid) => {
+  const next = newGrid();
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      next[r][c] = isAlive(grid, r, c);
     }
   }
   return next;
 };
 
-const neighborOffsets = (row, col) => {
-  const rowOk = (o) => row === 0 ? o.row >= 0 : row === ROWS - 1 ? o.row <= 0 : true;
-  const colOk = (o) => col === 0 ? o.col >= 0 : col === COLS - 1 ? o.col <= 0 : true;
-  return ALL_OFFSETS.filter(o => rowOk(o) && colOk(o));
+const isAlive = (grid, r, c) => {
+  const n = livingNeighbors(grid, r, c);
+  return n === 3 || grid[r][c] && n == 2;
 };
 
-const countLivingNeighbors = (i, j, cells) => {
-  return neighborOffsets(i, j).reduce((c, o) => cells[i + o.row][j + o.col] ? c + 1 : c, 0);
+const livingNeighbors = (grid, r, c) => {
+  let count = 0;
+  for (let dx = -1; dx <= 1; dx++) {
+    for (let dy = -1; dy <= 1; dy++) {
+      if (!(dx === 0 && dy === 0)) {
+        const nr = r + dx;
+        const nc = c + dy;
+        if (inBounds(nr, grid) && inBounds(nc, grid[nr]) && grid[nr][nc]) {
+          count++
+        }
+      }
+    }
+  }
+  return count;
 };
 
-const go = () => {
-  drawCells(current);
-  current = nextCells(current);
-  setTimeout(go, 100);
+const inBounds = (index, array) => 0 <= index && index < array.length;
+
+const newGrid = () => {
+  const grid = [];
+  for (let r = 0; r < rows; r++) {
+    const row = [];
+    for (let c = 0; c < columns; c++) {
+      row.push(false);
+    }
+    grid.push(row);
+  }
+  return grid;
 };
 
-let current = originalCells(ROWS, COLS);
-go();
+const addPattern = (grid, top, left, pattern) => {
+  const p = pattern.trim().split('\n');
+  for (let r = 0; r < p.length; r++) {
+    for (let c = 0; c < p[r].length; c++) {
+      grid[top + r][left + c] = p[r][c] !== '.';
+    }
+  }
+};
+
+let next = now();
+let grid = newGrid();
+
+const clearGrid = () => {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      grid[r][c] = false;
+    }
+  }
+};
+
+const randomize = (p) => {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      grid[r][c] = Math.random() < p;
+    }
+  }
+};
+
+const glider = (r, c) => {
+  addPattern(grid, r, c, GLIDER);
+};
+
+const gun = (r, c) => {
+  addPattern(grid, r, c, GOSPER_GG);
+};
+
+const blinker = (r, c) => {
+  addPattern(grid, r, c, H_BLINKER);
+};
+
+const multiBlinker = (r, c) => {
+  addPattern(grid, r, c, H_BLINKER);
+  addPattern(grid, r, c + 6, H_BLINKER);
+  addPattern(grid, r - 4, c + 4, V_BLINKER);
+  addPattern(grid, r + 2, c + 4, V_BLINKER);
+};
+
+const block = (r, c) => {
+  addPattern(grid, r, c, BLOCK);
+};
+
+const pulsar = (r, c) => {
+  addPattern(grid, r, c, PULSAR);
+};
+
+// Try foo(12, 42) with SIZE = 4. Or foo(12, 64) on big screen
+const foo = (r, c) => {
+  pulsar(5, columns - 22);
+  pulsar(Math.floor(rows / 2), Math.floor(columns / 2));
+  gun(r, c);
+};
+
+const bar = () => {
+  const mr = Math.floor(rows / 2);
+  const mc = Math.floor(columns / 2);
+  pulsar(mr - 42, mc + 72)
+  pulsar(mr, mc);
+  gun(mr - 45, mc - 62);
+};
+
+const makePulsar = () => {
+  glider(10, 30);
+  glider(0, 25);
+};
+
+const makeExplosion = () => {
+  const midRow = Math.floor(rows / 2);
+  const midCol = Math.floor(columns / 2);
+  addPattern(grid, midRow, midCol, '##\n##');
+  glider(0, midCol - (midRow - 4));
+};
+
+const drawFrame = (t) => {
+  if (t > next) {
+    render(grid);
+    const iters = RATE < 0 ? -RATE : 1;
+    for (let i = 0; i < iters; i++) {
+      grid = nextGeneration(grid)
+    }
+    next += Math.max(0, RATE);
+    next = Math.max(next, now());
+  }
+};
+
+animate(drawFrame);
