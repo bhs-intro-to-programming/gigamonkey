ef2761cbf834b23d8fb5b98ebf52c532d489e71f
Peter Seibel <peter@gigamonkeys.com>
2023-03-09 16:47:26 -0800

    New refactoring original code.

--- projects/new-refactoring/public/script.js
+++ projects/new-refactoring/public/script.js
@@ -1,5 +1,101 @@
-import { setCanvas, drawCircle, width, height } from './graphics.js';
+import {
+  setCanvas,
+  drawLine,
+  drawCircle,
+  drawRect,
+  drawTriangle,
+  drawFilledCircle,
+  drawFilledRect,
+  drawFilledTriangle,
+  drawText,
+  clear,
+  width,
+  height,
+} from './graphics.js';
 
 setCanvas(document.getElementById('screen'));
 
-drawCircle(width / 2, height / 2, 100, 'black');
+const drawPicture = (horizon) => {
+  drawSky(horizon);
+  drawGround(horizon);
+  drawSun(100, 6, 2.0)
+  drawClouds();
+  drawTrees(5, horizon * 1.1);
+};
+
+const drawSky = (horizon) => {
+  drawFilledRect(0, 0, width, horizon, 'skyblue');
+};
+
+const drawGround = (horizon) => {
+  drawFilledRect(0, horizon, width, horizon, 'green');
+};
+
+const drawSun = (size, rays, rayProportion) => {
+  drawFilledCircle(width, 0, size, 'yellow');
+  drawRays(rays, size * rayProportion);
+};
+
+const drawRays = (rays, length) => {
+  const startAngle = (Math.PI / 2) * 0.023;
+  const r = ((Math.PI / 2) - 2 * startAngle) / (rays - 1);
+  for (let i = 0; i < rays; i++) {
+    const angle = startAngle + Math.PI + (i * r);
+    drawRay(width, 0, angle, length);
+  }
+};
+
+const drawRay = (x, y, angle, length) => {
+  const x2 = x + length * Math.cos(angle);
+  const y2 = y - length * Math.sin(angle);
+  drawLine(x, y, x2, y2, 'yellow', 7);
+};
+
+const drawClouds = () => {
+  drawCloud(width * 0.1, height * 0.2, 25);
+  drawCloud(width * 0.5, height * 0.2, 35);
+};
+
+const drawCloud = (x, y, size) => {
+  drawFilledCircle(x, y, size, 'white');
+  drawFilledCircle(x+size*2.5, y, size, 'white');
+  drawFilledCircle(x+(size*1.25), y - size * 0.5, size, 'white');
+  drawFilledCircle(x+(size*1.25), y + size * 0.5, size, 'white');
+};
+
+const drawTrees = (num, baseY) => {
+  const gap = width / (num + 1);
+  for (let i = 0; i < num; i++) {
+    drawTree((i + 1) * gap, baseY, 20, 55, 40);
+  }
+};
+
+const drawTree = (baseX, baseY, trunkWidth, trunkHeight, leavesRadius) => {
+  drawFilledRect(baseX, baseY - trunkHeight, trunkWidth, trunkHeight, 'sienna');
+  const leavesX = baseX + trunkWidth / 2;
+  const leavesY = baseY - trunkHeight - (leavesRadius - 2);
+  drawLeaves(leavesX, leavesY, leavesRadius);
+  drawApples(5 + Math.floor(Math.random() * 3), leavesX, leavesY, leavesRadius, 6);
+};
+
+const drawLeaves = (x, y, radius) => {
+  drawFilledCircle(x, y, radius, 'forestgreen');
+};
+
+const drawApples = (num, x, y, radius, appleRadius) => {
+  drawApple(x, y, appleRadius);
+  for (let i = 0; i < num; i++) {
+    const angle = i * ((Math.PI * 2) / num);
+    const d = radius - appleRadius * 1.25 - (Math.random() * appleRadius * 2);
+    const ax = x + d * Math.cos(angle);
+    const ay = y + d * Math.sin(angle);
+    drawApple(ax, ay, appleRadius);
+  }
+};
+
+const drawApple = (x, y, r) => {
+  const jitter = () => -r/2 + Math.random() * r;
+  drawFilledCircle(x + jitter(), y + jitter(), r, 'crimson');
+};
+
+drawPicture(height * 0.78);
