177d945e0d1f3104ebfe2f4ea7f791e814bf4572
Peter Seibel <peter@gigamonkeys.com>
2023-03-13 18:05:40 -0700

    Updating assessments/2023-03-08-hofs/code.js

--- assessments/2023-03-08-hofs/code.js
+++ assessments/2023-03-08-hofs/code.js
@@ -1,3 +1,9 @@
-const shouty = (strings) => {
-  return strings.map((s) => s.toUpperCase());
-};
+const evens = (numbers) => {
+  let r = [];
+  for (let i = 0; i < numbers; i++) {
+    if (numbers[i] % 2 === 0) {
+      r.push(numbers[i]);
+    }
+  }
+  return r;
+}
