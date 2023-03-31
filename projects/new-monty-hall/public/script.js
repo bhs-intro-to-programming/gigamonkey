0f103760d9bc9ab8413a90ae343113de645e8ff1
Peter Seibel <peter@gigamonkeys.com>
2023-03-30 21:54:33 -0700

    Rebuilding monty hall in class.

--- projects/new-monty-hall/public/script.js
+++ projects/new-monty-hall/public/script.js
@@ -1,1 +1,28 @@
 // Write your code here.
+
+const prizeBehind = Math.floor(Math.random() * 3);
+
+console.log(prizeBehind);
+
+const container = document.querySelector('#doors');
+
+const doors = container.querySelectorAll('div');
+
+const showDoor = (i, text) => {
+  doors[i].innerText = text;
+};
+
+
+
+for (let i = 0; i < doors.length; i++) {
+  doors[i].onclick = (e) => {
+    console.log(`Clicked door ${i}`);
+    let possibleDoors = [];
+    for (let j = 0; j < 3; j++) {
+      if (j !== i && j !== prizeBehind) {
+        possibleDoors.push(j);
+      }
+    }
+    showDoor(possibleDoors[Math.floor(Math.random() * possibleDoors.length)], '$0');
+  };
+}
