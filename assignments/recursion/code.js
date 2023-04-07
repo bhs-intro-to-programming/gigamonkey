d4d11beca15b462ca0988283bdd1bbbfaf450525
Peter Seibel <peter@gigamonkeys.com>
2023-04-07 14:13:11 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -4,4 +4,4 @@ const factorial = (n) => n < 2 ? 1 : n * factorial(n - 1);
 
 const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
-const sum = (ns) => 0;
+const sum = (ns) => ns.length === 0 ? 0 : ns[0] + sum(ns.slice(1));
