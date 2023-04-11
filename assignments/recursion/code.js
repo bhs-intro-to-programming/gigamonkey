e984a194b0284fce043516cf76197ad172d0f433
Peter Seibel <peter@gigamonkeys.com>
2023-04-10 20:03:31 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -2,7 +2,10 @@ const factorial = (n) => n < 2 ? 1 : n * factorial(n - 1);
 
 const triangular = (n) => n === 0 ? 0 : n + triangular(n - 1);
 
-const fibonacci = (n) => n < 2 ? n : fibonacci(n - 2) + fibonacci(n - 1);
+const fibonacci = (n) => {
+  console.log(n);
+  return n < 2 ? n : fibonacci(n - 2) + fibonacci(n - 1);
+};
 
 const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
