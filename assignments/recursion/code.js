a0b6083e82f02d78d44dc3a265dea47bf951bc85
Peter Seibel <peter@gigamonkeys.com>
2023-04-11 18:07:48 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -13,7 +13,7 @@ const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
 const sum = (ns) => ns.length === 0 ? 0 : ns[0] + sum(ns.slice(1));
 
-const search = (xs, x) => xs.length === 0 ? false : xs[0] === x || search(xs.slice(1), x);
+const search = (xs, x) => xs.length > 0 && (xs[0] === x || search(xs.slice(1), x));
 
 const reverseString = (s) => s === "" ? s : reverseString(s.substring(1)) + s[0];
 
