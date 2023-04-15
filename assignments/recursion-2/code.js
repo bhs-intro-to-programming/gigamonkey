48bce068c47b00b0075c9aca3c88694f53e91cb5
Peter Seibel <peter@gigamonkeys.com>
2023-04-15 15:14:48 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -4,4 +4,6 @@ const multiply = (a, b) => b === 0 ? 0 : b === 1 ? a : a + multiply(a, b - 1);
 
 const double = (a, b) => b === 0 ? a : 2 * double(a, b - 1);
 
+const triple = (a, b) => b === 0 ? a : 3 * triple(a, b - 1);
+
 const exponentiation = (a, b) => b === 0 ? 1 : a * exponentiation(a, b - 1);
