499d47d192cbd8a0051c694060a3eec2bcc384ef
Peter Seibel <peter@gigamonkeys.com>
2023-04-16 14:16:30 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -1,10 +1,4 @@
-const factorial = (n) => {
-  if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-};
+const factorial = (n) => n === 0 ? 1 : n * factorial(n - 1);
 
 const triangular = (n) => {
   if (n === 0) {
