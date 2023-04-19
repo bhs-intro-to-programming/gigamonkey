167e606508ea367fedf9f0b11dcdd53fbe2399cd
Peter Seibel <peter@gigamonkeys.com>
2023-04-19 08:51:38 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -1,1 +1,9 @@
+const product = (ns) => {
+  let r = 1;
+  for (let i = 0; i < ns.length; i++) {
+    r *= ns[i];
+  }
+  return r;
+}
+
 const sumSquares = (n) => n === 0 ? 0 : n ** 2 + sumSquares(n - 1);
