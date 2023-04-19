908e57916630e2ac3e4ddd1569d32e89552b3a81
Peter Seibel <peter@gigamonkeys.com>
2023-04-19 10:28:51 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -98,17 +98,8 @@ const triple = (a, b) => b === 0 ? a : 3 * triple(a, b - 1);
 
 const power = (a, b) => b === 0 ? 1 : a * power(a, b - 1);
 
-const deleteXs = (s) => {
-  if (s === '') {
-    return s;
-   } else {
-     if (s[0] === 'x') {
-       return deleteXs(s.substring(1));       
-     } else {
-       return s[0] + deleteXs(s.substring(1));
-     }
-   }
-};
+const deleteXs = (s) => s === '' ? '' : (s[0] === 'x' ? '' : s[0]) + deleteXs(s.substring(1));
+
 
 const countXs = (s) => {
   return [...s].reduce((count, c) => count + (c === 'x' ? 1 : 0), 0);
