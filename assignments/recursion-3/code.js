e89816a6d6bb4c98486b7c380de19676bbae34d2
Peter Seibel <peter@gigamonkeys.com>
2023-04-19 17:55:41 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -10,10 +10,10 @@ const isAscending = (ns) => ns.length < 2 || ns[0] <= ns[1] && isAscending(ns.sl
 const isDescending = (ns) => ns.length < 2 || ns[0] >= ns[1] && isDescending(ns.slice(1));
 
 const sumNested = (arg) =>
-  arg.length === 0
-    ? 0
-    : isNumber(arg)
-      ? arg
+  isNumber(arg)
+    ? arg
+    : arg.length === 0
+      ? 0
       : sumNested(arg[0]) + sumNested(arg.slice(1));
 
 const searchNested = (tree, target) =>
