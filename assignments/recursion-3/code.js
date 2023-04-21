2e762022658a9f1a278abc3ae25cb73cf945fd4f
Peter Seibel <peter@gigamonkeys.com>
2023-04-20 18:02:50 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -118,6 +118,6 @@ const evaluate = (expr) => {
 
 
 const isPrime = (n) => {
-  const loop = (f) => f ** 2 < n && n % f !== 0 && loop(f + 1);
+  const loop = (f) => f ** 2 < n ? (n % f !== 0) && loop(f + 1) : true;
   return loop(2);  
 }
