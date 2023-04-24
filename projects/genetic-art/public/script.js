39f6aa3d4adc468c8f9786051ad6f97194a59c74
Peter Seibel <peter@gigamonkeys.com>
2023-04-24 12:51:23 -0700

    Loading reference image.

--- projects/genetic-art/public/script.js
+++ projects/genetic-art/public/script.js
@@ -1,5 +1,18 @@
-import { setCanvas, drawCircle, width, height } from './graphics.js';
+const doc = Object.fromEntries([...document.querySelectorAll('[id]')].map(e => [e.id, e]));
 
-setCanvas(document.getElementById('screen'));
+const fillReference = () => {
 
-drawCircle(width / 2, height / 2, 100, 'black');
+  const w = 500;
+  const h = doc.image.naturalHeight * w / doc.image.naturalWidth;
+
+  doc.reference.width = w
+  doc.reference.height = h;
+
+  doc.generated.width = w;
+  doc.generated.height = h;
+
+  doc.reference.getContext('2d').drawImage(doc.image, 0, 0, w, h);
+};
+
+
+doc.image.onload = fillReference;
