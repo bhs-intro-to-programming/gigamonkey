a7997ff5662907f87ea5f078d9b9b1a032c3585f
Peter Seibel <peter@gigamonkeys.com>
2023-05-02 12:57:08 -0700

    Updating assessments/refactoring-3/code.js

--- assessments/refactoring-3/code.js
+++ assessments/refactoring-3/code.js
@@ -1,5 +1,10 @@
 const drawPicture = () => {
   drawFilledRect(0, 0, width, height, '#ff00ff99');
+  drawSnowflake(width/2, height/2, 10);
+};
+
+const drawSnowflake = (x, y, size) => {
+  drawFilledTriangle(x - 10, y - 10, x + 10, y - 10, x, y + 10, 'white')
 };
 
 drawPicture();
