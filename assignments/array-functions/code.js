31c1e155dd36008520b722653b6e82496ab6541a
Peter Seibel <peter@gigamonkeys.com>
2023-05-11 07:31:21 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,3 +1,3 @@
 //const countTens = (ns) => ns.filter(n => n === 10).length;
 
-const countTens = (ns) => ns.reduce((c, n) => c + (n === 10 ? 1 : -1), 0)
+const countTens = (ns) => ns.reduce((c, n) => c + (n === 10 ? 1 : 0), 0)
