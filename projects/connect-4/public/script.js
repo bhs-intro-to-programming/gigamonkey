f6c96e8cba4b5c02d9a1e9f763e23f9dbd90ab43
Peter Seibel <peter@gigamonkeys.com>
2023-05-19 16:15:16 -0700

    wip

--- projects/connect-4/public/script.js
+++ projects/connect-4/public/script.js
@@ -1,5 +1,19 @@
-for (let c = 0; c < 7; c++) {
+const board = Array(6).fill().map(() => Array(7).fill());
+
+const setupBoard = () => {
   for (let r = 0; r < 6; r++) {
-    document.getElementById('grid').append(document.createElement('div'));
+    for (let c = 0; c < 7; c++) {
+      const cell = document.createElement('div');
+      document.getElementById('grid').append(cell);
+      board[r][c] = cell;
+    }
   }
 }
+
+const placePiece = (r, c, color) => {
+  board[r][c].classList.add(color);
+}
+
+setupBoard();
+
+placePiece(5, 3, 'red');
