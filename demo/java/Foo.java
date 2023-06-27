b3781dd5b4bfd4e879fee31bcdaf1fd224e8649d
Peter Seibel <peter@gigamonkeys.com>
2023-06-27 16:56:10 -0700

    Updating demo/java/Foo.java

--- demo/java/Foo.java
+++ demo/java/Foo.java
@@ -1,5 +1,5 @@
 public class Foo {
   public static void main(String[] argv) {
-    System.out.println("hello, world!");
+    System.out.println("hello, world!" + System.currentTimeMillis());
   }
 }
