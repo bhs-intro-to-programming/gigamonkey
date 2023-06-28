17487676a3accb21e8d211ebecf4f22f89bfde37
Peter Seibel <peter@gigamonkeys.com>
2023-06-27 19:42:06 -0700

    Updating demo/java/Foo.java

--- demo/java/Foo.java
+++ demo/java/Foo.java
@@ -1,7 +1,7 @@
 import java.nio.charset.StandardCharsets;
 
 public class Foo {
-  public static void main(String[] argv) {
+  public static void main(String[] argv) throws Exception {
     var text = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
     System.out.println("hello, world! " + System.currentTimeMillis());
     System.out.println(text.length() + " characters in input.");
