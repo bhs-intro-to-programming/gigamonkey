4a6f0d1e2cf5712d82bc5724c0f3e7becf7bca67
Peter Seibel <peter@gigamonkeys.com>
2023-06-28 09:53:18 -0700

    Updating demo/java/Foo.java

--- demo/java/Foo.java
+++ demo/java/Foo.java
@@ -6,6 +6,10 @@ public class Foo {
     var text = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
     System.out.println("hello, world! " + System.currentTimeMillis());
     System.out.println(text.length() + " characters in input.");
+    System.out.println(fib(40));
   }
-  public static long fib(long n) { return n < 2 ? n : fib(n - 2) + fib(n - 1); }
+
+  public static long fib(long n) {
+    return n < 2 ? n : fib(n - 2) + fib(n - 1);
+  }
 }
