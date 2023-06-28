af5779680d8874aff0b1b9c2e642c661496485a6
Peter Seibel <peter@gigamonkeys.com>
2023-06-27 20:03:48 -0700

    Updating demo/java/Foo.java

--- demo/java/Foo.java
+++ demo/java/Foo.java
@@ -4,6 +4,6 @@ public class Foo {
   public static void main(String[] argv) throws Exception {
     var text = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
     System.out.println("hello, world! " + System.currentTimeMillis());
-    System.out.println(text.length() + " characters in input.");
+    System.out.println(text.length() + " characters in input.")
   }
 }
