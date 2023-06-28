cc62b0e706ddbf83ab1b2f2b82e887008d3dc559
Peter Seibel <peter@gigamonkeys.com>
2023-06-27 19:41:26 -0700

    Updating demo/java/Foo.java

--- demo/java/Foo.java
+++ demo/java/Foo.java
@@ -1,6 +1,6 @@
 public class Foo {
   public static void main(String[] argv) {
-    var text = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+    var text = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
     System.out.println("hello, world! " + System.currentTimeMillis());
     System.out.println(text.length() + " characters in input.");
   }
