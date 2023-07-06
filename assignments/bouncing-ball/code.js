55ad3dd57fe12e9056b9aa1e80b1d7f86ccd92e0
Peter Seibel <peter@gigamonkeys.com>
2023-07-06 13:50:30 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -29,7 +29,7 @@ const drawFrame = (time) => {
 };
 
 // Compute the height in pixels at time t after the ball hit the ground
-const height = (t) => Math.max(0, t * (bounce - (gravity*t)) / 2);
+const ballHeight = (t) => Math.max(0, t * (bounce - (gravity*t)) / 2);
 
 // Compute the shade of the shadow. 0 is black; 255 is white.
 const shadowDarkness = (h) => 128;
