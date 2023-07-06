d94e86f3b22403563848748e00a89fc492a0a407
Peter Seibel <peter@gigamonkeys.com>
2023-07-06 13:52:55 -0700

    Updating assignments/bouncing-ball/code.js

--- assignments/bouncing-ball/code.js
+++ assignments/bouncing-ball/code.js
@@ -1,5 +1,5 @@
 // Change these to change the physics of our world.
-let bounce = 1.2
+let bounce = 0.7;
 let gravity = 0.001;
 let ballSize = 15;
 
@@ -11,31 +11,32 @@ let ballSize = 15;
 //
 // drawBall(height, size) - draws the ball at the given height and size.
 //
-// now() - returns the number of milliseconds since the program started.
+// now() - returns the current time in milliseconds.
 
 // Implement this in terms of drawShadow(), drawBall() and the functions below.
 // May also need to define a variable to keep track of when each bounce starts
 // as the time value that is passed in is just the current time, i.e. the same
 // thing we would get from calling now().
+
 let start = now();
 
 const drawFrame = (time) => {
-    let h = height(time - start);
-    drawShadow(shadowSize(h), shadowDarkness(h));
-    drawBall(h, ballSize);
-    if (h <= 0) {
-        start = now();
-    }
+  const h = height(time - start);
+  drawShadow(shadowSize(h), shadowDarkness(h));
+  drawBall(h, ballSize);
+  if (h <= 0) {
+    start = time;
+  }
 };
 
 // Compute the height in pixels at time t after the ball hit the ground
-const height = (t) => Math.max(0, t * (bounce - (gravity*t)) / 2);
+const height = (t) => Math.max(0, t * (bounce - (gravity * t) / 2));
 
-// Compute the shade of the shadow. 0 is black; 255 is white.
-const shadowDarkness = (h) => 128;
+// The higher we are, the lighter the shadow.
+const shadowDarkness = (h) => 148 + 1.015 ** h;
 
-// Compute the size of the shadow.
-const shadowSize = (h) => ballSize/2 * 1.01 ** h;
+// The higher we are, the bigger the shadow.
+const shadowSize = (h) => 0.1 * (h + ballSize) + ballSize / 2;
 
 // Call the animate function from the framework.
 animate(drawFrame);
