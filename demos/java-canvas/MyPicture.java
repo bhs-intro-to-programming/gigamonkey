4bb395588124adad67c31703e9858c1282e9a29f
Peter Seibel <peter@gigamonkeys.com>
2023-07-08 12:56:37 -0700

    Updating demos/java-canvas/MyPicture.java

--- demos/java-canvas/MyPicture.java
+++ demos/java-canvas/MyPicture.java
@@ -7,7 +7,7 @@ public class MyPicture {
 
   public static void main(String[] argv) {
     var c = new Canvas(200, 400);
-    c.drawLine(0, 0, 200, 400, "blue", 1);
+    c.drawLine(0, 0, 200, 400, "red", 1);
     System.out.println(c.getCode());
   }
 
