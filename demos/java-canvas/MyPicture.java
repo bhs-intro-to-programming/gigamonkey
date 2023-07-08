81df986c191db32359ed83689c50d463932fc686
Peter Seibel <peter@gigamonkeys.com>
2023-07-07 20:54:49 -0700

    Updating demos/java-canvas/MyPicture.java

--- demos/java-canvas/MyPicture.java
+++ demos/java-canvas/MyPicture.java
@@ -6,7 +6,20 @@ public class MyPicture {
 
   public static void main(String[] argv) {
     System.out.println("""
-    alert('hello from Java');
+    const canvas = document.querySelector('canvas');
+    const r = canvas.parentElement.getBoundingClientRect();
+    canvas.setAttribute('width', r.width - 2);
+    canvas.setAttribute('height', r.height - 2);
+
+    const ctx = canvas.getContext('2d');
+    const width = canvas.width;
+    const height = canvas.height;
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(width, height);
+    ctx.stroke();
     """);
   }
 
