1ca9f63d2ebd2e939626d9f8b4dd153d6ca42b77
Peter Seibel <peter@gigamonkeys.com>
2023-07-08 12:45:40 -0700

    Updating demos/java-canvas/MyPicture.java

--- demos/java-canvas/MyPicture.java
+++ demos/java-canvas/MyPicture.java
@@ -6,25 +6,9 @@ import com.gigamonkeys.bhs.Canvas;
 public class MyPicture {
 
   public static void main(String[] argv) {
-    System.out.println("""
-    (() => {
-    const canvas = document.querySelector('canvas');
-    const r = canvas.parentElement.getBoundingClientRect();
-    canvas.setAttribute('width', r.width - 2);
-    canvas.setAttribute('height', r.height - 2);
-
-    const ctx = canvas.getContext('2d');
-    const width = canvas.width;
-    const height = canvas.height;
-    ctx.clearRect(0, 0, width, height);
-    ctx.strokeStyle = 'rebeccapurple';
-    ctx.lineWidth = 10;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(width, height);
-    ctx.stroke();
-    })();
-    """);
+    var c = new Canvas(200, 400);
+    c.drawLine(0, 0, 200, 400, 'blue', 1);
+    System.out.println(c.toCode());
   }
 
 }
