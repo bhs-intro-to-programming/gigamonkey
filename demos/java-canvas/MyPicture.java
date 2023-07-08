a7263e4a89d4488f9bef886ba200fd789f5e0886
Peter Seibel <peter@gigamonkeys.com>
2023-07-08 14:20:58 -0700

    Updating demos/java-canvas/MyPicture.java

--- demos/java-canvas/MyPicture.java
+++ demos/java-canvas/MyPicture.java
@@ -3,6 +3,10 @@ import com.gigamonkeys.bhs.*;
 public class MyPicture implements Picture {
 
   public void draw(Canvas c) {
+    for (var x = 0; x < width; x += 20) {
+      c.drawLine(x, 0, x, height, "black", 2);
+    }
+
     c.drawLine(0, 0, c.width(), c.height(), "blue", 10);
     c.drawLine(0, c.height(), c.width(), 0, "red", 10);
   }
