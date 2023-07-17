3bc43a2db4de61a9db80f33f5ddd08b0d03f0572
Peter Seibel <peter@gigamonkeys.com>
2023-07-17 10:15:46 -0700

    Updating demos/java/Foo.java

--- demos/java/Foo.java
+++ demos/java/Foo.java
@@ -1,14 +1,7 @@
-import com.gigamonkeys.bhs.Canvas;
-
-/*
- * Draw a picture.
- */
 public class Foo {
 
   public static void main(String[] argv) {
-    var c = new Canvas(200, 400);
-    c.drawLine(0, 0, 200, 400, "blue", 1);
-    System.out.println(c.getCode());
+    System.out.println("hello, world!")
   }
 
 }
