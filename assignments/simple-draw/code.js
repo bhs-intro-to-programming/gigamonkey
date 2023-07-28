9c8bb776e27ac59364e77b617dd323e4b318807a
Peter Seibel <peter@gigamonkeys.com>
2023-07-28 11:41:46 -0400

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,4 +1,8 @@
 const headSize = 50;
 drawFilledCircle(width / 2, headSize + 20, headSize, 'white');
 const bodySize = headSize * 1.2;
-drawFilledCircle(width / 2, headSize + 20 + headSize + bodySize, bodySize, 'white');
+const bodyY = headSize + 20 + headSize + bodySize;
+drawFilledCircle(width / 2, bodyY, bodySize, 'white');
+const buttSize = bodySize * 1.3;
+const buttY = bodyY + bodySize + buttSize;
+drawFilledCircle(width / 2, buttY, buttSize, 'white');
