494444533dd41ba86543d6ec727d0da7a43232ee
Peter Seibel <peter@gigamonkeys.com>
2023-07-28 11:46:35 -0400

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,8 +1,8 @@
 const drawSnowman = (x, headY, headSize, squish) => {
   const bodySize = headSize * 1.2;
-  const bodyY = headY + (headSize * (1 - squish) + bodySize;
+  const bodyY = headY + (headSize * (1 - squish)) + bodySize;
   const buttSize = bodySize * 1.3;
-  const buttY = bodyY + (bodySize * (1 - squish) + buttSize;
+  const buttY = bodyY + (bodySize * (1 - squish)) + buttSize;
 
   drawFilledCircle(x, headY, headSize, 'white');
   drawFilledCircle(x, bodyY, bodySize, 'white');
