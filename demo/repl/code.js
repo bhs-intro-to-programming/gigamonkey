c305fe01cc828419379f3d8b73971d4366077014
Peter Seibel <peter@gigamonkeys.com>
2023-07-28 12:52:27 -0400

    Updating demo/repl/code.js

--- demo/repl/code.js
+++ demo/repl/code.js
@@ -298,5 +298,5 @@ const code = (strings, ...exprs) => {
 // test
 
 const revN = (n) => {
-  return (n % 10) * (10 ** Math.floor(Math.log10(n))) + r(Math.floor(n / 10));
+  return (n % 10) * (10 ** Math.floor(Math.log10(n))) + revN(Math.floor(n / 10));
 }
