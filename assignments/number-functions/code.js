abb24d2f90f845dc15f944558c83a491c93e3d79
Peter Seibel <peter@gigamonkeys.com>
2023-08-22 13:17:46 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -5,3 +5,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;   
 
 const distance = (a, b) => Math.abs(a - b);
+
+function manhattanDistance(x1, y1, x2, y2) {
+  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+ }
